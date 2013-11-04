0ef9abdcc7366c3e57656d540415061d1d7a15d2
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-04 06:13:16 +0000

    v8: add bottle.

--- Library/Formula/v8.rb
+++ Library/Formula/v8.rb
@@ -10,6 +10,13 @@ class V8 < Formula
   url 'https://github.com/v8/v8/archive/3.19.18.4.tar.gz'
   sha1 'f44c8eed0fe93b2d04d1d547a1e2640f41161354'
 
+  bottle do
+    cellar :any
+    sha1 'd17716c427c0d7f6cf287fbd43689d4b59ea4471' => :mavericks
+    sha1 '6c0c8f7c0c4e8edfbfb8b3dd4ee185d71b3c984b' => :mountain_lion
+    sha1 'b227a126ccd3a5b6ff18dee7ad366eb4eb775dce' => :lion
+  end
+
   devel do
     url 'https://github.com/v8/v8/archive/3.20.12.tar.gz'
     sha1 '1463f4b8b33674bfd366e84b739713a727e9f9ac'
