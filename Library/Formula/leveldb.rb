f5ed023d3c5ec33152f8295ebf01eb1367786514
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-07 11:18:13 +0000

    leveldb: add bottle.

--- Library/Formula/leveldb.rb
+++ Library/Formula/leveldb.rb
@@ -5,6 +5,13 @@ class Leveldb < Formula
   url 'https://leveldb.googlecode.com/files/leveldb-1.14.0.tar.gz'
   sha1 '641d54df4aaf7ee569ae003cfbdb888ebdee0d7f'
 
+  bottle do
+    cellar :any
+    sha1 '026fb2f75c6dc6b29ef61383cc6995bf74b99838' => :mavericks
+    sha1 '0d357e90f7ef027aeb479274cfeb749eefc3f15b' => :mountain_lion
+    sha1 'dcd6d027e34dbd5f86e8620da3fe119ba55c5b56' => :lion
+  end
+
   depends_on 'snappy' => :build
 
   def install
