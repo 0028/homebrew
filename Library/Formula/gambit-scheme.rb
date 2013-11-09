c3416b011456ca77c491000bb2b1b94a567388f1
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-09 07:02:32 +0000

    gambit-scheme: add bottle.

--- Library/Formula/gambit-scheme.rb
+++ Library/Formula/gambit-scheme.rb
@@ -3,6 +3,12 @@ require 'formula'
 class GambitScheme < Formula
   homepage 'http://dynamo.iro.umontreal.ca/~gambit/wiki/index.php/Main_Page'
   url 'http://www.iro.umontreal.ca/~gambit/download/gambit/v4.7/source/gambc-v4_7_0.tgz'
+  bottle do
+    sha1 '4d271cdc2bc76776deafd50b889d59922b39a103' => :mavericks
+    sha1 '627252d849ca6656a3ca76a4c2476da346ece155' => :mountain_lion
+    sha1 'a8f2f67e379424d0492f4a3cc22e3555958d199d' => :lion
+  end
+
   sha256 '2b03ecef89da2a53212dc3e6583ee4175d91a0752779e1758bcab5d09e9d1e63'
 
   option 'with-check', 'Execute "make check" before installing'
