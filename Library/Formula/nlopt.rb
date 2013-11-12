66de2b64e4f34065250cf9abd59bb0e7a1dfab51
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-12 16:05:50 +0000

    nlopt: add bottle.

--- Library/Formula/nlopt.rb
+++ Library/Formula/nlopt.rb
@@ -5,6 +5,12 @@ class Nlopt < Formula
   url 'http://ab-initio.mit.edu/nlopt/nlopt-2.4.tar.gz'
   sha1 'e766f4c49fa5923fb45220f278c01c04c38fc369'
 
+  bottle do
+    sha1 'b217101d63ce2186ec99ca5d022f6cbe23a7ef52' => :mavericks
+    sha1 '8f2141ad1e6571f932b1ebfe39b53ad8f997f4ef' => :mountain_lion
+    sha1 '2b85a2ec2f573b91a4e4d59114498bcfabc325fb' => :lion
+  end
+
   option :cxx11
 
   def install
