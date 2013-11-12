b07f99b311aa77619e55052f35c6ea1dc9828957
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-12 12:12:01 +0000

    smartsim: add bottle.

--- Library/Formula/smartsim.rb
+++ Library/Formula/smartsim.rb
@@ -6,6 +6,12 @@ class Smartsim < Formula
   sha1 'ee3e5b4b4d7615ebbd9a1e944d0e506e697e7da0'
   head 'https://github.com/ashleynewson/smartsim'
 
+  bottle do
+    sha1 '95c6dd1b5ec18395e9861cbdc4e89c6e1ae32f2c' => :mavericks
+    sha1 '2f9c689e9052c980306d86ed61daa84f8da516ed' => :mountain_lion
+    sha1 '324774422c0b8dd7b0508d4fb5813ed8392e3758' => :lion
+  end
+
   depends_on :x11
   depends_on 'gtk+'
   depends_on 'librsvg'
