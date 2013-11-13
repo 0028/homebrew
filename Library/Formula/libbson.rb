c17a12f39b131024a74d96668cd6fe38077dc182
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-13 18:14:40 +0000

    libbson: add bottle.

--- Library/Formula/libbson.rb
+++ Library/Formula/libbson.rb
@@ -5,6 +5,12 @@ class Libbson < Formula
   url 'https://github.com/mongodb/libbson/releases/download/0.2.4/libbson-0.2.4.tar.gz'
   sha1 '53972c042f7d453e172132fe499b21a216746b9d'
 
+  bottle do
+    sha1 'a0226e7ae74fd0fed93a2db776007d2ef06b52e4' => :mavericks
+    sha1 '9b756f984910014ffaf1e6cbfb559a5185cb6cda' => :mountain_lion
+    sha1 '1dd66c5dc9422b2fe2570796a03c294c7505d34a' => :lion
+  end
+
   depends_on :automake => :build
 
   def install
