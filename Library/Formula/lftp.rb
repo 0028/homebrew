aa1841a9268800499f6a3a2b8ce532a196767d68
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-15 20:08:09 +0000

    lftp: add bottle.

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Lftp < Formula
   homepage 'http://lftp.yar.ru/'
   url 'http://lftp.yar.ru/ftp/lftp-4.4.11.tar.bz2'
+  bottle do
+    sha1 '9c5613bd33241625f461cbc4b1a580d68f34909f' => :mavericks
+    sha1 '2ce8a145c56269859765259ce9ba08e93540656b' => :mountain_lion
+    sha1 'df05499dc34f9a779e4933a636c1ca6ba6ef3130' => :lion
+  end
+
   mirror 'ftp://ftp.cs.tu-berlin.de/pub/net/ftp/lftp/lftp-4.4.11.tar.bz2'
   sha1 '56cefb9aa683acfa5c5713d530f594085ea7b149'
 
