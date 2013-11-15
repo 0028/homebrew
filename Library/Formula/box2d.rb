a9cf0bcde1bccde4971133e481f636adcdf4cee9
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-15 19:55:03 +0000

    box2d: add bottle.

--- Library/Formula/box2d.rb
+++ Library/Formula/box2d.rb
@@ -5,6 +5,13 @@ class Box2d < Formula
   url 'https://box2d.googlecode.com/files/Box2D_v2.3.0.7z'
   sha1 '1d3ea1f872b3cab3ef5130a2404d74f9ff66f265'
 
+  bottle do
+    cellar :any
+    sha1 '56a4a56f09a50027534439f7693e2095f88f1523' => :mavericks
+    sha1 '657ae0f2b94bc8353dfa580764c111979ffd1965' => :mountain_lion
+    sha1 'c0b31e7574493b2a0678aad55d80050681cf22f8' => :lion
+  end
+
   depends_on 'cmake' => :build
 
   def install
