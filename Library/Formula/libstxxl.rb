c1b216831b80750813d333a3f0f450e50a5bb403
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-15 17:58:18 +0000

    libstxxl: add bottle.

--- Library/Formula/libstxxl.rb
+++ Library/Formula/libstxxl.rb
@@ -5,6 +5,13 @@ class Libstxxl < Formula
   url 'http://downloads.sourceforge.net/project/stxxl/stxxl/1.3.1/stxxl-1.3.1.tar.gz'
   sha1 '5fba2bb26b919a07e966b2f69ae29aa671892a7d'
 
+  bottle do
+    cellar :any
+    sha1 'f962f22fefc84f3404b02829c71e173203a2f569' => :mavericks
+    sha1 '0be402cb3b5c83ff5609a8759fc3b83b82280833' => :mountain_lion
+    sha1 '6e94aae85fbaa9f1026ac782e8541990683fe381' => :lion
+  end
+
   # issue has been rectified in upstream and future 1.4.0 release
   def patches; DATA; end if MacOS.version >= :mavericks
 
