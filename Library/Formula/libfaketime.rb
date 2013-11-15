8cac236ce2dcb544d25e566220b351877a1ee708
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-15 12:07:21 +0000

    libfaketime: add bottle.

--- Library/Formula/libfaketime.rb
+++ Library/Formula/libfaketime.rb
@@ -5,6 +5,12 @@ class Libfaketime < Formula
   url 'http://code-wizards.com/projects/libfaketime/libfaketime-0.9.5.tar.gz'
   sha1 '12199af854004f231892ab6976c2e99b937e2d61'
 
+  bottle do
+    sha1 'ce40fb9c7cb292d6ae4e2568990192d8ea9c2a62' => :mavericks
+    sha1 'd2da7d6b9dd7cccc9aecc5185a1320b3726b5d6f' => :mountain_lion
+    sha1 '465f9df87bfd04704c3d7f90c3ff57397f2496c2' => :lion
+  end
+
   depends_on :macos => :lion
 
   fails_with :llvm do
