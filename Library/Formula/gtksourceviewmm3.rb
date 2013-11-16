5ebe837ef562662b79717535dc72f9296f104526
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-16 20:01:49 +0000

    gtksourceviewmm3: add bottle.

--- Library/Formula/gtksourceviewmm3.rb
+++ Library/Formula/gtksourceviewmm3.rb
@@ -5,6 +5,12 @@ class Gtksourceviewmm3 < Formula
   url 'http://ftp.gnome.org/pub/GNOME/sources/gtksourceviewmm/3.2/gtksourceviewmm-3.2.0.tar.xz'
   sha1 'cac8d2fdde7b862bdb5eb12c6b8998a29e3fcf95'
 
+  bottle do
+    sha1 '888b2055df5f30423c7060d4cbbb3ea6f6e497a9' => :mavericks
+    sha1 '06312c13ff8180d7528a94df1b7093ff33dd199c' => :mountain_lion
+    sha1 'a1a34756b23560985a5b62be4d3b5dbf0fdf6c36' => :lion
+  end
+
   depends_on 'gtksourceview3'
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
