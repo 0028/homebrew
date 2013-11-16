cb207e2cd12e9bfa697263e30bb98371aff2b37f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-16 05:07:13 +0000

    cmu-sphinxbase: add bottle.

--- Library/Formula/cmu-sphinxbase.rb
+++ Library/Formula/cmu-sphinxbase.rb
@@ -5,6 +5,13 @@ class CmuSphinxbase < Formula
   url 'http://downloads.sourceforge.net/project/cmusphinx/sphinxbase/0.8/sphinxbase-0.8.tar.gz'
   sha1 'c0c4d52e143d07cd593bd6bcaeb92b9a8a5a8c8e'
 
+  bottle do
+    cellar :any
+    sha1 '6c6af3d56ad67453d22e5dc08f04c54018b48727' => :mavericks
+    sha1 '91f56102e1279daa1375bbc562adb2555941e8d6' => :mountain_lion
+    sha1 'b09c3e2e691e1674a984eaef490c46e41be437f5' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on :python
   depends_on 'libsndfile' => :optional
