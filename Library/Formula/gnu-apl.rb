cdc3ffe525e4c28d9182faa23cc6ddbf3bbe509c
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-16 04:17:26 +0000

    gnu-apl: add bottle.

--- Library/Formula/gnu-apl.rb
+++ Library/Formula/gnu-apl.rb
@@ -5,6 +5,12 @@ class GnuApl < Formula
   url 'http://ftpmirror.gnu.org/apl/apl-1.1.tar.gz'
   sha1 'de5071372b64a6d6921141cbbc3555e3b40da7af'
 
+  bottle do
+    sha1 '38daefbb549903a05aa985a05f637ff5b893ebe3' => :mavericks
+    sha1 'fa9203d107c76c5a7b4b52084fd6f71b637c5d6d' => :mountain_lion
+    sha1 'b5118e5d1b62cb6531337fcd7269aff25728517e' => :lion
+  end
+
   # GNU Readline is required; libedit won't work.
   depends_on 'readline'
 
