f801e44a9b0fcc9b010fdf34e17d58d07ed65574
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-17 09:18:20 +0000

    plenv-update: add bottle.

--- Library/Formula/plenv-update.rb
+++ Library/Formula/plenv-update.rb
@@ -7,6 +7,13 @@ class PlenvUpdate < Formula
 
   head 'git@github.com:Tacahilo/plenv-update.git'
 
+  bottle do
+    cellar :any
+    sha1 '5ad744a3b00df7e9a457671d39d1b81f852ff971' => :mavericks
+    sha1 '4372f336f80cadc579d096622a09f4ea4b660c47' => :mountain_lion
+    sha1 '73abf607009d27935574a1254bc73385e7870e4c' => :lion
+  end
+
   depends_on 'plenv'
 
   def install
