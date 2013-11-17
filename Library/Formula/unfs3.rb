a7f1ee3cc92b3fd652b52d0d239c830e70fee413
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-17 16:19:15 +0000

    unfs3: add bottle.

--- Library/Formula/unfs3.rb
+++ Library/Formula/unfs3.rb
@@ -6,6 +6,13 @@ class Unfs3 < Formula
   sha1 'a6c83e1210ce75836c672cd76e66577bfef7a17a'
   head 'https://svn.code.sf.net/p/unfs3/code/trunk/'
 
+  bottle do
+    cellar :any
+    sha1 '32b8b6750bf8db25686e45d5aa6e5289584f5359' => :mavericks
+    sha1 'de8175939db70a3ceb809ecdeb64db20c7e45a54' => :mountain_lion
+    sha1 '458172f1aa59badfb80a772ea3247bcb944d0c28' => :lion
+  end
+
   if build.head?
     depends_on "autoconf" => :build
     depends_on "automake" => :build
