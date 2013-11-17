4e5e5874a746c42c2cb5477f09d42183c8f8fa56
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-17 11:18:39 +0000

    casperjs: add bottle.

--- Library/Formula/casperjs.rb
+++ Library/Formula/casperjs.rb
@@ -5,6 +5,13 @@ class Casperjs < Formula
   url 'https://github.com/n1k0/casperjs/archive/1.0.4.tar.gz'
   sha1 '4e1bdbf2ce93506b6c0d193c90a6f9bcd58c6254'
 
+  bottle do
+    cellar :any
+    sha1 '961f9fbd6be5e173197c3ed45503f274e4b8caed' => :mavericks
+    sha1 '47ecd50baf77a1e790404b40bedb9d903334f843' => :mountain_lion
+    sha1 'd245f47572e9665512a28814ce9ad205c796a4c5' => :lion
+  end
+
   devel do
     url 'https://github.com/n1k0/casperjs/archive/1.1-beta2.tar.gz'
     sha1 'bb45f98c0196b1e6d47bd9969436abd2e888cd96'
