33694ed9aeae03c7de04bdeb895af3815d26a50f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-17 05:53:57 +0000

    arabica: add bottle.

--- Library/Formula/arabica.rb
+++ Library/Formula/arabica.rb
@@ -6,6 +6,13 @@ class Arabica < Formula
   version '20121126'
   sha1 '34d043607e048e0972a57e31bfff09086d893d14'
 
+  bottle do
+    cellar :any
+    sha1 'c8cd5af97f7e6081fa3588ac97779750e22dbd39' => :mavericks
+    sha1 '4ac37f18fb3ea748665138458daf246e33800287' => :mountain_lion
+    sha1 'c837bf4e20f7c511aa9f276734c5295a2af24057' => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
