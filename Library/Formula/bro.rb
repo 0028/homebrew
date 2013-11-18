9a97f115f4844fe8799929ef0d8c8e12ffc1b0c0
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-18 01:22:00 +0000

    bro: add bottle.

--- Library/Formula/bro.rb
+++ Library/Formula/bro.rb
@@ -5,6 +5,12 @@ class Bro < Formula
   url 'http://www.bro-ids.org/downloads/release/bro-2.2.tar.gz'
   sha1 'cd70c426ca0369f16919cf45ad3222e6287908df'
 
+  bottle do
+    sha1 'c9385679a49ffb1927536c8a8140c89aaa3bdbaf' => :mavericks
+    sha1 '4596089bbcab0cc306e38d96371563b99a273219' => :mountain_lion
+    sha1 'b57b91dd3b592d8c617ed138fc969409ad99a105' => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'swig' => :build
   depends_on 'libmagic'
