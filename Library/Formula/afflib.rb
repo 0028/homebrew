7d4d92a8ca510d4d6eb0a8e2c20fb9a5417d7354
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-18 01:24:13 +0000

    afflib: add bottle.

--- Library/Formula/afflib.rb
+++ Library/Formula/afflib.rb
@@ -5,6 +5,13 @@ class Afflib < Formula
   url 'https://github.com/simsong/AFFLIBv3/archive/v3.7.4.tar.gz'
   sha1 '589dae6f8439e97ab080026701cd0caa0636ac22'
 
+  bottle do
+    cellar :any
+    sha1 'faa3777b0e4c6776f69e303429fb5306fb5e8dfa' => :mavericks
+    sha1 '114fa84a9c4d3190a057d78edba5f0deb38e1416' => :mountain_lion
+    sha1 'dd9aaaeba77ca0206da4cda851fa315d288741e5' => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
