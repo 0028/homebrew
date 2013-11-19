8e87c234f61f5a75542f8fa4018650f67dfee372
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 18:45:19 +0000

    exiftool: add bottle.

--- Library/Formula/exiftool.rb
+++ Library/Formula/exiftool.rb
@@ -5,6 +5,13 @@ class Exiftool < Formula
   url 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-9.41.tar.gz'
   sha1 '39f5cd51eafa1b1881cc0193a38bb31dda132b59'
 
+  bottle do
+    cellar :any
+    sha1 '21ecc4ad8f8fd720fcca8e04a9feff7f4fc5f333' => :mavericks
+    sha1 'c3cf218aa405300b07a4181a3e2fe2c6846f1df3' => :mountain_lion
+    sha1 '3a6a7d72250e70ef2e05fd9357099573eab65e98' => :lion
+  end
+
   def install
     system "perl", "Makefile.PL"
     system "make", "test"
