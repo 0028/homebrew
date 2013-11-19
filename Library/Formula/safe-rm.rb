66aedc871693a9fa3e67cc0678a5e1d147f8bb4f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 00:23:10 +0000

    safe-rm: add bottle.

--- Library/Formula/safe-rm.rb
+++ Library/Formula/safe-rm.rb
@@ -5,6 +5,13 @@ class SafeRm < Formula
   url 'https://launchpad.net/safe-rm/trunk/0.10/+download/safe-rm-0.10.tar.gz'
   sha1 '6b829ae68e1fa3c8016e15ab37fcc08caef7712f'
 
+  bottle do
+    cellar :any
+    sha1 '7b61fb4cd47ba88fdada653314d2d6b053f852e1' => :mavericks
+    sha1 '9df21f25bbd414bc74b5dfeb6de8e74d4d0553bc' => :mountain_lion
+    sha1 'f721bb06573bd5ec9318cb93e59bc76432116a03' => :lion
+  end
+
   def install
     bin.install 'safe-rm'
   end
