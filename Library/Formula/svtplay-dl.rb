861dcdc61c564a4e5b42df8506e2f84f8874148c
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 14:34:56 +0000

    svtplay-dl: add bottle.

--- Library/Formula/svtplay-dl.rb
+++ Library/Formula/svtplay-dl.rb
@@ -5,6 +5,13 @@ class SvtplayDl < Formula
   url 'https://github.com/spaam/svtplay-dl/archive/0.9.2013.11.15.tar.gz'
   sha1 '62e5d3e9c3fbdf3f27c230500c8790c187b240d5'
 
+  bottle do
+    cellar :any
+    sha1 '4696dbeb5c6253992c23ae0ff467574d7ac4ffc1' => :mavericks
+    sha1 'c5f311e4d12e89d98bca914fda5c35bda51e3eea' => :mountain_lion
+    sha1 'c284142084621889e2289d928c3595f765dd8aa7' => :lion
+  end
+
   depends_on 'rtmpdump'
 
   def install
