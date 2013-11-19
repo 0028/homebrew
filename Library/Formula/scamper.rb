84baa6f08680f43df5483a633b1b24ce6c44f9eb
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 18:45:20 +0000

    scamper: add bottle.

--- Library/Formula/scamper.rb
+++ Library/Formula/scamper.rb
@@ -5,6 +5,13 @@ class Scamper < Formula
   url 'http://www.caida.org/tools/measurement/scamper/code/scamper-cvs-20130824.tar.gz'
   sha1 'c3d5c5bb28fd39020a13d9d7be25e4e5ac0cb05e'
 
+  bottle do
+    cellar :any
+    sha1 '0e05a358e8d10328f1c57bad5d057d60744ddffe' => :mavericks
+    sha1 '47d663f01de9588396f03dce2e444fb58779e5cc' => :mountain_lion
+    sha1 '4e523642fbad47a6ac07474c2ed26b4468f79001' => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
