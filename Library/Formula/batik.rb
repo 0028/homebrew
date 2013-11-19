d542ab84ee50b15941ad8021920697d281edd93f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 15:13:02 +0000

    batik: add bottle.

--- Library/Formula/batik.rb
+++ Library/Formula/batik.rb
@@ -5,6 +5,12 @@ class Batik < Formula
   url 'http://www.us.apache.org/dist/xmlgraphics/batik/batik-1.7.zip'
   sha1 '69d974ab0a4e13cbdd649fa96785776563b73fcf'
 
+  bottle do
+    sha1 'e8c125cc23b3c1f6ea8282fc372c2fc3470c61e6' => :mavericks
+    sha1 '1404fcf6509f2648ca775a24f28e97ed41be9aa0' => :mountain_lion
+    sha1 '17a265ed78e2d56a1695235e055a060a60b3521d' => :lion
+  end
+
   def install
     doc.install Dir['docs/*']
 
