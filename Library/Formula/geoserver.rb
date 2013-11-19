c27abfa2187d89162858eedae80075441fd1b7e6
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 16:02:48 +0000

    geoserver: add bottle.

--- Library/Formula/geoserver.rb
+++ Library/Formula/geoserver.rb
@@ -5,6 +5,12 @@ class Geoserver < Formula
   url 'http://downloads.sourceforge.net/project/geoserver/GeoServer/2.4.1/geoserver-2.4.1-bin.zip'
   sha1 '1f33e16c14bc0bceea1ffa74a0d528dd832de611'
 
+  bottle do
+    sha1 'e75e428a6a448ab40667f33112a4af34dac8e98d' => :mavericks
+    sha1 '16d37375286f6c1a3077e0372d2f6f5c190e2567' => :mountain_lion
+    sha1 '6e3ffeefbc51fc3a59b57cc726cdaebb5c5a4607' => :lion
+  end
+
   def install
     libexec.install Dir['*']
     (bin/'geoserver').write <<-EOS.undent
