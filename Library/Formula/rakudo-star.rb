73b31483c00e56451d2ba70cf96f7611ffd5e99f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 18:30:35 +0000

    rakudo-star: add bottle.

--- Library/Formula/rakudo-star.rb
+++ Library/Formula/rakudo-star.rb
@@ -3,6 +3,12 @@ require 'formula'
 class RakudoStar < Formula
   homepage 'http://rakudo.org/'
   url 'http://rakudo.org/downloads/star/rakudo-star-2013.10.tar.gz'
+  bottle do
+    sha1 '7b18490c2d8b5ba9b58f6b9cd901d98416bd6ea8' => :mavericks
+    sha1 '13cfa442dca408fe6d67f3ac917af15b85aceff4' => :mountain_lion
+    sha1 '1bf5dd5f7d862df7639755d5010769a12790790e' => :lion
+  end
+
   sha256 'cbc6c8b976d19d351eaf1fb0257e971a534c385252ed50b59b41b2459245e4cf'
 
   conflicts_with 'parrot'
