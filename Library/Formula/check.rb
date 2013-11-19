41d77f946e5e292b38b31bb34e1ae770fcb8fcfb
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 20:34:50 +0000

    check: add bottle.

--- Library/Formula/check.rb
+++ Library/Formula/check.rb
@@ -5,6 +5,13 @@ class Check < Formula
   url 'http://downloads.sourceforge.net/project/check/check/0.9.11/check-0.9.11.tar.gz'
   sha1 '84b5af72dd49df4ac837645d117ce9126535d549'
 
+  bottle do
+    cellar :any
+    sha1 '23ab6d9dc64968722c7b9a901724972a93605643' => :mavericks
+    sha1 'bcf3f1fb67390a77f5e4c3e00d82e5ebdd511377' => :mountain_lion
+    sha1 '8066831645bc7b98687065436ee254bdaff8f7d4' => :lion
+  end
+
   option :universal
 
   def install
