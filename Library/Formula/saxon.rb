24967c0b1cba943c1a56837f416b964541f4c066
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 16:02:48 +0000

    saxon: add bottle.

--- Library/Formula/saxon.rb
+++ Library/Formula/saxon.rb
@@ -6,6 +6,12 @@ class Saxon < Formula
   sha1 'a8cde14c9af99b40e52624253cf19ac4b1c02f16'
   version '9.5.1.3'
 
+  bottle do
+    sha1 'cb0a3360d592d64b73d68e8f8748e0b5f29ba474' => :mavericks
+    sha1 'aeb87ed9082617615331a6e447a58ee24744d86a' => :mountain_lion
+    sha1 '3e968adcd338dd118e5692fa9d2986440fcd66fc' => :lion
+  end
+
   def install
     libexec.install Dir["*"]
     bin.write_jar_script libexec/'saxon9he.jar', 'saxon'
