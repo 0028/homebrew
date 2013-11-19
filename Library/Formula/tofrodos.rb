b094b42e7049320d124a27eb1978239967c99458
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 18:45:20 +0000

    tofrodos: add bottle.

--- Library/Formula/tofrodos.rb
+++ Library/Formula/tofrodos.rb
@@ -5,6 +5,13 @@ class Tofrodos < Formula
   url 'http://tofrodos.sourceforge.net/download/tofrodos-1.7.13.tar.gz'
   sha1 '665cff074a19030705eb80274f483f20aa24b38e'
 
+  bottle do
+    cellar :any
+    sha1 '0b202ee68e396d11f21b6198bd984d2f1e089c68' => :mavericks
+    sha1 '2704524ecac19cf5545c19e1ccb44e3fd511f35a' => :mountain_lion
+    sha1 '9bb1edbba57be03d4fc59c328da9f19bf3970685' => :lion
+  end
+
   def install
     cd 'src' do
       system "make"
