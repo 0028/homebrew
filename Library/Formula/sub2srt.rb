2d2db0de4046a1a7500fd9bf86c3e1e69a9ac76d
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 14:34:56 +0000

    sub2srt: add bottle.

--- Library/Formula/sub2srt.rb
+++ Library/Formula/sub2srt.rb
@@ -5,6 +5,13 @@ class Sub2srt < Formula
   url 'https://github.com/robelix/sub2srt/archive/0.5.4.tar.gz'
   sha1 '2aaaae905223b924a05978b3c52cad73d06828cc'
 
+  bottle do
+    cellar :any
+    sha1 'cee7d25b7a6c85dd7adc9fbf6a469e7b75737690' => :mavericks
+    sha1 '518e8d63eb98e03501e416d11f23266445e5f780' => :mountain_lion
+    sha1 '9023f1edbabaa8b4517c1af3bd1b8187cbb47ca1' => :lion
+  end
+
   def install
     bin.install 'sub2srt'
   end
