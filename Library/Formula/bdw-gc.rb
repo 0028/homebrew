0c843d741cffe91ca76e3dc7ba4252b3f4853bf9
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 18:45:20 +0000

    bdw-gc: add bottle.

--- Library/Formula/bdw-gc.rb
+++ Library/Formula/bdw-gc.rb
@@ -5,6 +5,13 @@ class BdwGc < Formula
   url 'http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc-7.2e.tar.gz'
   sha1 '3ad593c6d0ed9c0951c21a657b86c55dab6365c8'
 
+  bottle do
+    cellar :any
+    sha1 'ec70613b7da1d4b87bc4b052326d88fea78a5f1e' => :mavericks
+    sha1 'e490ec3beec1bcce9f62ba56cc6a188741ab3a9b' => :mountain_lion
+    sha1 'bde27d3b630e256d27a8477206f61fd8485bd559' => :lion
+  end
+
   option :universal
 
   def install
