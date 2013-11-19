90662f2627d254d076235efa6e20980aac9f4538
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 01:30:29 +0000

    cvs: add bottle.

--- Library/Formula/cvs.rb
+++ Library/Formula/cvs.rb
@@ -10,6 +10,13 @@ class Cvs < Formula
   url 'http://ftp.gnu.org/non-gnu/cvs/source/feature/1.12.13/cvs-1.12.13.tar.bz2'
   sha1 '93a8dacc6ff0e723a130835713235863f1f5ada9'
 
+  bottle do
+    cellar :any
+    sha1 '1d8b21d85b000fd693587edfca9035f9dbf3a983' => :mavericks
+    sha1 'e9928372c22c4e6eb515e0c0c0b0e3727464f02f' => :mountain_lion
+    sha1 'f6aa08e3248a4f15413c1f9b115774952d6596c0' => :lion
+  end
+
   keg_only :provided_by_osx if MacOS.version < :mavericks
 
   def patches
