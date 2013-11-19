7e51d9891f5364a28f2bded4804c3fb1b131f0bf
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 12:53:40 +0000

    plplot: add bottle.

--- Library/Formula/plplot.rb
+++ Library/Formula/plplot.rb
@@ -5,6 +5,12 @@ class Plplot < Formula
   url 'http://downloads.sourceforge.net/project/plplot/plplot/5.9.10%20Source/plplot-5.9.10.tar.gz'
   sha1 '10dbdfe41d71a9f51e4396729c001ed2baea22fe'
 
+  bottle do
+    sha1 '70691fcc29527a9a342be6e9249a639c26604c66' => :mavericks
+    sha1 'e73555fd025e63b10be9658995318718feea0b13' => :mountain_lion
+    sha1 '32ff958d7c4d7c3a19494edc64e027ae4b7e7ba9' => :lion
+  end
+
   # failing wxWidget includes on snow leopard
   depends_on :macos => :lion
   depends_on 'cmake' => :build
