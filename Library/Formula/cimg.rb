2b51085c2d6d1fc27a4637266b651549f8c5637c
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 16:02:48 +0000

    cimg: add bottle.

--- Library/Formula/cimg.rb
+++ Library/Formula/cimg.rb
@@ -5,6 +5,13 @@ class Cimg < Formula
   url 'http://downloads.sourceforge.net/cimg/CImg-1.5.7.zip'
   sha1 '399e332b982b4ca0bcb27ea4f6763fd84a3b8457'
 
+  bottle do
+    cellar :any
+    sha1 '511e7e73486c9127f3508f588986804b249f429f' => :mavericks
+    sha1 '5634486c1070ce748d01e2694c772fced641c9b2' => :mountain_lion
+    sha1 '6d0e29f39c0ae963df3a71a72be1d1c7a5a133ec' => :lion
+  end
+
   def install
     include.install 'CImg.h'
 
