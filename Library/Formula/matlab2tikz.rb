827fd2437986e958f469675548ed12eca876aa0f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-19 14:34:56 +0000

    matlab2tikz: add bottle.

--- Library/Formula/matlab2tikz.rb
+++ Library/Formula/matlab2tikz.rb
@@ -7,6 +7,13 @@ class Matlab2tikz < Formula
 
   head 'https://github.com/nschloe/matlab2tikz.git'
 
+  bottle do
+    cellar :any
+    sha1 'fcaa2171577efabe0475d999e9c579cb7561da0a' => :mavericks
+    sha1 'f5c2ce25e36076013b77db48000df5e62068766c' => :mountain_lion
+    sha1 'ca13966d599f649473549ec947fb678456a1ed92' => :lion
+  end
+
   def install
     (share/'matlab2tikz').install Dir['src/*']
   end
