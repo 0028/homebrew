f283ba397606b6671babcd27bf55bfa3e699476e
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-20 04:23:13 +0000

    apib: add bottle.

--- Library/Formula/apib.rb
+++ Library/Formula/apib.rb
@@ -5,6 +5,13 @@ class Apib < Formula
   url 'https://github.com/apigee/apib/archive/APIB_1_0.zip'
   sha1 '25d5b29bfab858e71ca166d29e9218bd39ba5cc6'
 
+  bottle do
+    cellar :any
+    sha1 'd6650808cda17885ef909e0a3d6aad638e2e5108' => :mavericks
+    sha1 'e7e024bd3d9229e1e14196004b54eebd01e76b1c' => :mountain_lion
+    sha1 'd028684fab16964c54cd4ee2c600073cc9222337' => :lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
