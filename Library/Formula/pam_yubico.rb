667977afda3ec992999ecc4d1aa75d2decd90a35
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-25 20:07:25 +0000

    pam_yubico: add bottle.

--- Library/Formula/pam_yubico.rb
+++ Library/Formula/pam_yubico.rb
@@ -5,6 +5,13 @@ class PamYubico < Formula
   url 'http://opensource.yubico.com/yubico-pam/releases/pam_yubico-2.14.tar.gz'
   sha1 '3dd6bddce69785c5d61ae0f64ba568e1a81498a1'
 
+  bottle do
+    cellar :any
+    sha1 '6d2cc5c4650960377140e489fba11ea7065eb7e3' => :mavericks
+    sha1 '4779e37daa3c5ec7bf2a532711984b06abe29cec' => :mountain_lion
+    sha1 '0bcacff6147d856f8c953b156d57e84d522a6ad6' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on :autoconf
   depends_on :automake
