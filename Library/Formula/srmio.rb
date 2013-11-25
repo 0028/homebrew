d52e5aed0172f4a6618a73089673d8f861239127
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-25 19:41:52 +0000

    srmio: add bottle.

--- Library/Formula/srmio.rb
+++ Library/Formula/srmio.rb
@@ -5,6 +5,13 @@ class Srmio < Formula
   url 'http://www.zuto.de/project/files/srmio/srmio-0.1.1~git1.tar.gz'
   sha1 '0db685d6046fca38ad64df05840d01b5f3b27499'
 
+  bottle do
+    cellar :any
+    sha1 '4392664dccce8f72ccc6fd0038c56e32ed4d8e27' => :mavericks
+    sha1 '5afd310a96cc6f49ab4840d71ea8fe1bbfab1e86' => :mountain_lion
+    sha1 '099f49f52eda75eb834749a83d3f61632abb4817' => :lion
+  end
+
   head do
     url 'https://github.com/rclasen/srmio.git'
 
