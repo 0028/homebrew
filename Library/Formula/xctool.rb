a15428cc0a4ee44f99f2eb3cb65aa96358cf94cb
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-26 09:53:20 +0000

    xctool: add bottle.

--- Library/Formula/xctool.rb
+++ Library/Formula/xctool.rb
@@ -6,6 +6,12 @@ class Xctool < Formula
   sha1 '57e610081b781b19ec0c0f2ca81d897b708826f4'
   head 'https://github.com/facebook/xctool.git'
 
+  bottle do
+    sha1 'f049b19c3d580b9c071ce14d7778d5692ec17553' => :mavericks
+    sha1 'f54cb8cdcf32200596c8e32acbcd3c3656989560' => :mountain_lion
+    sha1 '8065cf0c17c812166c49d0422a0baded85cf7994' => :lion
+  end
+
   depends_on :xcode
   depends_on :macos => :lion
 
