ccdfe45b21245f33be3d72b722c631eeef80b664
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-26 15:06:46 +0000

    sendemail: add bottle.

--- Library/Formula/sendemail.rb
+++ Library/Formula/sendemail.rb
@@ -5,6 +5,13 @@ class Sendemail < Formula
   url 'http://caspian.dotconf.net/menu/Software/SendEmail/sendEmail-v1.56.tar.gz'
   sha1 '5c7c03ce60785c7b7695ec486c84d6e15704df38'
 
+  bottle do
+    cellar :any
+    sha1 'c7a2fb19f83cc9fc78536a2daee00ca35879417c' => :mavericks
+    sha1 '128d25deecf64871e9823ea1c61516571eed0df5' => :mountain_lion
+    sha1 '9c158bcec7396e8cd2495f56f3518901e932a593' => :lion
+  end
+
   def patches
     { :p1 => "https://raw.github.com/mogaal/sendemail/e785a6d284884688322c9b39c0f64e20a43ea825/debian/patches/fix_ssl_version.patch" }
   end
