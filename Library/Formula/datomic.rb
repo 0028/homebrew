bd63f51e08743f94e5e10bd3d56a26c70e4d22a6
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-27 16:56:59 +0000

    datomic: add 0.9.4324 bottle.

--- Library/Formula/datomic.rb
+++ Library/Formula/datomic.rb
@@ -6,6 +6,12 @@ class Datomic < Formula
   sha1 '8d2948e41e86b1dba9741a9eacdb7c27f4a025d5'
   version '0.9.4324'
 
+  bottle do
+    sha1 '1901b49c583e09b2da481745b6fc2b5f85585f15' => :mavericks
+    sha1 'b7f19b8522ed0c0fb0460915869a542ca26c4989' => :mountain_lion
+    sha1 'c27c12e89f9113d10f0b7bbbc3e583ea2077e465' => :lion
+  end
+
   def write_libexec_alias *script_names
     script_names.each do |script_name|
       alias_name = script_name == 'datomic' ? 'datomic' : "datomic-#{script_name}"
