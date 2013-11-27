263483f6b8db884383dd46d13a2f63a6a5165886
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-27 07:52:44 +0000

    open-mesh: add bottle.

--- Library/Formula/open-mesh.rb
+++ Library/Formula/open-mesh.rb
@@ -5,6 +5,13 @@ class OpenMesh < Formula
   url 'http://www.openmesh.org/fileadmin/openmesh-files/2.4/OpenMesh-2.4.tar.gz'
   sha1 '5fd3f27e8c5803caf003c752de2dffc88ae4f874'
 
+  bottle do
+    cellar :any
+    sha1 'c921aa2546d11452ae3b36b33f67bb01e5251389' => :mavericks
+    sha1 '9950f1d4db97ef707bd8d2149c54bbdba179b7da' => :mountain_lion
+    sha1 '4bdb689653dac8ace49a4944baa4447318026071' => :lion
+  end
+
   head 'http://openmesh.org/svnrepo/OpenMesh/trunk/', :using => :svn
 
   depends_on 'cmake' => :build
