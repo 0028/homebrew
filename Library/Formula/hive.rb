61640f23c9a829e04f92ae36c9bedb0db3df9a54
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-27 03:52:24 +0000

    hive: add bottle.

--- Library/Formula/hive.rb
+++ Library/Formula/hive.rb
@@ -5,6 +5,12 @@ class Hive < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=hive/hive-0.12.0/hive-0.12.0-bin.tar.gz'
   sha1 '488f625a859facc89312320ef7e9802f1cc2531e'
 
+  bottle do
+    sha1 '03fe28faa885ff46ccffeb27e7f8e188d806cea8' => :mavericks
+    sha1 'c87cceded037a298fc7ed9440050e1484416bd1f' => :mountain_lion
+    sha1 'e97fcc5ef2a22257ac82e5a275bef21cd19b0dab' => :lion
+  end
+
   depends_on 'hadoop'
   def install
     rm_f Dir["bin/*.bat"]
