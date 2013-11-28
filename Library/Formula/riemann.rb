562e234dbe85be8e9750c51a7bebda84f093f3ff
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-28 01:37:02 +0000

    riemann: add 0.2.4 bottle.

--- Library/Formula/riemann.rb
+++ Library/Formula/riemann.rb
@@ -5,6 +5,12 @@ class Riemann < Formula
   url 'http://aphyr.com/riemann/riemann-0.2.4.tar.bz2'
   sha1 '465965eb1c82f65872b85e685c44111a9af9f794'
 
+  bottle do
+    sha1 '11f9f613fa835b768206efeef35605e3ae22fa09' => :mavericks
+    sha1 '3639485f4c7838a3ff83851da43145be73919739' => :mountain_lion
+    sha1 '600aa0cd07828f4d6aea7e61f19e5bc38ee36877' => :lion
+  end
+
   def shim_script
     <<-EOS.undent
       #!/bin/bash
