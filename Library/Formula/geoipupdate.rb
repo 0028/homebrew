399dd7039a106a57904d8bd981ea8d081a2a900c
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-28 02:48:10 +0000

    geoipupdate: add 2.0.0 bottle.

--- Library/Formula/geoipupdate.rb
+++ Library/Formula/geoipupdate.rb
@@ -6,6 +6,12 @@ class Geoipupdate < Formula
   sha1 'd3c90ad9c9ad5974e8a5a30c504e7827978ddea7'
   head 'https://github.com/maxmind/geoipupdate.git'
 
+  bottle do
+    sha1 '44976c619fa5e3da65d2206981eb558a6c09918d' => :mavericks
+    sha1 'c426f504c59f58f3522d588a558709db0df78a7d' => :mountain_lion
+    sha1 'f031bc61fa7a11188255c75840076d7f06700163' => :lion
+  end
+
   if build.head?
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
