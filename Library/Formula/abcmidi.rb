d070b62ed4590c933fe689688e2ec81589c67c3a
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-29 02:26:45 +0000

    abcmidi: add 2013-09-15 bottle.

--- Library/Formula/abcmidi.rb
+++ Library/Formula/abcmidi.rb
@@ -6,6 +6,13 @@ class Abcmidi < Formula
   version '2013-09-15'
   sha1 '3e743b2a89404ad5a7d7f51c48cc2a32ba50654b'
 
+  bottle do
+    cellar :any
+    sha1 'b6a7115b86a85bca57f1849804aef3886f162f7b' => :mavericks
+    sha1 '43003c7b5dec60ee91e875bc95e7e1a556ad5bab' => :mountain_lion
+    sha1 '0e7719cba484e3e9008a028e2a416b329cef828d' => :lion
+  end
+
   def install
     # configure creates a "Makefile" file. A "makefile" file already exist in
     # the tarball. On case-sensitive file-systems, the "makefile" file won't
