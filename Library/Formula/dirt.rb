a0bedce06186086f42da11cd8d5e57305bacc68d
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-29 14:35:05 +0000

    dirt: add 1.0 bottle.

--- Library/Formula/dirt.rb
+++ Library/Formula/dirt.rb
@@ -6,6 +6,13 @@ class Dirt < Formula
   url 'https://github.com/yaxu/Dirt/archive/1.0.tar.gz'
   sha1 '853d6a80bf77ebceabd25411ea01568d9acb3362'
 
+  bottle do
+    cellar :any
+    sha1 '365a1c5f99ae31aff4c1afedcd237a81d58eab9f' => :mavericks
+    sha1 '0c52c927687c105c8dc1484452d918c3cf8de170' => :mountain_lion
+    sha1 '7975c151c9bdcc8fd0d67850f471851a47938f3f' => :lion
+  end
+
   depends_on 'jack'
   depends_on 'liblo'
 
