b1dc1dfa195d6a2fd33efe0e076378991c38b3aa
BrewTestBot <brew-test-bot@googlegroups.com>
2013-11-29 08:31:48 +0000

    sbt: add 0.13.0 bottle.

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -6,6 +6,12 @@ class Sbt < Formula
   version '0.13.0'
   sha1 'b78a29b8db7ae0fc90a7890a84a4617975e1d450'
 
+  bottle do
+    sha1 'fc79cb7c91b873d5be33ed79b7bb9d3b9265c466' => :mavericks
+    sha1 '1bd115a0c071b9027bb094665c5f81eb6044c19f' => :mountain_lion
+    sha1 'c35206ca5a72d3d5ffd9fb2480791e550c182efb' => :lion
+  end
+
   devel do
     url 'http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.1-RC5/sbt-launch.jar'
     version '0.13.1-RC5'
