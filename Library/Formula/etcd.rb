ddbf4dea99eb6f599a2b83d88c041edb6860c94e
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-02 02:01:09 +0000

    etcd: add 0.1.2 bottle.

--- Library/Formula/etcd.rb
+++ Library/Formula/etcd.rb
@@ -6,6 +6,12 @@ class Etcd < Formula
   sha1 'f6a644ec0ff30681b816bff192a881ed65d0c9e5'
   head 'https://github.com/coreos/etcd.git'
 
+  bottle do
+    sha1 '8725d957ee466475d44464a655d5ce66d4a9b255' => :mavericks
+    sha1 '7e413d07d3492d9f7b9fae32c9a973850ecf0ff8' => :mountain_lion
+    sha1 '8f606fb56f0529a64f16b6c49f7f58bd1e203983' => :lion
+  end
+
   depends_on 'go' => :build
 
   fails_with :clang do
