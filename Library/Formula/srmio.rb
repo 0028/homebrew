5711f355aa17ae22310bcff835ca2ec7c43c03f8
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-04 06:16:56 +0000

    srmio: add 0.1.1~git1 bottle.

--- Library/Formula/srmio.rb
+++ Library/Formula/srmio.rb
@@ -6,6 +6,13 @@ class Srmio < Formula
   sha1 '0db685d6046fca38ad64df05840d01b5f3b27499'
   version '0.1.1~git1'
 
+  bottle do
+    cellar :any
+    sha1 'f158ec8e0555b0e92c94ed51e582702d8bdb01c8' => :mavericks
+    sha1 '2ed445e9d073f8360c010a6f00a3e7284fae42e3' => :mountain_lion
+    sha1 '9084dac9180adcdab473414c0d5575ee110e33db' => :lion
+  end
+
   head do
     url 'https://github.com/rclasen/srmio.git'
 
