8aad9782e3b42bca759dcefdd56c97229df47b5e
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-04 21:12:54 +0000

    yacas: add 1.3.3 bottle.

--- Library/Formula/yacas.rb
+++ Library/Formula/yacas.rb
@@ -5,6 +5,12 @@ class Yacas < Formula
   url 'http://yacas.sourceforge.net/backups/yacas-1.3.3.tar.gz'
   sha1 '749952102f5321d62788be8ae459c1a67078b33d'
 
+  bottle do
+    sha1 '0c526ccb623e64326e08cf187f86ac33817c566e' => :mavericks
+    sha1 'e9b240c8ff4f232b2570615171c9111561574122' => :mountain_lion
+    sha1 'ed5d3eca9bcce67019eda3e73c92ff9aca63bc57' => :lion
+  end
+
   option "with-server", "Build the network server version"
 
   def install
