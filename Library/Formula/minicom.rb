25cde2e12885c514340a563c087457110229c16d
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-04 04:46:50 +0000

    minicom: add 2.6.2 bottle.

--- Library/Formula/minicom.rb
+++ Library/Formula/minicom.rb
@@ -5,6 +5,12 @@ class Minicom < Formula
   url 'http://ftp.de.debian.org/debian/pool/main/m/minicom/minicom_2.6.2.orig.tar.gz'
   sha1 'e4267f89e4046c4e3d28cad5aa643edb1de4169a'
 
+  bottle do
+    sha1 'b2359564a6b5a55a522e6c7b2ba9623a0079acba' => :mavericks
+    sha1 '0f6a358d7fc4f89ecc506da9d5360a3da763b81d' => :mountain_lion
+    sha1 '2f3a1950514da18c72051673d6eba008bcc663f4' => :lion
+  end
+
   def install
     # There is a silly bug in the Makefile where it forgets to link to iconv. Workaround below.
     ENV['LIBS'] = '-liconv'
