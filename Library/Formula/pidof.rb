72f638caa00417c8fba30d60d4e796b379f04078
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-05 05:03:33 +0000

    pidof: update 0.1.4 bottle.

--- Library/Formula/pidof.rb
+++ Library/Formula/pidof.rb
@@ -7,11 +7,10 @@ class Pidof < Formula
   version '0.1.4'
 
   bottle do
-    # cellar :any
-    revision 1
-    sha1 'da148135814b86ac578de9d0b19f1f2deda0a312' => :mountain_lion
-    sha1 'e818806883c83e6315d68fc93a37071c90c9f2ea' => :lion
-    sha1 '6b950109d55d192d6e95dd23912ecb13ffa2e94f' => :snow_leopard
+    cellar :any
+    sha1 'fcc63653f62a74e5a536ad53fca486c0f9069d6f' => :mavericks
+    sha1 '21f3f668918bd3e903811de70466faeb73b73ff5' => :mountain_lion
+    sha1 '49d8865d855721a2d9beec53f455bcbf72e4aa82' => :lion
   end
 
   def install
