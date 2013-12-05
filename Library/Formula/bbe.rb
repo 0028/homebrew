b04cd43934d45d89a8a57d37ba81d257ccab78bf
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-05 14:46:48 +0000

    bbe: add 0.2.2 bottle.

--- Library/Formula/bbe.rb
+++ Library/Formula/bbe.rb
@@ -5,6 +5,13 @@ class Bbe < Formula
   url 'http://downloads.sourceforge.net/project/bbe-/bbe/0.2.2/bbe-0.2.2.tar.gz'
   sha1 '42d5b47d607a9633fb49e7d39e2aebfb7bb89c05'
 
+  bottle do
+    cellar :any
+    sha1 'f919e34088509b98e0396422efb7aa1dad1b8f87' => :mavericks
+    sha1 '8cdf13599103f5a82fd772f2ef14b3b93a3b4c84' => :mountain_lion
+    sha1 '55243c259b08b2d79683fb794d1a5d0255a31e33' => :lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
