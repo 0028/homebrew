ca3bb801598a547805e689b531cf915b164189b0
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-05 04:27:47 +0000

    hercules: add 3.09 bottle.

--- Library/Formula/hercules.rb
+++ Library/Formula/hercules.rb
@@ -5,6 +5,12 @@ class Hercules < Formula
   url 'http://downloads.hercules-390.eu/hercules-3.09.tar.gz'
   sha1 '5cbe89ec214de00b90e3077c27db636a7d2607f5'
 
+  bottle do
+    sha1 '7f59090394bee1bbece34d111f21e0dcac85a97b' => :mavericks
+    sha1 '3e8c664a725a4ec67d19f30887cb6452fd29b3a3' => :mountain_lion
+    sha1 '86a1f430182dd9179969fb89275875117ad8aaae' => :lion
+  end
+
   depends_on 'gawk'
 
   def install
