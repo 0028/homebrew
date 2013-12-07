fc7c7da59b1e9b437b290d23150b54e9c853ec84
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-07 23:59:35 +0000

    storm: add 0.9.0.1 bottle.

--- Library/Formula/storm.rb
+++ Library/Formula/storm.rb
@@ -5,6 +5,13 @@ class Storm < Formula
   url 'https://dl.dropboxusercontent.com/s/dj86w8ojecgsam7/storm-0.9.0.1.zip'
   sha1 '230abcc15a9e1358442429fd1c856dc12a3be920'
 
+  bottle do
+    cellar :any
+    sha1 '4c9b26d5798f06c152438a514231505503b21b16' => :mavericks
+    sha1 '07c34670842e8b27e6bec725e20571676d50802f' => :mountain_lion
+    sha1 'e6ea006bb7749090c9fc135ffd2572dd17b2aa59' => :lion
+  end
+
   def install
     libexec.install Dir['*']
     bin.install_symlink libexec/"bin/storm"
