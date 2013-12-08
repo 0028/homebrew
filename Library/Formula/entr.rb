b3bd396d7d809cdb5415ad9c2565f8bde4bc805b
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-08 00:42:29 +0000

    entr: add 2.2 bottle.

--- Library/Formula/entr.rb
+++ Library/Formula/entr.rb
@@ -5,6 +5,13 @@ class Entr < Formula
   url 'http://entrproject.org/code/entr-2.2.tar.gz'
   sha1 '71eedf5d9397a08a231f0ab400f5aeec4f77571b'
 
+  bottle do
+    cellar :any
+    sha1 '8bc89f26f526b30dea9e5ceb1057e6cc05890d2d' => :mavericks
+    sha1 '0cafd5e06e148aaf32a0d6bd5d3761ca79fee709' => :mountain_lion
+    sha1 '711d337f92345e7107b92bca5fec93a6ff4ccd7f' => :lion
+  end
+
   def install
     ENV['PREFIX'] = prefix
     ENV['MANPREFIX'] = man
