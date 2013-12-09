583fd4e42e399f8012fcaf6dac9bc4fa3edb0495
Ian Gable <igable@uvic.ca>
2013-12-09 13:40:32 -0800

    web100clt: 3.6.5.2 - update to build on 10.9
    With the new default secure strlcpy/strlcat functions in 10.9 web100clt
    no longer builds on 10.9. In order to get it to build we need to set
    CFLAG -D_FORTIFY_SOURCE=0. This should be a temporary fix until it
    can be fixed upstream.
    
    Upstream issue:
    http://code.google.com/p/perfsonar-ps/issues/detail?id=798

--- Library/Formula/web100clt.rb
+++ Library/Formula/web100clt.rb
@@ -1,11 +1,17 @@
 require 'formula'
 
 class Web100clt < Formula
-  homepage 'http://www.internet2.edu/performance/ndt/'
+  homepage 'http://software.internet2.edu/ndt/'
   url 'http://software.internet2.edu/sources/ndt/ndt-3.6.5.2.tar.gz'
   sha1 '533a7dbb1b660a0148a0e295b481f63ab9ecb8f7'
 
   def install
+    # avoid problem with default default secure strlcpy/strlcat
+    # functions in 10.9
+    if MacOS.version >= :mavericks
+      ENV.append 'CFLAGS', '-D_FORTIFY_SOURCE=0'
+    end
+
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -14,7 +20,7 @@ class Web100clt < Formula
     # we only want to build the web100clt client so we need
     # to change to the src directory before installing.
     cd 'src' do
-      system "make install"
+      system "make CFLAGS=#{ENV.cflags} install"
     end
 
     cd 'doc' do
