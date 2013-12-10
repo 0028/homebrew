f6d6e54e8ac73a934fb1d049792c3f7beb173823
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-10 03:46:02 +0000

    gifify: add 1.0 bottle.

--- Library/Formula/gifify.rb
+++ Library/Formula/gifify.rb
@@ -7,6 +7,13 @@ class Gifify < Formula
 
   head 'https://github.com/jclem/gifify.git'
 
+  bottle do
+    cellar :any
+    sha1 '393172ffc3f087308987aa84a4e7a94283c81e2d' => :mavericks
+    sha1 '09427d34c8b3622c3b20b92ba8b20cc5ba5651b1' => :mountain_lion
+    sha1 '7bb319827d49a50edfc59c656f2aceb005158dde' => :lion
+  end
+
   depends_on 'ffmpeg'
   depends_on 'imagemagick'
 
