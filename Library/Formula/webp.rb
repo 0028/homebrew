40b650b68c60e0f143e0c0c8e9fa8ce782e6897d
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-10 22:27:26 +0000

    webp: add 0.3.1 bottle.

--- Library/Formula/webp.rb
+++ Library/Formula/webp.rb
@@ -5,6 +5,13 @@ class Webp < Formula
   url 'https://webp.googlecode.com/files/libwebp-0.3.1.tar.gz'
   sha1 '52e3d2b6c0b80319baa33b8ebed89618769d9dd8'
 
+  bottle do
+    cellar :any
+    sha1 '8efbe6da417e05641a77eec1301f1eaa5d8771ca' => :mavericks
+    sha1 '00ce5eb8133fbf636857d165f52f052bda1bc09a' => :mountain_lion
+    sha1 'affdaf6d516f7c413735ae906a169f5754a77cc8' => :lion
+  end
+
   option :universal
 
   depends_on :libpng
