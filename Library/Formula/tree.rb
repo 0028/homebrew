12a6019b0aef9b721a8f98684749c1f0f6bf5c10
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-10 22:44:53 +0000

    tree: add 1.6.0 bottle.

--- Library/Formula/tree.rb
+++ Library/Formula/tree.rb
@@ -5,6 +5,13 @@ class Tree < Formula
   url 'http://mama.indstate.edu/users/ice/tree/src/tree-1.6.0.tgz'
   sha1 '350f851f68859a011668362dd0e7ee81fd1b713a'
 
+  bottle do
+    cellar :any
+    sha1 '5a009150fd5a821283a344718a924807b7039092' => :mavericks
+    sha1 '711431252eef19495787000645249908d0c802af' => :mountain_lion
+    sha1 '23bfa29eae246afd0cdeae1f3174e6d03f449476' => :lion
+  end
+
   def install
     ENV.append 'CFLAGS', '-fomit-frame-pointer'
     objs = 'tree.o unix.o html.o xml.o hash.o color.o strverscmp.o'
