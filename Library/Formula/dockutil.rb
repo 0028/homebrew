44cad435aae01ce94022b40afe27643be2190dbe
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-10 17:49:38 +0000

    dockutil: add 1.1.4 bottle.

--- Library/Formula/dockutil.rb
+++ Library/Formula/dockutil.rb
@@ -6,6 +6,12 @@ class Dockutil < Formula
   sha1 'bfb8db52d30dc254a76a34d8434d66b2bf8792e7'
   version '1.1.4'
 
+  bottle do
+    sha1 '62edca6daa6997b0fda341b75efd353394fbec65' => :mavericks
+    sha1 '3b92c9f1d4122f8d0d9789ec6a1e5d939b54dbb0' => :mountain_lion
+    sha1 '378ab458c324137c7eca9a93c22197b8adfe1a16' => :lion
+  end
+
   depends_on :python
 
   resource 'plistlib' do
