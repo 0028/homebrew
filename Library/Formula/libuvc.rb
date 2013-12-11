fc013cc9c7774485d64cc5ea9ed8f6a7a7346c27
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-11 08:57:08 +0000

    libuvc: add 0.0.2 bottle.

--- Library/Formula/libuvc.rb
+++ Library/Formula/libuvc.rb
@@ -5,6 +5,12 @@ class Libuvc < Formula
   url 'https://github.com/ktossell/libuvc/archive/v0.0.2.tar.gz'
   sha1 'e0d57007715eaf36c5ebeb8d348488984c681041'
 
+  bottle do
+    sha1 '96ec54d1658916548f5d4f5abc320834fefc6c91' => :mavericks
+    sha1 'f0b8ab1852fa7f004c3578b2bd3075db039cc09d' => :mountain_lion
+    sha1 'f77761e6b63281a37fc4d409f1ae2f60f7a5cc33' => :lion
+  end
+
   depends_on 'libusb'
   depends_on 'cmake' => :build
 
