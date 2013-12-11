e0b6a112d821f34df2e21e25ac7901e17e6acf0d
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-11 05:29:44 +0000

    ocrad: add 0.22 bottle.

--- Library/Formula/ocrad.rb
+++ Library/Formula/ocrad.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Ocrad < Formula
   homepage 'http://www.gnu.org/software/ocrad/'
   url 'http://ftpmirror.gnu.org/ocrad/ocrad-0.22.tar.lz'
+  bottle do
+    cellar :any
+    sha1 'b3f2e3e1aa613aac016c3dfd129661c5d20051ec' => :mavericks
+    sha1 'f85234cff1fc207cbec30ff924279dbbeafa0216' => :mountain_lion
+    sha1 '5a38e19e3f0eaeae56e5e0e3461945dc2e465b56' => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/ocrad/ocrad-0.22.tar.lz'
   sha1 'cfb3284b9d7387af80bcd795c093624eb40f6d13'
 
