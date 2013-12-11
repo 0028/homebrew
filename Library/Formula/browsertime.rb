f1d744ad3a06524d3a5c1c841f0b68d4f38415d0
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-11 14:29:36 +0000

    browsertime: add 0.4 bottle.

--- Library/Formula/browsertime.rb
+++ Library/Formula/browsertime.rb
@@ -5,6 +5,13 @@ class Browsertime < Formula
   url 'https://github.com/tobli/browsertime/releases/download/browsertime-0.4/browsertime-0.4.zip'
   sha1 'b62d3b5eff94a0ab04c8359dfd36086f7573c8ac'
 
+  bottle do
+    cellar :any
+    sha1 '281b1458fe8df85a88744032edd5bd7cef358da7' => :mavericks
+    sha1 'a99504df8ddd93069cfed406f422f36894082627' => :mountain_lion
+    sha1 'fad6dc7929eef43f02ac9e0546988e0503495cb0' => :lion
+  end
+
   depends_on 'chromedriver'
 
   def install
