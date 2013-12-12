53e3eb80d70eedc9add1c3b4ec83f72f986d6200
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-12 16:48:24 +0000

    plantuml: add 7987 bottle.

--- Library/Formula/plantuml.rb
+++ Library/Formula/plantuml.rb
@@ -5,6 +5,12 @@ class Plantuml < Formula
   url 'http://sourceforge.net/projects/plantuml/files/plantuml.7987.jar'
   sha1 '69beeb97a982b71fefd5ec5637de18b498910b13'
 
+  bottle do
+    sha1 '5161b1ee263aa769be3966ab65495ce107e69a95' => :mavericks
+    sha1 '0ce84f3c3060a24822e82bb48ddbc8240e3e76cd' => :mountain_lion
+    sha1 '6c9755e0926746e3b6b0e5f9d1fca3d9801dc64a' => :lion
+  end
+
   depends_on 'graphviz'
 
   def install
