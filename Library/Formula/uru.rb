e0d777b4748919e2e1dd71b24a8aef4a65cd3bc0
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-12 16:49:10 +0000

    uru: add 0.6.4 bottle.

--- Library/Formula/uru.rb
+++ Library/Formula/uru.rb
@@ -6,6 +6,13 @@ class Uru < Formula
   version '0.6.4'
   sha1 'fc99fc0867c1d236155c624b4d88636f4c1e6bbe'
 
+  bottle do
+    cellar :any
+    sha1 'cf1062b2a6f25d75736b7083a17873747c32fd29' => :mavericks
+    sha1 'd911481b7b8fdb3c1a48d7ea5ab3c72b777cf0e1' => :mountain_lion
+    sha1 '945eda44c8a99eb5cc52d664d012e64ef17c4223' => :lion
+  end
+
   def install
     bin.install Dir['*']
   end
