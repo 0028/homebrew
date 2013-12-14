780d50c43883709f11d929c35c376e138f907d0b
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-14 11:49:14 +0000

    iptux: add 0.6.1 bottle.

--- Library/Formula/iptux.rb
+++ Library/Formula/iptux.rb
@@ -5,6 +5,12 @@ class Iptux < Formula
   url 'https://github.com/iptux-src/iptux/archive/v0.6.1.tar.gz'
   sha1 '634c7e613bb83a18dfaaf6fc965202a390790921'
 
+  bottle do
+    sha1 '100c130fcbcf378d1c80953df8af447396e66880' => :mavericks
+    sha1 '8525e8e321cb2b022294b31a2262ad9c6e17a05d' => :mountain_lion
+    sha1 'b2dd9eddff389ce6a7929b83aa86b624c9d72642' => :lion
+  end
+
   depends_on :x11
   depends_on 'gettext'
   depends_on 'gtk+'
