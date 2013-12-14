73a03526b45d47edac6b177f24356c318637c125
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-14 14:13:23 +0000

    cpuminer: add 2.2.3 bottle.

--- Library/Formula/cpuminer.rb
+++ Library/Formula/cpuminer.rb
@@ -6,6 +6,13 @@ class Cpuminer < Formula
   sha1 '985ad94db5bd6579dff42933f7fe99f1ba45b364'
   head 'https://github.com/pooler/cpuminer.git'
 
+  bottle do
+    cellar :any
+    sha1 'c95d7c2cad2df0c1401fd6587723da238fc36ea1' => :mavericks
+    sha1 'b44c979928924a8ed1e6072493c4742722c0e547' => :mountain_lion
+    sha1 '4a3be6cf2caf0e26335268c173d33ceb1bc320c4' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'autoconf' => :build if build.head?
   depends_on 'automake' => :build if build.head?
