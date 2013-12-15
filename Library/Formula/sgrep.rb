f437dc8005291161a7d7189f7acdd17267032814
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-15 20:47:14 +0000

    sgrep: add 1.94a bottle.

--- Library/Formula/sgrep.rb
+++ Library/Formula/sgrep.rb
@@ -5,6 +5,12 @@ class Sgrep < Formula
   url 'ftp://ftp.cs.helsinki.fi/pub/Software/Local/Sgrep/sgrep-1.94a.tar.gz'
   sha1 '4f6815d212ac6e1ed4eb2720f9b28963d1d0e99d'
 
+  bottle do
+    sha1 'fc2052380feaaa3f0a3a96516e464edfe43e3afd' => :mavericks
+    sha1 '7b33dd0ac45611338b16db63b8529a228c4cfb07' => :mountain_lion
+    sha1 '3e18d2e3173b1f870108095c76763b5b8f4f621a' => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
