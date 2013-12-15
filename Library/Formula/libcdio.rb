b84b95a6ba41d2683e49000fcb6398cc88d0fb47
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-15 19:05:16 +0000

    libcdio: add 0.91 bottle.

--- Library/Formula/libcdio.rb
+++ Library/Formula/libcdio.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Libcdio < Formula
   homepage 'http://www.gnu.org/software/libcdio/'
   url 'http://ftpmirror.gnu.org/libcdio/libcdio-0.91.tar.gz'
+  bottle do
+    cellar :any
+    sha1 'e516de177490b9af5810f7e474bc6205c6632f6d' => :mavericks
+    sha1 '5aa9040d422eb970e2f0c179b0dab513ab084d8c' => :mountain_lion
+    sha1 '557cbdbe74f93d2ddbf21867af6d88e301c0e908' => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/libcdio/libcdio-0.91.tar.gz'
   sha1 '898ae74c3cb78ea1afbb0a387fd0eedee999327b'
 
