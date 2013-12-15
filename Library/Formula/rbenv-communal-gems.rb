f0e0ebaabbc52bcf7673934f8f3141c53fd532b0
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-15 20:54:45 +0000

    rbenv-communal-gems: add 1.0.1 bottle.

--- Library/Formula/rbenv-communal-gems.rb
+++ Library/Formula/rbenv-communal-gems.rb
@@ -5,6 +5,13 @@ class RbenvCommunalGems < Formula
   url 'https://github.com/tpope/rbenv-communal-gems/archive/v1.0.1.tar.gz'
   sha1 '39360238e89f2d723409c3de2a4132f561de7bb9'
 
+  bottle do
+    cellar :any
+    sha1 '6c7f4f0ede95dae7c9e7fff466282c9c16eda673' => :mavericks
+    sha1 'f320c767abb16dc1ad13896eedcc3aa7d9aaf131' => :mountain_lion
+    sha1 'a6c59205a61f4b3212243c338993a0b531ec2412' => :lion
+  end
+
   depends_on 'rbenv'
 
   def install
