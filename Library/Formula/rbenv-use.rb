6f35fa66470f9cb08de7606d7bb7a77061f14b70
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-15 10:50:33 +0000

    rbenv-use: add 1.0.0 bottle.

--- Library/Formula/rbenv-use.rb
+++ Library/Formula/rbenv-use.rb
@@ -5,6 +5,13 @@ class RbenvUse < Formula
   url 'https://github.com/rkh/rbenv-use/archive/v1.0.0.tar.gz'
   sha1 '39beb889704bc51213665fd994417ab0668c5f83'
 
+  bottle do
+    cellar :any
+    sha1 '93a25b86a399df0a067fa80a1da912d1046ba66b' => :mavericks
+    sha1 '7827db3b1e72c0d613b2e34c98e67d6af833355b' => :mountain_lion
+    sha1 '7a10ed8a9fa799404ba7b0df71f8de8be11d4977' => :lion
+  end
+
   depends_on 'rbenv'
   depends_on 'rbenv-whatis'
 
