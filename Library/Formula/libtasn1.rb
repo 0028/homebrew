08fafcc4af98fbcc3e8cede46aff47a0ecf58d7c
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-15 19:05:16 +0000

    libtasn1: add 3.4 bottle.

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Libtasn1 < Formula
   homepage 'http://www.gnu.org/software/libtasn1/'
   url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-3.4.tar.gz'
+  bottle do
+    cellar :any
+    sha1 '18735144aefb4a1702c7a76f23ca0b154e7f642e' => :mavericks
+    sha1 '2c0367afc00f7a8b7f539ee6f795fa695d75267b' => :mountain_lion
+    sha1 'b9b880201486608670707fab232c2c891836c544' => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/libtasn1/libtasn1-3.4.tar.gz'
   sha1 'f0e95f58b3c37405d48b91a585b517a4134586a9'
 
