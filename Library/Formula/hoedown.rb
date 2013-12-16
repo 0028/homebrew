13dec52e99f5262a29af7e3410b35c1dd7d9e12f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-16 13:57:02 +0000

    hoedown: add 2.0.0 bottle.

--- Library/Formula/hoedown.rb
+++ Library/Formula/hoedown.rb
@@ -5,6 +5,13 @@ class Hoedown < Formula
   url 'https://github.com/hoedown/hoedown/archive/2.0.0.tar.gz'
   sha1 '51cd3f80b4798fb305b66bbfce4cfa7da9f1b775'
 
+  bottle do
+    cellar :any
+    sha1 '4332db5d98a8f8001e187fb1308ae0a031aff0e8' => :mavericks
+    sha1 '35c74b5aa91966c9f3ee9c530842a982ed3d12ab' => :mountain_lion
+    sha1 '426b3f0c583f2e7677bb76a7cd60db006125901c' => :lion
+  end
+
   def install
     system 'make', 'hoedown'
     bin.install 'hoedown'
