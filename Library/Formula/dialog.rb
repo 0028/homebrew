f1aeae0e1c3a069109ffb7922864398df122a34f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-16 13:42:16 +0000

    dialog: add 1.2-20130928 bottle.

--- Library/Formula/dialog.rb
+++ Library/Formula/dialog.rb
@@ -5,6 +5,13 @@ class Dialog < Formula
   url 'ftp://invisible-island.net/dialog/dialog-1.2-20130928.tgz'
   sha1 '204d852856754817f5590f60ffaa1c07a8ed35ca'
 
+  bottle do
+    cellar :any
+    sha1 'aa68785155668e5b8a13e779b6dc8d08ab09c920' => :mavericks
+    sha1 'a33ea22d17a85151fb965a85e3ab82496dae69b2' => :mountain_lion
+    sha1 '6802fb21887ffb33738372371f83f094978ca298' => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--mandir=#{man}"
