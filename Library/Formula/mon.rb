e73084f8dfb6a6e9daf7b5b50bb08d90111c9e95
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-17 23:51:44 +0000

    mon: add 1.2.3 bottle.

--- Library/Formula/mon.rb
+++ Library/Formula/mon.rb
@@ -5,6 +5,13 @@ class Mon < Formula
   url 'https://github.com/visionmedia/mon/archive/1.2.3.tar.gz'
   sha1 '9c5013332b6ecccb6368b100e6aee377e35b5bb1'
 
+  bottle do
+    cellar :any
+    sha1 '142ce96f928a5cd7201bd7566c3592f49199739e' => :mavericks
+    sha1 'd44fc2e3beef51480bde662dd71b3da931f90def' => :mountain_lion
+    sha1 '211f1d03a908720ff64e2812b05d634ff2b2f279' => :lion
+  end
+
   def install
     system "make"
     bin.install 'mon'
