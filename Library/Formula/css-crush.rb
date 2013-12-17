664594e733ba909cd3a563dac8cf3de648f95197
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-17 23:51:44 +0000

    css-crush: add 2.0.1 bottle.

--- Library/Formula/css-crush.rb
+++ Library/Formula/css-crush.rb
@@ -5,6 +5,12 @@ class CssCrush < Formula
   url 'https://github.com/peteboere/css-crush/archive/v2.0.1.tar.gz'
   sha1 'dc91eda746938c0b876e47750177a70ccdb66568'
 
+  bottle do
+    sha1 '676c4ee0a6494029fc4a4772bf4ec86d1891d635' => :mavericks
+    sha1 '371a9080e24fc84f0ebc3a480b17fbc4c7ada5b6' => :mountain_lion
+    sha1 'd1b57dffb4e8854269323f4ddef1d7118517a5d1' => :lion
+  end
+
   def install
     libexec.install Dir['*']
     (bin+'csscrush').write <<-EOS.undent
