cff90d983bce8001c4822a8c0782adf92d479db1
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-17 23:51:44 +0000

    rbenv-gemset: add 0.5.0 bottle.

--- Library/Formula/rbenv-gemset.rb
+++ Library/Formula/rbenv-gemset.rb
@@ -7,6 +7,13 @@ class RbenvGemset < Formula
 
   head 'https://github.com/jf/rbenv-gemset.git'
 
+  bottle do
+    cellar :any
+    sha1 'c85b74ba12623038d996d898b14419c83161c9be' => :mavericks
+    sha1 'fe802691d7cf5a4a2e64dfa72ce8751c66ab4131' => :mountain_lion
+    sha1 '1425e68d9c8c937784a22c9e81c04e5e70a08df1' => :lion
+  end
+
   depends_on 'rbenv'
 
   def install
