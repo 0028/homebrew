6367e33c665ccc70ab4ee628e4b34b6cc3e53a3b
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-18 16:29:07 +0000

    quassel: add 0.9.1 bottle.

--- Library/Formula/quassel.rb
+++ Library/Formula/quassel.rb
@@ -7,6 +7,12 @@ class Quassel < Formula
 
   head 'https://github.com/quassel/quassel.git'
 
+  bottle do
+    sha1 '2ed6c5e2ca3bc942b7eb4226e99b0097814bf494' => :mavericks
+    sha1 'a610e2bab39f274b22b1e67ba6f5565b4fdedf59' => :mountain_lion
+    sha1 '2c49ac3b28a8401a4c1356d1e01ebeaf67a16599' => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'qt'
 
