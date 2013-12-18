ffa038ff82164c191144ede5982cc87c892887f8
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-18 02:08:44 +0000

    sqlcipher: add 3.0.1 bottle.

--- Library/Formula/sqlcipher.rb
+++ Library/Formula/sqlcipher.rb
@@ -5,6 +5,13 @@ class Sqlcipher < Formula
   url 'https://github.com/sqlcipher/sqlcipher/archive/v3.0.1.tar.gz'
   sha1 '9cec88e5a6e59058e675d37bbd4b0899689f9956'
 
+  bottle do
+    cellar :any
+    sha1 '0b3331ee381a45372667394af1c666017e6c8987' => :mavericks
+    sha1 '2f0479591573da397e972216454cbedfd45000d3' => :mountain_lion
+    sha1 'a5741ff811d8a789b9abd8e112451701e1379d15' => :lion
+  end
+
   head "https://github.com/sqlcipher/sqlcipher.git"
 
   keg_only "SQLCipher conflicts with the system and Homebrew SQLites."
