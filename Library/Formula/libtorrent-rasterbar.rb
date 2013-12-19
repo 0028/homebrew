7b838fd2e45196dc51b1dbe75ca9632b5738d2a7
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-19 05:27:32 +0000

    libtorrent-rasterbar: add 0.16.13 bottle.

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -5,6 +5,13 @@ class LibtorrentRasterbar < Formula
   url 'https://libtorrent.googlecode.com/files/libtorrent-rasterbar-0.16.13.tar.gz'
   sha1 '93a2098d77e9f36ff99966311be1e6cbb131e70f'
 
+  bottle do
+    cellar :any
+    sha1 '4fe37523c3c4d8dcd378e72360a129fcef549d7d' => :mavericks
+    sha1 'f16e4f5fedb07fdd4a7290a8e4d924117d56aaff' => :mountain_lion
+    sha1 '4a7e0f96c46548cc6dc94e936a802ed46f7b16f2' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'boost'
   depends_on 'openssl' if MacOS.version <= :snow_leopard # Needs a newer version of OpenSSL on Snow Leopard
