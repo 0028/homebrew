08e60560dd675170f5139435f26f4f6bb42aac08
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-19 15:31:22 +0000

    vert.x: add 2.0.2 bottle.

--- Library/Formula/vert.x.rb
+++ Library/Formula/vert.x.rb
@@ -5,6 +5,13 @@ class VertX < Formula
   url 'http://dl.bintray.com/vertx/downloads/vert.x-2.0.2-final.tar.gz'
   sha1 'fc8cce1d3fca9eeac90e15e6338f83c01ca1d44a'
 
+  bottle do
+    cellar :any
+    sha1 'bcf208982d225b6938c0b0eab7f2ea9353b2cf77' => :mavericks
+    sha1 'ee90a0d31d8be101b74e6b11c8c465074d28059a' => :mountain_lion
+    sha1 '997e0c052faf1cb1373d511f6c1d3c8b0fbf9795' => :lion
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin client conf lib]
