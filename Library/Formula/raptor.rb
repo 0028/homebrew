8fcb5aef98d2b815e7b1625d6c5f3e3361c7c5d9
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-19 05:27:32 +0000

    raptor: add 2.0.12 bottle.

--- Library/Formula/raptor.rb
+++ Library/Formula/raptor.rb
@@ -5,6 +5,13 @@ class Raptor < Formula
   url 'http://download.librdf.org/source/raptor2-2.0.12.tar.gz'
   sha1 'd485d409e93b79a9d3690ba2b9d15ca6e1c4b84e'
 
+  bottle do
+    cellar :any
+    sha1 '3c134829b69d924a3c078be80b6cb1b2f65f9101' => :mavericks
+    sha1 '648e8481c2baa06aeff4018ad30bccf4176d7d9e' => :mountain_lion
+    sha1 '6d70a544db5bea933451bf687e1e546aae4a86bf' => :lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
