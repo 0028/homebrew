fa9203e7d350b1806072666c1705eea5e624a172
Matt Chu <matt.chu@gmail.com>
2013-12-18 18:39:57 -0800

    locked to drake 0.1.4, pending next release. simplified recipe (courtesy https://github.com/agate/homebrew/commit/1e44ea52dcc54291733e07a86546d0685d93c70b)

--- Library/Formula/drake.rb
+++ Library/Formula/drake.rb
@@ -1,47 +1,21 @@
 require 'formula'
 
 class Drake < Formula
-  # TODO? detect version
-  VERSION = '0.1.5-SNAPSHOT'
-
   homepage 'https://github.com/Factual/drake'
-  # no tags yet, this is latest rev of default branch 'develop'
-  url 'https://api.github.com/repos/Factual/drake/tarball/97c4ffa193a13f990ce11c5f724ec2049a67a624'
-  sha1 '82acf3986f73395fa266610ce9311208e8cf8302'
-  version VERSION
+  url 'https://api.github.com/repos/Factual/drake/tarball/7119b1c7ae6feeef919a74090da696005fd70875'
+  sha1 '353a08b2b0b1e135f07522e91e64e9839a4f742c'
+  version '0.1.4'
 
   depends_on 'leiningen' => :build
   depends_on 'drip' => :optional
 
-  def build_from_source
-    system 'lein uberjar'
-    'target/drake.jar'
-  end
-
-  def install_binary(filename, version)
-    installjar = "drake-#{version}.jar"
-    File.rename filename, installjar
-    libexec.install installjar
-    installjar
-  end
-
-  # custom jar script
-  def write_bin_script(targetjar, script_name)
-    (bin+script_name).write <<-EOS.undent
-      #!/bin/bash
-      cmd='java'
-      # use drip if available
-      if [[ -x `which drip` ]]; then
-        cmd='drip'
-      fi
-      exec ${cmd} -jar #{libexec}/drake-#{self.version}.jar "$@"
-    EOS
-  end
-
   def install
-    uberjar = self.build_from_source
-    installjar = self.install_binary uberjar, VERSION
-    self.write_bin_script installjar, 'drake'
+    libexec.install Dir['*']
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+
+    Dir.chdir(libexec) do
+      system 'lein uberjar'
+    end
   end
 
   test do
