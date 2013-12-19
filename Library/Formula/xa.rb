6297f4741269bd0460aac090f85b748ed8c86ed4
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-19 18:56:24 +0000

    xa: add 2.3.5 bottle.

--- Library/Formula/xa.rb
+++ Library/Formula/xa.rb
@@ -5,6 +5,13 @@ class Xa < Formula
   url 'http://www.floodgap.com/retrotech/xa/dists/xa-2.3.5.tar.gz'
   sha1 'd8f4564953adfcee69faacfa300b954875fabe21'
 
+  bottle do
+    cellar :any
+    sha1 '9d030e7f4ae8d6e649767d0cc01aa9e3d894fdc6' => :mavericks
+    sha1 '37e57bffcbb2c1aaf89b7049c39f264b2a3d76b7' => :mountain_lion
+    sha1 '912927ff3ec02a1679707561197eab139b2c7366' => :lion
+  end
+
   def install
     inreplace "src/xa.c", /\bgetline\b/, "xa_getline"
 
