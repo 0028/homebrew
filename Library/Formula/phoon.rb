5cb136b319111f4b8bc16bc1f71c91e42b2e0224
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-20 17:26:16 +0000

    phoon: add 03A bottle.

--- Library/Formula/phoon.rb
+++ Library/Formula/phoon.rb
@@ -6,6 +6,13 @@ class Phoon < Formula
   version '03A'
   sha1 'd646af184b83e1a4104fe82588daadae2938e08c'
 
+  bottle do
+    cellar :any
+    sha1 '115f30476dca3df51a65d94df6222af712113fc2' => :mavericks
+    sha1 '3fa5c11bbe8f71790a4f038c1d71efc4cf383906' => :mountain_lion
+    sha1 '93432df9aaeca0417f075a8ed879c70d58072a5d' => :lion
+  end
+
   def install
     system "make"
     bin.install('phoon')
