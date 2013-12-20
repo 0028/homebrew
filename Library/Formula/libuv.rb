41693c2338badb3bec5dde0b0df392cf2cd91b94
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-20 21:41:19 +0000

    libuv: add 0.10.20 bottle.

--- Library/Formula/libuv.rb
+++ Library/Formula/libuv.rb
@@ -6,6 +6,13 @@ class Libuv < Formula
   url 'https://github.com/joyent/libuv/archive/v0.10.20.tar.gz'
   sha1 'c7e802418b00b0ebc559538d0d26880f484eb52f'
 
+  bottle do
+    cellar :any
+    sha1 '0e46b4a18f6300933fe9cc1976b5fae2ebeba0ad' => :mavericks
+    sha1 'c0142e48d3f7a7c90ad42f03147a8048497e578a' => :mountain_lion
+    sha1 '81ca577dc2ba665a680a49907903fc0c860298a2' => :lion
+  end
+
   head 'https://github.com/joyent/libuv.git', :branch => 'master'
 
   option :universal
