c968f8e0a25f3141eaaa2d1be64ebf5d3c0cf1d3
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-20 09:42:37 +0000

    felix: add 4.2.1 bottle.

--- Library/Formula/felix.rb
+++ Library/Formula/felix.rb
@@ -5,6 +5,13 @@ class Felix < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=/felix/org.apache.felix.main.distribution-4.2.1.tar.gz'
   sha1 '96322a8ccf802feb1151316b3367cac273f58823'
 
+  bottle do
+    cellar :any
+    sha1 '17685b0b4471f0280548d9e3c5b6d9ce4e86b6d9' => :mavericks
+    sha1 '825caa98786420951d920402955c6b67f33dbd3a' => :mountain_lion
+    sha1 '2470f331ef55c0120e5964501b811dca828076b5' => :lion
+  end
+
   def install
     libexec.install Dir['bin/*']
     prefix.install %w[conf bundle doc]
