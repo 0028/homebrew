3faa8adb6ac6a9655808d7a2c7350dff7dd9da07
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-20 18:27:07 +0000

    sassc: add 1.0.1 bottle.

--- Library/Formula/sassc.rb
+++ Library/Formula/sassc.rb
@@ -5,6 +5,13 @@ class Sassc < Formula
   url 'https://github.com/hcatlin/sassc/archive/v1.0.1.tar.gz'
   sha1 '69e7d97264b252593a3307330a96a5ccdc2813b5'
 
+  bottle do
+    cellar :any
+    sha1 '6fbe80a2d87eae5c5e5a5da36a94c4f987479a15' => :mavericks
+    sha1 '0dfa2e34a17ad41d828841654e39979ae4791204' => :mountain_lion
+    sha1 'e4d0905a44200f7edcde04dac9548911a19b8d3e' => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
