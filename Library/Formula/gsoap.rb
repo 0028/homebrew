a3c4bcba144af7e471f4bfd2c6d0dfb566bef115
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-20 21:37:08 +0000

    gsoap: add 2.8.17 bottle.

--- Library/Formula/gsoap.rb
+++ Library/Formula/gsoap.rb
@@ -5,6 +5,13 @@ class Gsoap < Formula
   url 'http://downloads.sourceforge.net/project/gsoap2/gSOAP/gsoap_2.8.17.zip'
   sha1 'd6c483ea2eabade138d71d005300be909be9a274'
 
+  bottle do
+    cellar :any
+    sha1 '7aa2c42af0f60e5f7598adc43b9bd1031760647e' => :mavericks
+    sha1 '9ac1b0bc4cc4f9285ad28439a7429f9e9663c93b' => :mountain_lion
+    sha1 '5381a17b0b135f29186618c0dd1998150249ab68' => :lion
+  end
+
   def install
     ENV.deparallelize
     system './configure', "--prefix=#{prefix}"
