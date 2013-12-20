19ca1de8ec2841653117528e5035f0202abed542
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-20 22:16:20 +0000

    dub: add 0.9.20 bottle.

--- Library/Formula/dub.rb
+++ Library/Formula/dub.rb
@@ -7,6 +7,12 @@ class Dub < Formula
 
   head 'https://github.com/rejectedsoftware/dub.git'
 
+  bottle do
+    sha1 'c676aa57f44d5102d9d29fb8fb09c1e5f07e2f8a' => :mavericks
+    sha1 '5de11a109e91eb465239a825ea31cdeef60ea760' => :mountain_lion
+    sha1 '1e06d315c348a5bb31cd19a2830403819ef1de11' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'dmd'  => :build
 
