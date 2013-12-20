2c284dec140113947fe44d47c9ccc25df372c3f4
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-20 21:09:42 +0000

    libsass: add 1.0.1 bottle.

--- Library/Formula/libsass.rb
+++ Library/Formula/libsass.rb
@@ -5,6 +5,13 @@ class Libsass < Formula
   url 'https://github.com/hcatlin/libsass/archive/v1.0.1.tar.gz'
   sha1 '9524e028bc8ebe84e36895269d07ecc7db496c7c'
 
+  bottle do
+    cellar :any
+    sha1 '24e0cf54c4aac623a29ca1a179ed9d77a07f425e' => :mavericks
+    sha1 'bf34c3f5962077c11b63ea8ea6b1f5e089fa60c4' => :mountain_lion
+    sha1 'd20ea34ac11f50bb0dadfea05a1e95b8ee1c87b5' => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
