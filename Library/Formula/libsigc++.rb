16e02c1c5935c504a52c972305da4e592572da51
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-21 14:56:33 +0000

    libsigc++: add 2.3.1 bottle.

--- Library/Formula/libsigc++.rb
+++ Library/Formula/libsigc++.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Libsigcxx < Formula
   homepage 'http://libsigc.sourceforge.net'
   url 'http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.3/libsigc++-2.3.1.tar.xz'
+  bottle do
+    sha1 '9400d51f34492c2e0019b9a66b5f78c1fcdb30dc' => :mavericks
+    sha1 '4a5a98b4fbc8ba3ed06a7636ae9f412be229050d' => :mountain_lion
+    sha1 '3cd49cb32d535c4ae46dc0c67c154b1a4331b06a' => :lion
+  end
+
   sha256 '67d05852b31fdb267c9fdcecd40b046a11aa54d884435e99e3c60dd20cd60393'
 
   depends_on 'xz' => :build
