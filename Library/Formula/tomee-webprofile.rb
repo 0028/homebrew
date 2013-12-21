34b4d07c9f5392a6e92071b5fe168e92587257b0
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-21 07:08:15 +0000

    tomee-webprofile: add 1.6.0 bottle.

--- Library/Formula/tomee-webprofile.rb
+++ Library/Formula/tomee-webprofile.rb
@@ -6,6 +6,13 @@ class TomeeWebprofile < Formula
   version '1.6.0'
   sha1 '7a13d9e3b3b66b7dd2f51ab3267f2709fc6e8424'
 
+  bottle do
+    cellar :any
+    sha1 'a4c26c4011807a545c038a5b687f4498c95038be' => :mavericks
+    sha1 'b29b4a049ae85c13cb92371dab540d169b5394e6' => :mountain_lion
+    sha1 '4e26f2dbfc6b19b703fdb9ad115deeb43b0ef8a9' => :lion
+  end
+
   # Keep log folders
   skip_clean 'libexec'
 
