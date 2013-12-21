372a9a2c43af997c7f9b2c3adf8340c6543a024e
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-21 17:51:17 +0000

    bup: add 0.25 bottle.

--- Library/Formula/bup.rb
+++ Library/Formula/bup.rb
@@ -6,6 +6,13 @@ class Bup < Formula
   url 'https://github.com/bup/bup/archive/0.25.tar.gz'
   sha1 'f32ba39582d0e8875632f282c54f7377ed2a4df9'
 
+  bottle do
+    cellar :any
+    sha1 '898d6705848660a7f7a855244dc0e8b227bf03a8' => :mavericks
+    sha1 'd82980785cceeb2a7756bc34ff00fd83c3137dd0' => :mountain_lion
+    sha1 '229b74cebdeb9ba623954ea68d0b170935182e66' => :lion
+  end
+
   option "run-tests", "Run unit tests after compilation"
 
   depends_on :python
