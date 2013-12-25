6915068901224f1b2847a81146d4390d931dad38
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-25 00:37:14 +0000

    mess: add 0.152 bottle.

--- Library/Formula/mess.rb
+++ Library/Formula/mess.rb
@@ -7,6 +7,13 @@ class Mess < Formula
 
   head 'svn://dspnet.fr/mame/trunk'
 
+  bottle do
+    cellar :any
+    sha1 '0bf0d7b58a4d576e652151be0370c281195226c5' => :mavericks
+    sha1 '56030d301e6f362a1ffbf91130f22254c05910a1' => :mountain_lion
+    sha1 'c769b9c7ea18130849397d7b6684a06e2a071211' => :lion
+  end
+
   depends_on :x11
   depends_on 'sdl'
 
