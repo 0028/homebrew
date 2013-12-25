5afe63f722f855398894cec7cc282b6c32cfb6c1
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-25 15:21:18 +0000

    ddptools: add 0.8.7a bottle.

--- Library/Formula/ddptools.rb
+++ Library/Formula/ddptools.rb
@@ -5,6 +5,13 @@ class Ddptools < Formula
   url 'http://ddp.andreasruge.de/dist/ddptools-0.8.7a-osx.tar.gz'
   sha1 'ff062d302d55ee2880b957f6e802b0b503d3ead9'
 
+  bottle do
+    cellar :any
+    sha1 '6eec381c716621feb10c4d2194bd69856e295755' => :mavericks
+    sha1 '19a4b234cb193cfe4412d01f51bdfe96fd7b4bab' => :mountain_lion
+    sha1 'aa14d4e440849c0939026b58099a27165c6586e5' => :lion
+  end
+
   def install
     bin.install ['ddpinfo', 'cue2ddp', 'cdtinfo']
     man1.install ['doc/ddpinfo.1', 'doc/cue2ddp.1', 'doc/cdtinfo.1']
