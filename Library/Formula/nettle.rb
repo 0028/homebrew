8bc8e982916f724f88e885c3a6c65c648c1c151f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-26 08:41:45 +0000

    nettle: add 2.7.1 bottle.

--- Library/Formula/nettle.rb
+++ Library/Formula/nettle.rb
@@ -5,6 +5,13 @@ class Nettle < Formula
   url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz'
   sha1 'e7477df5f66e650c4c4738ec8e01c2efdb5d1211'
 
+  bottle do
+    cellar :any
+    sha1 '1fc8c9f59619fc4e9e5d9463d370d81c1598cfc7' => :mavericks
+    sha1 '16c1dcea2cca96b29498cfb829dbccc8eb8e696b' => :mountain_lion
+    sha1 '6ebc9a219165f9dec14c8036123489584e16824a' => :lion
+  end
+
   depends_on 'gmp'
 
   def install
