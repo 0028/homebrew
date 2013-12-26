6381dc259c4e94e4ee437cb62ce422e925fd173c
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-26 20:42:43 +0000

    sshfs: add 2.4.0 bottle.

--- Library/Formula/sshfs.rb
+++ Library/Formula/sshfs.rb
@@ -5,6 +5,13 @@ class Sshfs < Formula
   url 'https://github.com/fuse4x/sshfs/archive/sshfs_2_4_0.tar.gz'
   sha1 '30b81ac7f32125088652937568d8886e3bb3f6e2'
 
+  bottle do
+    cellar :any
+    sha1 '080092e6b18c425c0367ddc5562a47d75baca30d' => :mavericks
+    sha1 '323a47a62fa060ce80b2a49c31f3579713572c73' => :mountain_lion
+    sha1 '4b6e6242fa7f54108afc6b9e9c68fae67b205884' => :lion
+  end
+
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on :libtool
