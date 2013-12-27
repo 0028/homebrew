9f0f4832fb9fc8de8f34b958009b3de5c0afbfdf
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-27 00:06:42 +0000

    camlistore: add 0.6 bottle.

--- Library/Formula/camlistore.rb
+++ Library/Formula/camlistore.rb
@@ -6,6 +6,12 @@ class Camlistore < Formula
   url 'https://github.com/bradfitz/camlistore/archive/0.6.tar.gz'
   sha1 'ca2a530fb2eb39a6cecf2f60e290934a9b6414f4'
 
+  bottle do
+    sha1 'c7e7c1a9d23824fc83a8808a15c397f01e62d568' => :mavericks
+    sha1 '469a4590d427509ea348c9919ed3fc0ee8a705c5' => :mountain_lion
+    sha1 '79ecd652cf0046ed784414c6404ddf6dbcc0467b' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'go' => :build
   depends_on 'sqlite'
