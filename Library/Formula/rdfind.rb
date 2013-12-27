a417ffc6398ae00d475585c408962ed9140093f7
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-27 00:11:39 +0000

    rdfind: add 1.3.4 bottle.

--- Library/Formula/rdfind.rb
+++ Library/Formula/rdfind.rb
@@ -5,6 +5,13 @@ class Rdfind < Formula
   url 'http://rdfind.pauldreik.se/rdfind-1.3.4.tar.gz'
   sha1 'c01bd2910cdec885b6c24164a389457e4f01ef61'
 
+  bottle do
+    cellar :any
+    sha1 '6fdbf2f11e2e1c42ea25b331326da2d4e8be5a27' => :mavericks
+    sha1 'd39c1fc940688c03cb2444a236a34c619e55d0f0' => :mountain_lion
+    sha1 '467eeb494b7ee6c40fd78182625d4da9837eb2db' => :lion
+  end
+
   depends_on 'nettle'
 
   def install
