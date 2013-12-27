a4fecdc0b0bb942b432c89c39f4b2103347bc328
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-27 18:16:19 +0000

    git-integration: add 0.2 bottle.

--- Library/Formula/git-integration.rb
+++ Library/Formula/git-integration.rb
@@ -5,6 +5,13 @@ class GitIntegration < Formula
   url 'https://github.com/johnkeeping/git-integration/archive/v0.2.zip'
   sha1 'ce86564077a683c8ce270c85530f9100f3f8c950'
 
+  bottle do
+    cellar :any
+    sha1 '95f0a564ebeb79cb797daad65e2e9443425deac9' => :mavericks
+    sha1 '7360c7fe3adbbe6e8ed0aba4f8d1e522ede5afd5' => :mountain_lion
+    sha1 'b0c606088f0a85140f1a5578919205e90c252e7b' => :lion
+  end
+
   depends_on 'asciidoc' => [:build, :optional]
 
   def install
