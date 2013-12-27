c91537c20cca8ed3718d33f9d2c3f3532f6def0d
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-27 17:01:27 +0000

    libslax: add 0.17.2 bottle.

--- Library/Formula/libslax.rb
+++ Library/Formula/libslax.rb
@@ -5,6 +5,12 @@ class Libslax < Formula
   url 'https://github.com/Juniper/libslax/releases/download/0.17.2/libslax-0.17.2.tar.gz'
   sha1 '20dba3ea27fc6dd6d9e2aa7ad6e931b1dfe8d6bc'
 
+  bottle do
+    sha1 'd55efa9a64c35c6adf0d26180c0a7a6f44d3addc' => :mavericks
+    sha1 '7edc9ced2fec2d3acf49079160dec1fcd03ae5a8' => :mountain_lion
+    sha1 '430b0d51f79a238a497854268eea7773f85f7ed6' => :lion
+  end
+
   head do
     url 'https://github.com/Juniper/libslax.git'
 
