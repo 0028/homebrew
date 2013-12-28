8f390e20a4679f2ebd68daa141cb80b909091373
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-28 06:22:12 +0000

    lftp: add 4.4.14 bottle.

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -5,6 +5,12 @@ class Lftp < Formula
   url 'http://lftp.yar.ru/ftp/lftp-4.4.14.tar.bz2'
   sha1 'f34cfc0351f8e63a066cf8014ac32a52f5159434'
 
+  bottle do
+    sha1 '6a436e04c79c7b7fc458b3d46f59d3442e8a26af' => :mavericks
+    sha1 '33914c1417df6b288765b971de3c8e799f55882d' => :mountain_lion
+    sha1 '784a42a38b582b703bcf066dadb5e4f10082ef43' => :lion
+  end
+
   option 'with-gnutls', "Use GnuTLS instead of the default OpenSSL"
   option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
 
