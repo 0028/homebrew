df3a8521860d391ea92a2faa04ec104e9ae12f51
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-29 00:17:09 +0000

    libfreefare: add 0.4.0 bottle.

--- Library/Formula/libfreefare.rb
+++ Library/Formula/libfreefare.rb
@@ -4,6 +4,13 @@ class Libfreefare < Formula
   homepage 'https://code.google.com/p/libfreefare/'
   url 'https://libfreefare.googlecode.com/files/libfreefare-0.4.0.tar.bz2'
   sha1 '74214069d6443a6a40d717e496320428a114198c'
+  bottle do
+    cellar :any
+    sha1 'e4f5135d44871c748c019995d1eba71a94238e8c' => :mavericks
+    sha1 '3d576399cbca7947334ed021377a0680a1734dff' => :mountain_lion
+    sha1 '67ce6c61033831993943a1fbd83ed88b74b7ea01' => :lion
+  end
+
   depends_on 'libnfc'
   depends_on 'pkg-config' => :build
 
