f7aa99363c9757c19851978653e2551259605f6b
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-29 01:58:09 +0000

    mfcuk: add 0.3.8 bottle.

--- Library/Formula/mfcuk.rb
+++ Library/Formula/mfcuk.rb
@@ -5,6 +5,13 @@ class Mfcuk < Formula
   url 'http://mfcuk.googlecode.com/files/mfcuk-0.3.8.tar.gz'
   sha1 '2a8259440ac5bed8516c8d771a945b713dacd2bc'
 
+  bottle do
+    cellar :any
+    sha1 '0588f2b0f9978f98c6ef41bf83315044aafe2284' => :mavericks
+    sha1 'a606e641e2541c714665d4ecc8ff70e20ff67861' => :mountain_lion
+    sha1 'eb4fed33ab4cf01c9b6f592d62e4301ebadcb77e' => :lion
+  end
+
   depends_on :automake
   depends_on :autoconf
   depends_on 'pkg-config' => :build
