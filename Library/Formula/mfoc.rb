ef92ca818a12ed851119c667e1f0d976f674a401
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-29 01:58:10 +0000

    mfoc: add 0.10.7 bottle.

--- Library/Formula/mfoc.rb
+++ Library/Formula/mfoc.rb
@@ -5,6 +5,13 @@ class Mfoc < Formula
   url 'http://mfoc.googlecode.com/files/mfoc-0.10.7.tar.bz2'
   sha1 '162a464baf6498926a72383c6b0040654321012d'
 
+  bottle do
+    cellar :any
+    sha1 '448e72aee781a83dd519f379c7f00e9d77ddf49a' => :mavericks
+    sha1 '5e0fa6552783d899a7651891f1a678d803914de7' => :mountain_lion
+    sha1 '7e7f86e3afea7632428334ad0a3aafd5138b1602' => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on 'pkg-config' => :build
