048f8b2042bccf17c3a642cbd735526743298cf0
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-29 01:58:10 +0000

    nfcutils: add 0.3.2 bottle.

--- Library/Formula/nfcutils.rb
+++ Library/Formula/nfcutils.rb
@@ -5,6 +5,13 @@ class Nfcutils < Formula
   url 'https://nfc-tools.googlecode.com/files/nfcutils-0.3.2.tar.gz'
   sha1 'e560ba7683175257ef9e72838b9f02cf75ce99b8'
 
+  bottle do
+    cellar :any
+    sha1 'ce6db5625fd73a4cc6fe8039890056b1ade8cbe2' => :mavericks
+    sha1 '5fd83ae1acbc115b8eceeccb20ba8033f251828a' => :mountain_lion
+    sha1 '7b06bc57eddf6496765d66e517e6727c6f1f07cc' => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on 'pkg-config' => :build
