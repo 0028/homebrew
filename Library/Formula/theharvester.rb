d95ca45535e7ef8681547b8fa6ce66c0d7cdfe2f
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-30 22:12:37 +0000

    theharvester: add 2.2a bottle.

--- Library/Formula/theharvester.rb
+++ Library/Formula/theharvester.rb
@@ -5,6 +5,13 @@ class Theharvester < Formula
   url 'https://theharvester.googlecode.com/files/theHarvester-2.2a.tar.gz'
   sha1 'e02661ed6dd8d9d48d476ccee99878e15f67842a'
 
+  bottle do
+    cellar :any
+    sha1 '1e019cb6d2a035070e4fce418d280eb24a292d44' => :mavericks
+    sha1 '1fdae77100f79090bd9c52cc5815354f0745439d' => :mountain_lion
+    sha1 '41b5c6a9a245a678c2c8f204cc29f65708f16c60' => :lion
+  end
+
   depends_on :python
 
   def install
