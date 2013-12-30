123e8aa523e18413db69c9fcacc1d9d3e6ee38e9
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-30 19:39:02 +0000

    ioping: add 0.8 bottle.

--- Library/Formula/ioping.rb
+++ Library/Formula/ioping.rb
@@ -7,6 +7,13 @@ class Ioping < Formula
 
   head 'http://ioping.googlecode.com/svn/trunk/'
 
+  bottle do
+    cellar :any
+    sha1 '5a66fd2cabee34c816f06c0d506c89114b5cc4f9' => :mavericks
+    sha1 '3c2aafd3d820cea97a4e4d91a4b3017e2340eadc' => :mountain_lion
+    sha1 '888030bc681b64bba57e9e00da0554ed498952c2' => :lion
+  end
+
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
