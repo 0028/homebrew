007d1bbb2f765d099df3cad823c7ff6dc2b65a90
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-31 13:33:17 +0000

    postgresql-jdbc3: add 9.3-1100 bottle.

--- Library/Formula/postgresql-jdbc3.rb
+++ Library/Formula/postgresql-jdbc3.rb
@@ -6,6 +6,13 @@ class PostgresqlJdbc3 < Formula
   sha1 '682c1724724a2fbbd338ba9336686696558e2a5f'
   version '9.3-1100'
 
+  bottle do
+    cellar :any
+    sha1 '2def987574fe32ad83d27aa2393b8172718c54de' => :mavericks
+    sha1 '6a715cbec5738db11009db1fbc622ca90de940b9' => :mountain_lion
+    sha1 '695f18ce61d9d9011fa9630a02b458374649ad43' => :lion
+  end
+
   def install
     libexec.install Dir['*.jar']
   end
