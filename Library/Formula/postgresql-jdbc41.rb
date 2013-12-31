bf8b5f016b21c6967ff53dd1e50fe192069e7037
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-31 13:33:17 +0000

    postgresql-jdbc41: add 9.3-1100 bottle.

--- Library/Formula/postgresql-jdbc41.rb
+++ Library/Formula/postgresql-jdbc41.rb
@@ -6,6 +6,13 @@ class PostgresqlJdbc41 < Formula
   sha1 '8f89bd15ef7b9a7db03b13a0d69ace8581916160'
   version '9.3-1100'
 
+  bottle do
+    cellar :any
+    sha1 'b84d6290d82714b64f03f4fb9f7449dc5733aef4' => :mavericks
+    sha1 'df969a12023908ad81b7bcd6a061ee68b28ae887' => :mountain_lion
+    sha1 '944bcdd0f9e0c79fc1f22ea1544fabed3e08571d' => :lion
+  end
+
   def install
     libexec.install Dir['*.jar']
   end
