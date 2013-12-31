63580e6db32dfc5912ab8f888109f18b5f69817a
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-31 12:57:48 +0000

    mysql-connector-java: add 5.1.28 bottle.

--- Library/Formula/mysql-connector-java.rb
+++ Library/Formula/mysql-connector-java.rb
@@ -5,6 +5,13 @@ class MysqlConnectorJava < Formula
   url 'http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.28.tar.gz'
   sha1 '872257139fbef8eed808e128eb90abc5d6effe5b'
 
+  bottle do
+    cellar :any
+    sha1 '0ff07b7132dfb36a77195e002a49c94e886cd22c' => :mavericks
+    sha1 'a1139a0bc28399ef06f964f4e41eab155ddb6934' => :mountain_lion
+    sha1 'b3a2953504c1685f5bf83e5eb5b28e70834d240f' => :lion
+  end
+
   def install
     prefix.install_metafiles
     libexec.install Dir['*.jar']
