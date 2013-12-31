ac6941c368d0ecff330e8510b74e1698a74ecea1
BrewTestBot <brew-test-bot@googlegroups.com>
2013-12-31 16:32:40 +0000

    postgresql-jdbc: add 9.3-1100 bottle.

--- Library/Formula/postgresql-jdbc.rb
+++ Library/Formula/postgresql-jdbc.rb
@@ -6,6 +6,13 @@ class PostgresqlJdbc < Formula
   sha1 '682c1724724a2fbbd338ba9336686696558e2a5f'
   version '9.3-1100'
 
+  bottle do
+    cellar :any
+    sha1 'a01e7319552cd13c95c1b91741d5af0dbbbf1793' => :mavericks
+    sha1 '6f6fa61694c4a8a5d8a57dbda1107e2e02b563da' => :mountain_lion
+    sha1 'd5c34a31cb65c8da520853446fca907c53f0a246' => :lion
+  end
+
   resource 'jdbc4' do
     url 'http://jdbc.postgresql.org/download/postgresql-9.3-1100.jdbc4.jar'
     sha1 'fe6ed26ff71d75ebb99e2ec2123003e05ea16dc5'
