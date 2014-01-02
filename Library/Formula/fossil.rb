2b72ad46d64c19b424ea0c20eb46ea343baaf417
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-02 17:50:49 +0000

    fossil: add 1.27 bottle.

--- Library/Formula/fossil.rb
+++ Library/Formula/fossil.rb
@@ -7,6 +7,13 @@ class Fossil < Formula
   sha1 '2e2149fff30d63a1869ecb5a7d6b17996477612a'
   version '1.27'
 
+  bottle do
+    cellar :any
+    sha1 'de9fcd67feec32d333380bc0060f1a423c058a93' => :mavericks
+    sha1 '4fa3bc359c4f7c021bc122173407caa7d72112d9' => :mountain_lion
+    sha1 'e362bb4461f0d7a218dd3951addae5a75370b931' => :lion
+  end
+
   option 'without-json', 'Build without "json" command support.'
   option 'without-tcl', "Build without the tcl-th1 command bridge."
 
