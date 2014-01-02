d381145fe19078f05d6e39559227420d1e8af387
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-02 18:12:56 +0000

    cairomm: add 1.10.0 bottle.

--- Library/Formula/cairomm.rb
+++ Library/Formula/cairomm.rb
@@ -5,6 +5,12 @@ class Cairomm < Formula
   url 'http://cairographics.org/releases/cairomm-1.10.0.tar.gz'
   sha256 '068d96c43eae7b0a3d98648cbfc6fbd16acc385858e9ba6d37b5a47e4dba398f'
 
+  bottle do
+    sha1 'e9cb495d3c6e4d9e54ecd3464c9a4f1abd9ea0b5' => :mavericks
+    sha1 '79426340d092f3a47d39914c47a0481ba7119dba' => :mountain_lion
+    sha1 'e81e46bd6129d7a81baf51533d9273655bf02dd2' => :lion
+  end
+
   option 'without-x', 'Build without X11 support'
   option :cxx11
 
