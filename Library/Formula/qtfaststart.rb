1de7d171a55eef572727535083c354ed393577d8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-02 21:17:04 +0000

    qtfaststart: add 2.1.1 bottle.

--- Library/Formula/qtfaststart.rb
+++ Library/Formula/qtfaststart.rb
@@ -5,6 +5,13 @@ class Qtfaststart < Formula
   url 'http://ffmpeg.org/releases/ffmpeg-2.1.1.tar.bz2'
   sha1 'e7a5b2d7f702c4e9ca69e23c6d3527f93de0d1bd'
 
+  bottle do
+    cellar :any
+    sha1 '87d9c221fedbcdab137a36cbc101c4e34c21e285' => :mavericks
+    sha1 'c202cd65add627eae6e16f96650b1514257ca65b' => :mountain_lion
+    sha1 'ff563d895e40af4555f547d4f74a253ba11ff2f0' => :lion
+  end
+
   def install
     system ENV.cc, '-o', 'tools/qt-faststart', 'tools/qt-faststart.c'
     bin.install 'tools/qt-faststart'
