1b5ca08e0f9da0c36b68161cd1695b1d5915db91
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-03 12:23:48 +0000

    zmap: add 1.1.1.1 bottle.

--- Library/Formula/zmap.rb
+++ Library/Formula/zmap.rb
@@ -6,6 +6,13 @@ class Zmap < Formula
   sha1 '0c53e6e6cfe35aefd7b018e41b80674fa25c3d49'
   head 'https://github.com/zmap/zmap.git'
 
+  bottle do
+    cellar :any
+    sha1 'd99ca74eec140d8091760265d6fd5222237aea8e' => :mavericks
+    sha1 '76f4d1b1d72629c36bb51b60def0e95ea3ef0978' => :mountain_lion
+    sha1 '39a0f2cebae36c0ece59d3594e629eb18c27806e' => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'gengetopt' => :build
   depends_on 'byacc' => :build
