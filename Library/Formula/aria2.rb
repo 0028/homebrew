d4d2c057e63c1d1208e37877201f28e926c8ceb1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-09 22:22:56 +0000

    aria2: add 1.18.2 bottle.

--- Library/Formula/aria2.rb
+++ Library/Formula/aria2.rb
@@ -5,6 +5,13 @@ class Aria2 < Formula
   url 'http://downloads.sourceforge.net/project/aria2/stable/aria2-1.18.2/aria2-1.18.2.tar.bz2'
   sha1 '2f04a17567e6b793420a517b3fb0511f12c76289'
 
+  bottle do
+    cellar :any
+    sha1 '26dcb9805d693aa8973ffe1cf9e945d0c9351a0c' => :mavericks
+    sha1 '8fc2150e1bee6685f3d0e81400a356bcc908a38f' => :mountain_lion
+    sha1 '5147b1d2a4fc8e10c090dd8bcdc68c070654ab1c' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'sqlite' => :build # required for --with-sqlite3 to support Chrome cookies
   depends_on :macos => :lion # Needs a c++11 compiler
