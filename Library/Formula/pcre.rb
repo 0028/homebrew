41a42392735f0d96397b8ee6ee0497271643cd0d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-10 12:31:50 +0000

    pcre: add 8.34 bottle.

--- Library/Formula/pcre.rb
+++ Library/Formula/pcre.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Pcre < Formula
   homepage 'http://www.pcre.org/'
   url 'http://downloads.sourceforge.net/project/pcre/pcre/8.34/pcre-8.34.tar.bz2'
+  bottle do
+    cellar :any
+    sha1 '87174d1664f04a07de266a9cfa35598ce7d82a16' => :mavericks
+    sha1 '078584117f65701358ed41b26c54869d2d384a5b' => :mountain_lion
+    sha1 '49ab80f7fb8d8d86a3df0058ee7f127435726eb6' => :lion
+  end
+
   mirror 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.34.tar.bz2'
   sha256 'b6043ae1ff2720be665ffa28dc22b7c637cdde96f389a116c0c3020caeae583f'
 
