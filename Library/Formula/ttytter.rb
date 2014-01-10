d791fa815170ad7e1ae466f1d80ba9d3972d1fef
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-10 06:17:00 +0000

    ttytter: add 2.1.00 bottle.

--- Library/Formula/ttytter.rb
+++ Library/Formula/ttytter.rb
@@ -5,6 +5,13 @@ class Ttytter < Formula
   url 'http://www.floodgap.com/software/ttytter/dist2/2.1.00.txt'
   sha1 'dd20d55aa819699b3e39ca4c35bf390b3e074db3'
 
+  bottle do
+    cellar :any
+    sha1 '84a522713f09d007e657c04a3d0b5558d543c1df' => :mavericks
+    sha1 '415333a139759012461a18eacc33f223be3e357f' => :mountain_lion
+    sha1 '34e63b1801249952bce9dd097219731e3b79589a' => :lion
+  end
+
   depends_on 'curl'
 
   def install
