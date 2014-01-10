2602b1d8f93dcfc3e7b5687c9a95791b2bdbc4bf
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-10 05:11:32 +0000

    jruby: add 1.7.10 bottle.

--- Library/Formula/jruby.rb
+++ Library/Formula/jruby.rb
@@ -5,6 +5,13 @@ class Jruby < Formula
   url 'http://jruby.org.s3.amazonaws.com/downloads/1.7.10/jruby-bin-1.7.10.tar.gz'
   sha1 '5ec267dd3ddaa6e27801692a7080204067662289'
 
+  bottle do
+    cellar :any
+    sha1 '72f9735add7404f00e070c62cea6bdc83fa1c978' => :mavericks
+    sha1 'c8f11c6deea980e15368c88d8e354f307a5648bf' => :mountain_lion
+    sha1 'bf9ecceaba588b3951d3ebc9d22ac62a9bf1c7e6' => :lion
+  end
+
   def install
     # Remove Windows files
     rm Dir['bin/*.{bat,dll,exe}']
