aa9c192b17caa02de2081abea4aff4dafafd4848
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-11 06:07:00 +0000

    coin: add 3.1.3 bottle.

--- Library/Formula/coin.rb
+++ Library/Formula/coin.rb
@@ -5,6 +5,12 @@ class Coin < Formula
   url 'https://bitbucket.org/Coin3D/coin/downloads/Coin-3.1.3.tar.gz'
   sha1 '8e9f05628461963623686d3ec53102214e233dd1'
 
+  bottle do
+    sha1 '1c0cec55ef6296e448c0c41307ff731367b2d620' => :mavericks
+    sha1 'a6e479a35809cc57e96878e33f69347eee155e74' => :mountain_lion
+    sha1 '9030928bfb1e7d44fe99ff5f8e13a0eb6278a1e5' => :lion
+  end
+
   def install
     # https://bitbucket.org/Coin3D/coin/issue/47 (fix misspelled test flag)
     inreplace "configure", '-fno-for-scoping', '-fno-for-scope'
