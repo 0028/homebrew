e3f2f327c28ee3357c8c5afc6da69d01c0fa9a4e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-11 19:27:46 +0000

    hive: add 0.12.0 bottle.

--- Library/Formula/hive.rb
+++ Library/Formula/hive.rb
@@ -5,6 +5,12 @@ class Hive < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=hive/hive-0.12.0/hive-0.12.0-bin.tar.gz'
   sha1 '488f625a859facc89312320ef7e9802f1cc2531e'
 
+  bottle do
+    sha1 'c8cc73a315a79ccd8c34ace630635ec9c7045e09' => :mavericks
+    sha1 'ced3210df209b144a30eb25b6e55dccebfafc2e3' => :mountain_lion
+    sha1 'fd37515eeb780afffd8f41fa3ec5cb1f9cf74374' => :lion
+  end
+
   depends_on 'hadoop'
 
   def install
