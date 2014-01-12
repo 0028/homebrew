02751bc4c5e505761389cfec4ef255230008dc78
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-12 05:03:32 +0000

    pdfmarks: add 0.2 bottle.

--- Library/Formula/pdfmarks.rb
+++ Library/Formula/pdfmarks.rb
@@ -5,6 +5,12 @@ class Pdfmarks < Formula
   url 'https://bitbucket.org/alexreg/pdfmarks/get/v0.2.tar.gz'
   sha1 '75943601cfb3b8aed110357a152838b86507403b'
 
+  bottle do
+    sha1 '014b18c6e2f867638f3c1a531a2ab0545f199e56' => :mavericks
+    sha1 '2df01f28b0e4ef74a64ef846664f7f9e1d3a10bd' => :mountain_lion
+    sha1 '865648e312d02e82c36c08be428c7b1b52bd9ed2' => :lion
+  end
+
   depends_on 'ghostscript'
 
   def install
