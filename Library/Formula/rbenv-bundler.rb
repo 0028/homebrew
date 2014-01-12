02171c85ff7812d1723d0599c1af657cf8eb2335
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-12 17:11:15 +0000

    rbenv-bundler: add 0.96 bottle.

--- Library/Formula/rbenv-bundler.rb
+++ Library/Formula/rbenv-bundler.rb
@@ -6,6 +6,13 @@ class RbenvBundler < Formula
   sha1 '02339c604c840f24b66523c9faad6ed82500eda1'
   head 'https://github.com/carsomyr/rbenv-bundler.git'
 
+  bottle do
+    cellar :any
+    sha1 'd9777d7ff9cd52d721b5da7d389c6fe38791bd98' => :mavericks
+    sha1 '48ccfd7975bb99a55308e3df8f256daeb723e801' => :mountain_lion
+    sha1 '70dd58f62f95d357757c203c562ca31b7f5d3f52' => :lion
+  end
+
   depends_on 'rbenv'
 
   def install
