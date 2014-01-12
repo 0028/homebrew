626c12a8ab24b6e837c46a8f921ae14a4714a234
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-12 04:52:02 +0000

    dar: add 2.4.11 bottle.

--- Library/Formula/dar.rb
+++ Library/Formula/dar.rb
@@ -5,6 +5,12 @@ class Dar < Formula
   url 'http://downloads.sourceforge.net/project/dar/dar/2.4.11/dar-2.4.11.tar.gz'
   sha1 '02c6257dca054fecbee1e7bcd91ce525f986a67d'
 
+  bottle do
+    sha1 '75e6a51d5de2172a3e9699edc7674bd5cd14b1b0' => :mavericks
+    sha1 '4326b9ed6e816ea34707afe49536b84e0044d3e2' => :mountain_lion
+    sha1 'f3586829a1c6c643113b799499e64ddc0755dd31' => :lion
+  end
+
   depends_on 'gettext'
 
   def patches
