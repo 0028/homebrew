17a26e0a0744e9dae8426babf689d224f52ab202
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-12 01:24:53 +0000

    curl: add 7.34.0 bottle.

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Curl < Formula
   homepage 'http://curl.haxx.se/'
   url 'http://curl.haxx.se/download/curl-7.34.0.tar.gz'
+  bottle do
+    cellar :any
+    sha1 '1e17275e1a23ecf0ca7f9551d008e7c4c0a3c2c5' => :mavericks
+    sha1 '0e556ccb13800f8dd5b3a21b214a2290a1aeedd0' => :mountain_lion
+    sha1 '6e48645789ee39d6dbbb8eac44ab0bffb1633f46' => :lion
+  end
+
   mirror 'ftp://ftp.sunet.se/pub/www/utilities/curl/curl-7.34.0.tar.gz'
   sha256 '0705271de8411a85460706e177cd0f1064ec07c0b9e140a66a916fb644696d6a'
 
