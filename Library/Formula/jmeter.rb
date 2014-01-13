792e54352d00767727689bba9e7f90450493da00
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-13 17:01:44 +0000

    jmeter: add 2.11 bottle.

--- Library/Formula/jmeter.rb
+++ Library/Formula/jmeter.rb
@@ -5,6 +5,12 @@ class Jmeter < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=jmeter/binaries/apache-jmeter-2.11.tgz'
   sha1 'e9b24f8b5f34565831aafcb046e72bdfa9537386'
 
+  bottle do
+    sha1 'cc218a9e5e9df5cac85026588be195a8f19dc4ee' => :mavericks
+    sha1 '932fdf60dedbff516a67d9b1f6ce4fb1a367b1b2' => :mountain_lion
+    sha1 '55f3c82a0da28b4b49c1ea6b62fe70870e0deeb5' => :lion
+  end
+
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
