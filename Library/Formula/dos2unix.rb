17a1743ef686f00cb483d3d04d7b29d5e360d029
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-13 04:16:15 +0000

    dos2unix: add 6.0.4 bottle.

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -5,6 +5,12 @@ class Dos2unix < Formula
   url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.4.tar.gz'
   sha1 '93d73148c09908a42dcbf5339312c9aa1f18ba7c'
 
+  bottle do
+    sha1 'a643d874fb75e8bbb9a588529ed7f8ee8047403f' => :mavericks
+    sha1 '6e0646e1000a19bac806599ecc36a553ecf00f6d' => :mountain_lion
+    sha1 'a78285edd978b4a6aaeb224ffd33992325ba0942' => :lion
+  end
+
   depends_on 'gettext'
 
   def install
