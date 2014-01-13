ea0fefb22bf534444e6b3982496d2b3144b3eeca
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-13 03:56:38 +0000

    uriparser: add 0.8.0 bottle.

--- Library/Formula/uriparser.rb
+++ Library/Formula/uriparser.rb
@@ -5,6 +5,13 @@ class Uriparser < Formula
   url 'http://downloads.sourceforge.net/project/uriparser/Sources/0.8.0/uriparser-0.8.0.tar.bz2'
   sha1 '4bfe347220b00ff9cd3252e2b784d13e583282fb'
 
+  bottle do
+    cellar :any
+    sha1 'e326421a34e63d8e0cd59ce4c607e6da30ae9947' => :mavericks
+    sha1 'f5567c73ca43b31cf03413bdcd6555bc48c226d1' => :mountain_lion
+    sha1 '5eebaf622ff6e628f2643ce0c97267fcf15bac0e' => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'cpptest'
 
