7a47bf8b1666adb042a67be334999637770f0c5f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-14 02:45:24 +0000

    libosip: add 4.1.0 bottle.

--- Library/Formula/libosip.rb
+++ Library/Formula/libosip.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Libosip < Formula
   homepage 'http://www.gnu.org/software/osip/'
   url 'http://ftpmirror.gnu.org/osip/libosip2-4.1.0.tar.gz'
+  bottle do
+    cellar :any
+    sha1 'c8bd26a92294bde3a26c8699926d3c63d9f107c9' => :mavericks
+    sha1 '7965738eced463bbbafb16d10a75f33474c4271c' => :mountain_lion
+    sha1 '1e2b3261b5c7f5911bbc39c101b38aa487b8ac81' => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/osip/libosip2-4.1.0.tar.gz'
   sha1 '61459c9052ca2f5e77a6936c9b369e2b0602c080'
 
