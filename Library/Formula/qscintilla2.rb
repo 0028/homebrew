0f8a7f7795906574039fb8aae1270f3fa95a1124
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-14 02:02:10 +0000

    qscintilla2: add 2.8 bottle.

--- Library/Formula/qscintilla2.rb
+++ Library/Formula/qscintilla2.rb
@@ -5,6 +5,13 @@ class Qscintilla2 < Formula
   url 'http://downloads.sf.net/project/pyqt/QScintilla2/QScintilla-2.8/QScintilla-gpl-2.8.tar.gz'
   sha1 '3edf9d476d4e6af0706a4d33401667a38e3a697e'
 
+  bottle do
+    cellar :any
+    sha1 'e9eee46d686be9b78838149a1b027e02a9de4d51' => :mavericks
+    sha1 'eeb6583cf6f578c7986c9f4e455333c0858a491f' => :mountain_lion
+    sha1 'ea4889509716519792885e2d0ee19e275244d928' => :lion
+  end
+
   depends_on 'pyqt'
   depends_on 'sip'
 
