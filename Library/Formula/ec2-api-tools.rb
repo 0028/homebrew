1241b729cc82aeac41260186ef415f3929ad6a26
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-15 10:26:26 +0000

    ec2-api-tools: add 1.6.12.0 bottle.

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -5,6 +5,13 @@ class Ec2ApiTools < AmazonWebServicesFormula
   url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.6.12.0.zip'
   sha1 'eb0732df59a2ad2c1ad3fa5839e846b69afad697'
 
+  bottle do
+    cellar :any
+    sha1 '9d609e35111826fce173e38f4e63b9ef467c6e54' => :mavericks
+    sha1 '252aa29b25ed1be04b361ccb0748b6c416e303f2' => :mountain_lion
+    sha1 '37f3b29ef418a3f1a5ccc5e61d2c3ebe68e14bdf' => :lion
+  end
+
   def install
     standard_install
   end
