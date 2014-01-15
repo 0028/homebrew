51f48c2ee3f5570c51bb21feecdb04caba8e50e3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-15 23:51:25 +0000

    keychain: add 2.7.1 bottle.

--- Library/Formula/keychain.rb
+++ Library/Formula/keychain.rb
@@ -7,6 +7,13 @@ class Keychain < Formula
   version '2.7.1'
   head 'https://github.com/funtoo/keychain.git'
 
+  bottle do
+    cellar :any
+    sha1 '9debb9a273f3bfa13fbff17e71336fa6ca272297' => :mavericks
+    sha1 'c33453cc833ea9f143b8fa355aac7d0728ce123b' => :mountain_lion
+    sha1 '96f9c89ee390e4e16032676c1b7a5b5c58ed4af8' => :lion
+  end
+
   def install
     system 'make'
     bin.install 'keychain'
