38fcc8b9c0dda8dd1c4884e167f2866d498a66f0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-15 15:31:23 +0000

    kqwait: add 1.0.3 bottle.

--- Library/Formula/kqwait.rb
+++ Library/Formula/kqwait.rb
@@ -8,6 +8,13 @@ class Kqwait < Formula
 
   head 'https://github.com/sschober/kqwait.git'
 
+  bottle do
+    cellar :any
+    sha1 'fca3df9923cbb313d8083b20a76bb626dad6879d' => :mavericks
+    sha1 'cce71dc57cf635508e1b35b6311bb2bb0fe36520' => :mountain_lion
+    sha1 '429233e374d74900b02c056b5f73f20e306a8057' => :lion
+  end
+
   def install
     system "make"
     bin.install "kqwait"
