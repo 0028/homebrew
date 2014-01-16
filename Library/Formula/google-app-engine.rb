3d60f68536fd23d050f207cffa16a2d4fa445266
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 01:34:51 +0000

    google-app-engine: add 1.8.9 bottle.

--- Library/Formula/google-app-engine.rb
+++ Library/Formula/google-app-engine.rb
@@ -5,6 +5,13 @@ class GoogleAppEngine < Formula
   url 'http://googleappengine.googlecode.com/files/google_appengine_1.8.9.zip'
   sha1 '6a5a79a81bf0f1fccf5b56dac41be6174888983e'
 
+  bottle do
+    cellar :any
+    sha1 'ccf629d6552ecb4c33287c18281c5085713e3107' => :mavericks
+    sha1 'c5ce18bf83681d711b4c3b6c180d51ab564647fa' => :mountain_lion
+    sha1 '1ece18a45f14534bf160be1088b4c0d9832f28c9' => :lion
+  end
+
   def install
     cd '..'
     share.install 'google_appengine' => name
