e84b5aaed195d1723625c315184950182827bea8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 05:46:17 +0000

    re2: add 20140111 bottle.

--- Library/Formula/re2.rb
+++ Library/Formula/re2.rb
@@ -7,6 +7,13 @@ class Re2 < Formula
 
   head 'https://re2.googlecode.com/hg'
 
+  bottle do
+    cellar :any
+    sha1 'a527a0b648e3b3bda1ffe11f1ba0644c01bd5d47' => :mavericks
+    sha1 '4b338ba57c1edddc6b6ba3c88d6784b10c5b192f' => :mountain_lion
+    sha1 '83120a63295e84ad0204c15e9ee3a18a5b723e5a' => :lion
+  end
+
   def install
     # https://code.google.com/p/re2/issues/detail?id=99
     if MacOS.version < :mavericks
