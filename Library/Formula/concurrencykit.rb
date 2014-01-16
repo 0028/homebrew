c2dd62b6ef3630bf2694735f85fc76dee9be9f34
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 05:41:18 +0000

    concurrencykit: add 0.3.4 bottle.

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -7,6 +7,13 @@ class Concurrencykit < Formula
 
   head 'git://git.concurrencykit.org/ck.git'
 
+  bottle do
+    cellar :any
+    sha1 'c96738fb0af7dc5eceadfa0372dbe011bf1614c9' => :mavericks
+    sha1 'f45620ad5ac95626c79bd3accedc94b28f58c222' => :mountain_lion
+    sha1 '53537313a4bd369af06fed381d6b4ad36300e785' => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "CC=#{ENV.cc}"
