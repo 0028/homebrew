9420132e45e57a2e1764d34c5516702cda4ea9ae
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 08:49:12 +0000

    log4c: add 1.2.4 bottle.

--- Library/Formula/log4c.rb
+++ Library/Formula/log4c.rb
@@ -7,6 +7,12 @@ class Log4c < Formula
 
   head 'cvs://:pserver:anonymous:@log4c.cvs.sourceforge.net:/cvsroot/log4c:log4c'
 
+  bottle do
+    sha1 '160f737ab08affa35345c57d505802822e82d1b0' => :mavericks
+    sha1 '8c912063cf7235c051de0fe00c0ec05d4e501add' => :mountain_lion
+    sha1 '331107989e48a5c632b728169cefb565fee6a0ea' => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
