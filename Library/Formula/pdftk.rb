11264391074ee0bdc8080804b211660b92a8d407
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 10:06:40 +0000

    pdftk: add 2.02 bottle.

--- Library/Formula/pdftk.rb
+++ Library/Formula/pdftk.rb
@@ -15,6 +15,13 @@ class Pdftk < Formula
     :using => PkgExtract
   sha1 '93226ecef996d7ad833a773ee15f9b62365fb567'
 
+  bottle do
+    cellar :any
+    sha1 'd0dae8717f488de81b2dd355cc98de4fe0ede69d' => :mavericks
+    sha1 'cff5d387d319a163265d5122519407c8087ad30a' => :mountain_lion
+    sha1 '6b051c1f3367835adf7b4f595e42c1fb323cd84a' => :lion
+  end
+
   depends_on :macos => :lion
 
   def install
