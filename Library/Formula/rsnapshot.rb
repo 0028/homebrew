7564e3661f6c3d16eabfea7499c7a2ea4363a82b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 08:49:12 +0000

    rsnapshot: add 1.3.1 bottle.

--- Library/Formula/rsnapshot.rb
+++ Library/Formula/rsnapshot.rb
@@ -7,6 +7,12 @@ class Rsnapshot < Formula
 
   head 'cvs://:pserver:anonymous:@rsnapshot.cvs.sourceforge.net:/cvsroot/rsnapshot:rsnapshot'
 
+  bottle do
+    sha1 '767de92772a7b1e886f28165c2768eed583bec3c' => :mavericks
+    sha1 '82cbc8c852d8dfbc1695475501e1d242797e5f36' => :mountain_lion
+    sha1 '2db00af346c37972eb6d73f7bae6320a6b479d03' => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
