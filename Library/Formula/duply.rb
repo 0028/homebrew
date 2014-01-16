5f7ccf6b08a47664548e193a00bb6a922b192d2c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 16:17:12 +0000

    duply: add 1.6.0 bottle.

--- Library/Formula/duply.rb
+++ Library/Formula/duply.rb
@@ -5,6 +5,13 @@ class Duply < Formula
   url 'http://downloads.sourceforge.net/project/ftplicity/duply%20(simple%20duplicity)/1.6.x/duply_1.6.0.tgz'
   sha1 '0e376c331b26b64cf1d537b3d9f417eac393fd92'
 
+  bottle do
+    cellar :any
+    sha1 '7b0953885295469cb2bda33dc333c42606598d7b' => :mavericks
+    sha1 '30f4406cd9db1a839d16e3ac45cec0ab1102c8b1' => :mountain_lion
+    sha1 '624dedfdd2b9001e3aa027753413cb287d935ae1' => :lion
+  end
+
   depends_on 'duplicity'
 
   def install
