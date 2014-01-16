c21bc9f6ca237fddfbee2cb2ab2afa0fd7847b93
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-16 16:17:13 +0000

    proguard: add 4.11 bottle.

--- Library/Formula/proguard.rb
+++ Library/Formula/proguard.rb
@@ -5,6 +5,12 @@ class Proguard < Formula
   url 'http://downloads.sourceforge.net/project/proguard/proguard/4.11/proguard4.11.tar.gz'
   sha256 '90a4dbf8c016fd84da6d48d8b16e450ee07abafbfb7bb6eb5028651a8c255d9e'
 
+  bottle do
+    sha1 'f5a5dd80d8f59a933f7baee8efe8589d35c990f6' => :mavericks
+    sha1 '56b617b1824694ef5b5844af3616dc9b83f15373' => :mountain_lion
+    sha1 '41553e2362b30d5a150ba310facb1ac6d05a6989' => :lion
+  end
+
   def install
     libexec.install 'lib/proguard.jar'
     bin.write_jar_script libexec/'proguard.jar', 'proguard'
