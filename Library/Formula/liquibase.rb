08b2bf2615e871af941e7539d3d7ec8e4f96a7b3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-17 08:06:15 +0000

    liquibase: add 3.1.1 bottle.

--- Library/Formula/liquibase.rb
+++ Library/Formula/liquibase.rb
@@ -5,6 +5,13 @@ class Liquibase < Formula
   url 'http://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.1.1-bin.tar.gz'
   sha1 '48af02bd837eab401236963c5868fc4dae5dfbee'
 
+  bottle do
+    cellar :any
+    sha1 '08d5188a9f0c902e616bf6d033b2ec2fabf3bdbe' => :mavericks
+    sha1 '3c31181829e1a0c024164c6bf1a19df791e80cf9' => :mountain_lion
+    sha1 'f92d644cb2e533c10ed465eefdaacf7b840bc2d9' => :lion
+  end
+
   def install
     rm_f Dir['*.bat']
 
