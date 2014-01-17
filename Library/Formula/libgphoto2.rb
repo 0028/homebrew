e64c06b16823572eed9141f930d670bf7fefe701
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-17 16:02:32 +0000

    libgphoto2: add 2.5.3 bottle.

--- Library/Formula/libgphoto2.rb
+++ Library/Formula/libgphoto2.rb
@@ -5,6 +5,12 @@ class Libgphoto2 < Formula
   url 'http://downloads.sourceforge.net/project/gphoto/libgphoto/2.5.3/libgphoto2-2.5.3.tar.bz2'
   sha1 '42c4d99e8036f4fc113de0daf6b8029e4d81f570'
 
+  bottle do
+    sha1 'c9133fb1c14affd0b063df27a17a62447cdebc9c' => :mavericks
+    sha1 'cc0df8c72b5fdea4397e9e3f1b8452a2e4ca4c4e' => :mountain_lion
+    sha1 '18de2b6e5548edcd1d45be3bc24ec1360ef7f081' => :lion
+  end
+
   option :universal
 
   depends_on 'pkg-config' => :build
