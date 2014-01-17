bcfa71f997c6a9f896e1e0bb95441a47ceb019e6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-17 04:36:16 +0000

    ant: add 1.9.3 bottle.

--- Library/Formula/ant.rb
+++ Library/Formula/ant.rb
@@ -5,6 +5,13 @@ class Ant < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.3-bin.tar.gz'
   sha1 '11a0b936fba02f96b8d737d90c610382232ffea6'
 
+  bottle do
+    cellar :any
+    sha1 '39a04559b1718d192f4a1a46d47f4b1cbc40d8c6' => :mavericks
+    sha1 '364a9d392e2b790e7e4df5859c6b62b9e31c509a' => :mountain_lion
+    sha1 '4fece2081f6b961b909bbd408a56fdfcde4aafe6' => :lion
+  end
+
   keg_only :provided_by_osx if MacOS.version < :mavericks
 
   def install
