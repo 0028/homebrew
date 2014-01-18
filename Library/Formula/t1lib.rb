b5611ce9cc1bb18a92596c6a9ea316311e1029e1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-18 01:38:44 +0000

    t1lib: add 5.1.2 bottle.

--- Library/Formula/t1lib.rb
+++ Library/Formula/t1lib.rb
@@ -5,6 +5,12 @@ class T1lib < Formula
   url 'http://www.ibiblio.org/pub/Linux/libs/graphics/t1lib-5.1.2.tar.gz'
   sha1 '4b4fc22c8688eefaaa8cfc990f0039f95f4287de'
 
+  bottle do
+    sha1 '190f1e1b194301b8d3987fec108931bb5b3ce90c' => :mavericks
+    sha1 'feaca1dd03993c7ab51066f68590ee99cf575be3' => :mountain_lion
+    sha1 '49162b73d6695fe214374ee6c3532951003f2130' => :lion
+  end
+
   def install
     system './configure', "--prefix=#{prefix}"
     system 'make', 'without_doc'
