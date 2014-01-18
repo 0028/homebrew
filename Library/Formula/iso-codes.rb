53834cd6432696847e5e57d7b8ed0f1d08c85598
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-18 22:34:19 +0000

    iso-codes: add 3.50 bottle.

--- Library/Formula/iso-codes.rb
+++ Library/Formula/iso-codes.rb
@@ -5,6 +5,12 @@ class IsoCodes < Formula
   url 'http://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.50.tar.xz'
   sha1 '250d22b2977a6f5a536753d85af85917474cf312'
 
+  bottle do
+    sha1 '7e95502527ceb0cb71189dd86a772cca04485d15' => :mavericks
+    sha1 '0b41b4d7ce624ddd63aac4c3cfa76bf342eea707' => :mountain_lion
+    sha1 '1bd9d04229b8d4cdc5981c8a8192a6722b839b1b' => :lion
+  end
+
   depends_on 'xz' => :build
   depends_on 'gettext' => :build
 
