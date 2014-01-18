c33272dfa34c9d6d0ffa167d75497596e4221bca
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-18 22:34:18 +0000

    yaz: add 5.0.11 bottle.

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -5,6 +5,13 @@ class Yaz < Formula
   url 'http://ftp.indexdata.dk/pub/yaz/yaz-5.0.11.tar.gz'
   sha1 'a7606a281d6e9454797789ce36a9ae39aba66634'
 
+  bottle do
+    cellar :any
+    sha1 'fc6053b771c88de2ce0550c0fd95415e2336382e' => :mavericks
+    sha1 'c455442eae3a1ef0e920a5fa0609073b686eb69e' => :mountain_lion
+    sha1 '7297aabb2e710ea81f2865f70dd21a94885e0fdf' => :lion
+  end
+
   depends_on 'pkg-config' => :build
 
   def install
