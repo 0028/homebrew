3596a491787663334e8ade76106281be0ade14c1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-18 23:07:04 +0000

    primesieve: add 5.0 bottle.

--- Library/Formula/primesieve.rb
+++ Library/Formula/primesieve.rb
@@ -5,6 +5,13 @@ class Primesieve < Formula
   url 'http://dl.bintray.com/kimwalisch/primesieve/primesieve-5.0.tar.gz'
   sha1 '2ffdf45dfd243f456cf6f07f9660fc92eebf9603'
 
+  bottle do
+    cellar :any
+    sha1 '321eee80820b3d471b4e7bdd3c52f7ca98f75731' => :mavericks
+    sha1 'a703c79f50a14bdc91add4057113a66cee68c554' => :mountain_lion
+    sha1 '51f9c2bbedb3a5494888002e0a8cb62adba099cb' => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "CXX=#{ENV.cxx}",
                           "CXXFLAGS=#{ENV.cflags}"
