3cb86b7eb19aba6dc11f35f9186b28c0b6bcd51e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-18 06:52:21 +0000

    little-cms2: add 2.5 bottle.

--- Library/Formula/little-cms2.rb
+++ Library/Formula/little-cms2.rb
@@ -5,6 +5,13 @@ class LittleCms2 < Formula
   url 'http://downloads.sourceforge.net/project/lcms/lcms/2.5/lcms2-2.5.tar.gz'
   sha1 'bab3470471fc7756c5fbe71be9a3c7d677d2ee7b'
 
+  bottle do
+    cellar :any
+    sha1 'ad0d11f07ca396502106af1ab371a8aeefe67a25' => :mavericks
+    sha1 '380b58abfeb0eb855c4a8ebf341d931132707c3e' => :mountain_lion
+    sha1 '616b4b68a7edfa08b00a7b1aef7d484e83775d63' => :lion
+  end
+
   depends_on 'jpeg' => :recommended
   depends_on 'libtiff' => :recommended
 
