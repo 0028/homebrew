3f6d21b92558633b846be51aa00d13f81193843a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-19 18:52:30 +0000

    swig: add 2.0.11 bottle.

--- Library/Formula/swig.rb
+++ Library/Formula/swig.rb
@@ -6,6 +6,12 @@ class Swig < Formula
   sha1 'd3bf4e78824dba76bfb3269367f1ae0276b49df9'
   head 'https://github.com/swig/swig.git'
 
+  bottle do
+    sha1 "f0663d84825501eb807ea10153ab1fcbe4da7cac" => :mavericks
+    sha1 "8014cb9ffa272e45092d32d9e3c13033630af210" => :mountain_lion
+    sha1 "698153a1137ae396a6852001c6b858865b0d3099" => :lion
+  end
+
   option :universal
 
   depends_on 'pcre'
