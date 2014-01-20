136edcddaa0fd9c0b17341670cd33ac3e2f3e04d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-20 04:12:24 +0000

    ori: add 0.8.1 bottle.

--- Library/Formula/ori.rb
+++ Library/Formula/ori.rb
@@ -5,6 +5,13 @@ class Ori < Formula
   url 'https://bitbucket.org/orifs/ori/downloads/ori-0.8.1.tgz'
   sha1 '3ad31ab14bdb9305423f66cb919fdc445215f3c9'
 
+  bottle do
+    cellar :any
+    sha1 "65eaf015a40439e9dded5469f4e760c427667823" => :mavericks
+    sha1 "ef6490d7cc0694478092515c0ad68114205f6887" => :mountain_lion
+    sha1 "24bb5d449da49340c1ce0e838c667c2e366aa330" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'scons' => :build
   depends_on 'boost'
