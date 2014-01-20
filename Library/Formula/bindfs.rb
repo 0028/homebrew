59921ce386c99f2a405cc39653ebf07b5fc25de0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-20 02:07:59 +0000

    bindfs: add 1.12 bottle.

--- Library/Formula/bindfs.rb
+++ Library/Formula/bindfs.rb
@@ -5,6 +5,13 @@ class Bindfs < Formula
   url 'http://bindfs.googlecode.com/files/bindfs-1.12.tar.gz'
   sha1 'e4d5d9ab3056c06e1e8b9fe423222de86cce46cf'
 
+  bottle do
+    cellar :any
+    sha1 "0f07a3098159f960ca9c28858ef75a4709cdb33e" => :mavericks
+    sha1 "d6e4ff602e30baca114a2af35e6b8d09cb730e3e" => :mountain_lion
+    sha1 "ab31820287538acf9a4de3802eaa213257c25b32" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'osxfuse'
 
