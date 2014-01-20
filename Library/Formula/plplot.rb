ce6076f5d91a8db2b6f73d5ec1cccb3ca9304a6b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-20 02:17:33 +0000

    plplot: add 5.9.11 bottle.

--- Library/Formula/plplot.rb
+++ Library/Formula/plplot.rb
@@ -5,6 +5,12 @@ class Plplot < Formula
   url 'http://downloads.sourceforge.net/project/plplot/plplot/5.9.11%20Source/plplot-5.9.11.tar.gz'
   sha1 'cfe7e8085ca054d484b24598636d4a5dcbf357c6'
 
+  bottle do
+    sha1 "465b692711067de4e250580ee860ad05831bcf25" => :mavericks
+    sha1 "497a2ae1e931c18eaa8837fe5fb95cf1d511b6ac" => :mountain_lion
+    sha1 "850292da048012ee8e102b494364f16dd24c4838" => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
   depends_on 'pango'
