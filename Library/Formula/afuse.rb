d5bb0b1e302cc12ee5a8f3abdb770b35047bff86
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-20 02:07:59 +0000

    afuse: add 0.4.1 bottle.

--- Library/Formula/afuse.rb
+++ Library/Formula/afuse.rb
@@ -5,6 +5,13 @@ class Afuse < Formula
   url 'https://afuse.googlecode.com/files/afuse-0.4.1.tar.gz'
   sha1 '156b196a27c181eee8b192e7922fbe3c32c858e3'
 
+  bottle do
+    cellar :any
+    sha1 "9d6a69701c110808d4250958418186ca661061d7" => :mavericks
+    sha1 "da0bcfad5c506505e62732ed7e002d8299a22ed5" => :mountain_lion
+    sha1 "5bfa7edfb029234b122af592e9f3c040a8ca0a08" => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
