7d416f2913f125b298024f2e2c592065ac55e61e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-20 02:08:00 +0000

    wdfs: add 1.4.2 bottle.

--- Library/Formula/wdfs.rb
+++ Library/Formula/wdfs.rb
@@ -5,6 +5,13 @@ class Wdfs < Formula
   url 'http://noedler.de/projekte/wdfs/wdfs-1.4.2.tar.gz'
   sha1 '71ae2e355d00bc1fbe7093b0a3b15ddc76a74516'
 
+  bottle do
+    cellar :any
+    sha1 "aa3e6492b41a61b191527a880700f43e2f45165c" => :mavericks
+    sha1 "be4266d56cfd9a1022f1c2f636f9dc8fc8603853" => :mountain_lion
+    sha1 "24ee20632f54437f32a8c8e4c650919efda6bae7" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'neon'
