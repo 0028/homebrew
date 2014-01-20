3dea6ad8c2be3a7b17ed5988f0f2de9ee6b4b937
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-20 02:07:59 +0000

    s3-backer: add 1.3.7 bottle.

--- Library/Formula/s3-backer.rb
+++ Library/Formula/s3-backer.rb
@@ -5,6 +5,13 @@ class S3Backer < Formula
   url 'http://s3backer.googlecode.com/files/s3backer-1.3.7.tar.gz'
   sha1 'c75c7e70cb38bcac41d342a2bdb63e9505ab550a'
 
+  bottle do
+    cellar :any
+    sha1 "31c4b039e5405128a4973c3cf40ab6cd9fcc7f01" => :mavericks
+    sha1 "3597fa98a864dda5e024f04f00f400d9d2ee2cd1" => :mountain_lion
+    sha1 "015d81c13de71e8dc7be3ad1ab0dea18fd032ea3" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'osxfuse'
 
