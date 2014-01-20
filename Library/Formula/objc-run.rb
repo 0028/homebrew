8d871efc77f809a12f159566f4f210e7b42a5bf6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-20 16:37:50 +0000

    objc-run: add 1.4 bottle.

--- Library/Formula/objc-run.rb
+++ Library/Formula/objc-run.rb
@@ -6,6 +6,13 @@ class ObjcRun < Formula
   sha1 '86c15705359f2dc2c62d70e358caf63c2a468fee'
   head 'https://github.com/iljaiwas/objc-run.git'
 
+  bottle do
+    cellar :any
+    sha1 "ed45a23fc44fa393f999a5da876c226d0c78e723" => :mavericks
+    sha1 "55583ee26b1731cfaf68603cedf06e1f31ca90b0" => :mountain_lion
+    sha1 "b8fb08dbf62d3b7e22b20d4510bd90cc55066676" => :lion
+  end
+
   def install
     bin.install 'objc-run'
     (share+'objc-run').install Dir['examples', 'test.bash']
