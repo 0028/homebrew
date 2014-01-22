25a7f68ed21be3232c03bb487bd0f6a31176b1d1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-22 01:36:03 +0000

    hevea: add 2.12 bottle.

--- Library/Formula/hevea.rb
+++ Library/Formula/hevea.rb
@@ -5,6 +5,12 @@ class Hevea < Formula
   url 'http://hevea.inria.fr/distri/hevea-2.12.tar.gz'
   sha1 '21ca8c366a3fd1030b3cfba3cd2aa16e6baf5c56'
 
+  bottle do
+    sha1 "ee061d92ca502541d3dd89ca8d348a5a347d5311" => :mavericks
+    sha1 "f20cb72303792d1a6bdae822a031fca082da23c6" => :mountain_lion
+    sha1 "991e4cf26b7e6c3f894cfb59ce9da62532b88dfe" => :lion
+  end
+
   depends_on 'objective-caml'
   depends_on 'ghostscript' => :optional
 
