c953dec887a5cf7f67e594809f39d39b14800ba2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-22 19:23:12 +0000

    s3cmd: add 1.0.1 bottle.

--- Library/Formula/s3cmd.rb
+++ Library/Formula/s3cmd.rb
@@ -6,6 +6,13 @@ class S3cmd < Formula
   sha1 '4a6f7bfb9300b728ca466967b91aa07521ef6f80'
   head 'https://github.com/s3tools/s3cmd.git'
 
+  bottle do
+    cellar :any
+    sha1 "4cdd07df0d3424c363b5ad15790af1dccef87c43" => :mavericks
+    sha1 "270a938642e250908f7688d0109df70b5f14a56b" => :mountain_lion
+    sha1 "9e22264ac3c493452e38a3484085cb16ceb29965" => :lion
+  end
+
   devel do
     url 'https://github.com/s3tools/s3cmd/archive/v1.5.0-beta1.zip'
     sha1 'ce5738c8d389b705b5a1283da6356df92673863b'
