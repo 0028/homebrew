147ebcaa6e1badd864520db96935f06943bb764a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-22 18:28:29 +0000

    libvorbis: add 1.3.4 bottle.

--- Library/Formula/libvorbis.rb
+++ Library/Formula/libvorbis.rb
@@ -5,6 +5,13 @@ class Libvorbis < Formula
   url 'http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.xz'
   sha1 'b99724acdf3577982b3146b9430d765995ecf9e1'
 
+  bottle do
+    cellar :any
+    sha1 "4fd9ca85cdcf26bcac17fd4bc64b0e27d5ecb1f0" => :mavericks
+    sha1 "4f0059ea8a92354a92121f98d11f57ee59517150" => :mountain_lion
+    sha1 "660bb9411e0f16a4ca937d05949d6447d82bbd59" => :lion
+  end
+
   head do
     url 'http://svn.xiph.org/trunk/vorbis'
 
