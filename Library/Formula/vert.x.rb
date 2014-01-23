ef5b16a0d2564fb75e3b1c5d67291986b2fc22b3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-23 11:58:09 +0000

    vert.x: add 2.1M3 bottle.

--- Library/Formula/vert.x.rb
+++ Library/Formula/vert.x.rb
@@ -6,6 +6,13 @@ class VertX < Formula
   sha1 'f880cb78b116f27c303021d2eaa8e6fd00c2850b'
   version '2.1M3'
 
+  bottle do
+    cellar :any
+    sha1 "eccaf6cbdb6fae1c61c85e6be60f96edfd458e21" => :mavericks
+    sha1 "831cfe43e2dd9c0155622065c68f8a40eebac734" => :mountain_lion
+    sha1 "749e72c2d84eef90652604ed470628d75e0767f9" => :lion
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin client conf lib]
