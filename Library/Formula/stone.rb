6dd138b0733e63d1c7f7fadb9ba48ee574802f15
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-23 02:17:10 +0000

    stone: add 2.3e bottle.

--- Library/Formula/stone.rb
+++ Library/Formula/stone.rb
@@ -5,6 +5,13 @@ class Stone < Formula
   url 'http://www.gcd.org/sengoku/stone/stone-2.3e.tar.gz'
   sha1 '1ed12f29c1f896c1a94237b30201c615af5bb0f8'
 
+  bottle do
+    cellar :any
+    sha1 "907cfe301a1c318e0dd95cee2be927c0ac918d7e" => :mavericks
+    sha1 "f0afe398467227c7373293dbe8f8abe42bdb473a" => :mountain_lion
+    sha1 "8bc2be2858d987ffb598b103441c0067dffb4735" => :lion
+  end
+
   option 'with-ssl', 'SSL support'
 
   def install
