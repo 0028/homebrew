21674cc02222ef0f8e92a66fee40f7ba73160e04
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-23 05:36:18 +0000

    ifuse: add 1.1.2 bottle.

--- Library/Formula/ifuse.rb
+++ Library/Formula/ifuse.rb
@@ -5,6 +5,13 @@ class Ifuse < Formula
   url 'http://www.libimobiledevice.org/downloads/ifuse-1.1.2.tar.bz2'
   sha1 '885d88b45edb85c38b0ce9863f0d45fd378b5614'
 
+  bottle do
+    cellar :any
+    sha1 "6de01ffcb2cded792bd759db22aff695de2066e0" => :mavericks
+    sha1 "a24258766013e152139f59f46fac160a58435742" => :mountain_lion
+    sha1 "0d9aef7b18518df86f2169256dc23df246c3c476" => :lion
+  end
+
   head do
     url 'http://cgit.sukimashita.com/ifuse.git'
 
