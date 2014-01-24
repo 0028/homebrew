6ebd262033ec5a63533ad35ed898cd8473fc0fb0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-24 09:37:25 +0000

    irods: add 3.3 bottle.

--- Library/Formula/irods.rb
+++ Library/Formula/irods.rb
@@ -5,6 +5,13 @@ class Irods < Formula
   url 'https://github.com/irods/irods-legacy/archive/3.3.tar.gz'
   sha1 '2e975fcda20b023155dfd53ff098dde0c3995f75'
 
+  bottle do
+    cellar :any
+    sha1 "0431996f00e05c8ebc1a998551680f84218c71ef" => :mavericks
+    sha1 "4b754d49c66cba681ac148ca64aaf17345b2edf3" => :mountain_lion
+    sha1 "e5e191c170f6f46654232009292139dfb9a0e024" => :lion
+  end
+
   conflicts_with 'sleuthkit', :because => 'both install `ils`'
 
   option 'with-fuse', 'Install iRODS FUSE client'
