3d86c5dc85a08576a46f61eae2cab0c108ef01f1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-26 23:42:05 +0000

    x3270: add 3.3.14ga6 bottle.

--- Library/Formula/x3270.rb
+++ Library/Formula/x3270.rb
@@ -5,6 +5,12 @@ class X3270 < Formula
   url 'http://downloads.sourceforge.net/project/x3270/x3270/3.3.14ga6/suite3270-3.3.14ga6-src.tgz'
   sha1 '4788ea7396d4aff04685382e82c440689e8014aa'
 
+  bottle do
+    sha1 "6b4c42d66e41afed16601e643d5ace7f5e6de93a" => :mavericks
+    sha1 "59e18ca8b602f63a23eaf36546df126ab07a4358" => :mountain_lion
+    sha1 "8da07a1348a7724eca21823fa22b0a0d7f6e807a" => :lion
+  end
+
   depends_on :x11
 
   option 'with-c3270', 'Include c3270 (curses-based version)'
