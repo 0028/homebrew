c189f7b1dcca3ee7017b7d4d473cbc65ab82ca01
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-26 21:19:36 +0000

    taskd: add 1.0.0 bottle.

--- Library/Formula/taskd.rb
+++ Library/Formula/taskd.rb
@@ -6,6 +6,12 @@ class Taskd < Formula
   sha1 '5a89406a21be1f95ece03674315b35814fe4f037'
   head 'http://tasktools.org/taskd.git'
 
+  bottle do
+    sha1 "2b300cca9e268911a36e769a7cf8f941b788f09c" => :mavericks
+    sha1 "8bf66d99dde208398c62343e688161775379101d" => :mountain_lion
+    sha1 "36a8305d044672488f54659b2a8dd939d60b0cb2" => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'gnutls'
   depends_on 'ossp-uuid'
