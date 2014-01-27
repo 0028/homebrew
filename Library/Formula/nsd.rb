8f7a059e6730b8d09d6dd5fd12e10156b0fbe8b4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-27 20:32:42 +0000

    nsd: add 4.0.1 bottle.

--- Library/Formula/nsd.rb
+++ Library/Formula/nsd.rb
@@ -5,6 +5,12 @@ class Nsd < Formula
   url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-4.0.1.tar.gz'
   sha1 '788cc290ade7fc6a61fe4391837d3abddbda4df0'
 
+  bottle do
+    sha1 "2666dee18fd2635625d27703e1370e3898686a85" => :mavericks
+    sha1 "b18f97bb915d731b04d4179502acaa5d9246215b" => :mountain_lion
+    sha1 "e56f19c730db0ca031d478bcd557d027487d9958" => :lion
+  end
+
   option 'with-root-server', 'Allow NSD to run as a root name server'
   option 'with-bind8-stats', 'Enable BIND8-like NSTATS & XSTATS'
   option 'with-ratelimit', 'Enable rate limiting'
