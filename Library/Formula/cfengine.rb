dfb32107c59434e48200357d9e1379a1f8880a7e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-27 22:44:08 +0000

    cfengine: add 3.5.3 bottle.

--- Library/Formula/cfengine.rb
+++ Library/Formula/cfengine.rb
@@ -7,6 +7,13 @@ class Cfengine < Formula
   url 'http://cfengine.com/source-code/download?file=cfengine-3.5.3.tar.gz'
   sha1 '95a03e7bc9e31704d6aac4b3023b9c5037fc33f6'
 
+  bottle do
+    cellar :any
+    sha1 "90600710de4bcba705072544764341427edee5be" => :mavericks
+    sha1 "75929da81af76808a2e5cdf06061e913f4b6b0d4" => :mountain_lion
+    sha1 "e1876c909ff1abbb31a181161b3d117e6c18bdb2" => :lion
+  end
+
   depends_on 'pcre'
   depends_on 'tokyo-cabinet'
   depends_on 'libxml2' if MacOS.version < :mountain_lion
