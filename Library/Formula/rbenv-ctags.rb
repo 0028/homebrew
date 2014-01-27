0b52508a5fe0c8fd8c29b78980878d85f18e0fbc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-27 23:37:21 +0000

    rbenv-ctags: add 1.0.1 bottle.

--- Library/Formula/rbenv-ctags.rb
+++ Library/Formula/rbenv-ctags.rb
@@ -7,6 +7,13 @@ class RbenvCtags < Formula
 
   head 'https://github.com/tpope/rbenv-ctags.git'
 
+  bottle do
+    cellar :any
+    sha1 "55b31226a333127bc1da7e1b1e3ecfc3a0a49fb9" => :mavericks
+    sha1 "5179021ba4f372b2faacb26c57b5aa2369275516" => :mountain_lion
+    sha1 "cb7e5405e7c677b65db6474f7c689bf4f01e79fd" => :lion
+  end
+
   depends_on 'rbenv'
   depends_on 'ruby-build'
   depends_on 'ctags'
