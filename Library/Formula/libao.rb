f47dc2e9b301b4f1d64bbc6f487cea1c52eb3d8c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-28 00:12:15 +0000

    libao: add 1.2.0 bottle.

--- Library/Formula/libao.rb
+++ Library/Formula/libao.rb
@@ -5,6 +5,12 @@ class Libao < Formula
   url 'http://downloads.xiph.org/releases/ao/libao-1.2.0.tar.gz'
   sha1 '6b1d2c6a2e388e3bb6ebea158d51afef18aacc56'
 
+  bottle do
+    sha1 "f99dbd0ad01718cca1fe979a03ea44cf4e63a765" => :mavericks
+    sha1 "824966ede3225bab0788a70499a58470665370ba" => :mountain_lion
+    sha1 "c2ca96e5ca4833d67922ed572d1f5c27fbea757f" => :lion
+  end
+
   depends_on 'pkg-config' => :build
 
   def install
