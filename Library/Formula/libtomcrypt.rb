ae67081f21885eee08d60b760c7e07a56f63584f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-29 17:29:27 +0000

    libtomcrypt: add 1.17 bottle.

--- Library/Formula/libtomcrypt.rb
+++ Library/Formula/libtomcrypt.rb
@@ -5,6 +5,13 @@ class Libtomcrypt < Formula
   url 'http://libtom.org/files/crypt-1.17.tar.bz2'
   sha256 'e33b47d77a495091c8703175a25c8228aff043140b2554c08a3c3cd71f79d116'
 
+  bottle do
+    cellar :any
+    sha1 "155eb3c1e06a60c4e7eee8459fdccf48a5d3fd7e" => :mavericks
+    sha1 "4af2323b18d2da7a6e572001fd6c2f31c9c11cf5" => :mountain_lion
+    sha1 "58dc6ebe031a967bd191c51067b3ed759071e6ee" => :lion
+  end
+
   depends_on 'libtommath'
 
   def install
