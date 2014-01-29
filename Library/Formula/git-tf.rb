8532e27264a35a54394329b2e51086af7f66ac82
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-29 17:30:13 +0000

    git-tf: add 2.0.3.20131219 bottle.

--- Library/Formula/git-tf.rb
+++ Library/Formula/git-tf.rb
@@ -5,6 +5,12 @@ class GitTf < Formula
   url 'http://download.microsoft.com/download/A/E/2/AE23B059-5727-445B-91CC-15B7A078A7F4/git-tf-2.0.3.20131219.zip'
   sha1 'a16f98aa1cd6bff2931b2fa361711ca7051258f4'
 
+  bottle do
+    sha1 "6050e127409a47f808e1128713502ab6d86faa0e" => :mavericks
+    sha1 "fe8a26973c1ab3d7bfc02346fa965ea26f8a2354" => :mountain_lion
+    sha1 "7e655f19b6ea48aece4591657426584e564784b5" => :lion
+  end
+
   head do
     url 'https://git01.codeplex.com/gittf', :using => :git
     depends_on 'maven'
