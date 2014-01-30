0ff502037c8175425e4e8fce190467d273ac2f03
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-30 02:49:40 +0000

    sleuthkit: add 4.1.3 bottle.

--- Library/Formula/sleuthkit.rb
+++ Library/Formula/sleuthkit.rb
@@ -5,6 +5,12 @@ class Sleuthkit < Formula
   url 'http://downloads.sourceforge.net/project/sleuthkit/sleuthkit/4.1.3/sleuthkit-4.1.3.tar.gz'
   sha1 '9350bb59bb5fbe41d6e29a8d0494460b937749ef'
 
+  bottle do
+    sha1 "7ed5d9b6d837800d6fc9f8880b99bf4e7a7f55c7" => :mavericks
+    sha1 "007d1563b3119e7caec5cda8d497cb878b88c24a" => :mountain_lion
+    sha1 "e7982c66241fa9388ef771d6ab34f263a91466cd" => :lion
+  end
+
   head do
     url 'https://github.com/sleuthkit/sleuthkit.git'
 
