854ca7f42e102e9a33c404911b2a41636fa33d27
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-30 09:32:18 +0000

    giflib: add 4.1.6 bottle.

--- Library/Formula/giflib.rb
+++ Library/Formula/giflib.rb
@@ -7,6 +7,13 @@ class Giflib < Formula
   url 'http://downloads.sourceforge.net/project/giflib/giflib-4.x/giflib-4.1.6/giflib-4.1.6.tar.bz2'
   sha1 '22680f604ec92065f04caf00b1c180ba74fb8562'
 
+  bottle do
+    cellar :any
+    sha1 "b1b1927ca7e5672202bad281daeac151b7dff3d5" => :mavericks
+    sha1 "7e93ba66fabc291234ea41db30b2e7b89cba6ab6" => :mountain_lion
+    sha1 "71d03c88012d613269f4bfb35a4765c6701cdc3e" => :lion
+  end
+
   option :universal
 
   def install
