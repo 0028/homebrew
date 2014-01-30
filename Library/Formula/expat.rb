bc55d06950623be7c6d004aa337544e7b2f1c0fb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-30 09:32:18 +0000

    expat: add 2.1.0 bottle.

--- Library/Formula/expat.rb
+++ Library/Formula/expat.rb
@@ -5,6 +5,13 @@ class Expat < Formula
   url 'http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz'
   sha1 'b08197d146930a5543a7b99e871cba3da614f6f0'
 
+  bottle do
+    cellar :any
+    sha1 "37f1bb244d17bb83b612f60b626fcd8540c66785" => :mavericks
+    sha1 "7991be0f51a50ad3fe076992df5a26a069e44db8" => :mountain_lion
+    sha1 "ea4b9122b163a0b72517f19f4c594fe9b4b4fc5b" => :lion
+  end
+
   option :universal
 
   def install
