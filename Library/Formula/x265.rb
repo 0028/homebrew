b50e93d90e74d91fe39afd94bc564f97c054a354
Homebrew <brew@robotunicorn.local>
2014-01-31 23:32:43 +0000

    x265: update 0.7 bottle.

--- Library/Formula/x265.rb
+++ Library/Formula/x265.rb
@@ -5,6 +5,13 @@ class X265 < Formula
   url 'https://bitbucket.org/multicoreware/x265/get/0.7.tar.bz2'
   sha1 'c9bec77b86ba131211c7901542228c3d52abe065'
 
+  bottle do
+    cellar :any
+    sha1 "fa26bd99b6ee0709723baaf9c52e2fcb34440e3c" => :mavericks
+    sha1 "9937193d9592868eb4a8aace28604f341bb74532" => :mountain_lion
+    sha1 "44f4ec54379c847b9bcca4cef79d7d36a13f5fc7" => :lion
+  end
+
   head 'https://bitbucket.org/multicoreware/x265', :using => :hg
 
   depends_on 'yasm' => :build
