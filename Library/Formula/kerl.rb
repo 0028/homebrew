408bbeb960ea40200b11b21b4c3be42cbc0473fd
Homebrew <brew@robotunicorn.local>
2014-02-01 11:36:51 +0000

    kerl: update 20140131 bottle.

--- Library/Formula/kerl.rb
+++ Library/Formula/kerl.rb
@@ -6,6 +6,13 @@ class Kerl < Formula
   version '20140131'
   sha1 '9ac26009c2b4b5f059cd5ae338db123127e390e4'
 
+  bottle do
+    cellar :any
+    sha1 "b16e59685b6e62c9fb7f5f184a02cf1bf434ced1" => :mavericks
+    sha1 "bb1c6b7778da6e3971cbd77433e7bcd33f52f965" => :mountain_lion
+    sha1 "cf4574f3241d068e94af51eddfa053b1bd9c33ce" => :lion
+  end
+
   def install
     bin.install 'kerl'
     bash_completion.install 'bash_completion/kerl'
