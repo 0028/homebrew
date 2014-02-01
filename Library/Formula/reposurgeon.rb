55e7ab32d3dcabd38aacdbb2a0a1fe98a20e32a7
Homebrew <brew@robotunicorn.local>
2014-02-01 02:55:24 +0000

    reposurgeon: update 3.1 bottle.

--- Library/Formula/reposurgeon.rb
+++ Library/Formula/reposurgeon.rb
@@ -5,6 +5,13 @@ class Reposurgeon < Formula
   url 'http://www.catb.org/~esr/reposurgeon/reposurgeon-3.1.tar.gz'
   sha256 '34f6ca25ce9ebab477661294dcde943e70621bafb523a0cb28a8b551bb6a532f'
 
+  bottle do
+    cellar :any
+    sha1 "8138fe3e06d33f85296d0b68bbfc8590a8be8d92" => :mavericks
+    sha1 "816e4713aa118d2c3bb5bf1d25d1f56ccc5730a6" => :mountain_lion
+    sha1 "4290bf0e966da85dac1153aef99266411293686a" => :lion
+  end
+
   depends_on 'asciidoc'
   depends_on 'xmlto'
 
