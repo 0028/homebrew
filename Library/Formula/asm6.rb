fc23ac55f064be88cf8b5e1eb7830bb1a69351c7
Homebrew <brew@robotunicorn.local>
2014-02-03 23:27:48 +0000

    asm6: update 1.6 bottle.

--- Library/Formula/asm6.rb
+++ Library/Formula/asm6.rb
@@ -6,6 +6,13 @@ class Asm6 < Formula
   version '1.6'
   sha1 '9fbc1eed740a16107730e7f996d7ddad8277eb4e'
 
+  bottle do
+    cellar :any
+    sha1 "af8e86d478dcfb7e10e76ef184a0ec1d035ff146" => :mavericks
+    sha1 "9c32c7ad99c1d51e41d0ebc0786aaafc0e5a04ff" => :mountain_lion
+    sha1 "4bca103917ef634715f9460854b8b8cbb5f2fd11" => :lion
+  end
+
   def install
     system "#{ENV.cc} -o asm6 asm6.c"
     bin.install "asm6"
