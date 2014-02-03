0ab777a8c76f4e03f2051e2b67a8fdf0260cb982
Homebrew <brew@robotunicorn.local>
2014-02-03 22:49:48 +0000

    nmap: update 6.40 bottle.

--- Library/Formula/nmap.rb
+++ Library/Formula/nmap.rb
@@ -6,6 +6,12 @@ class Nmap < Formula
   url 'http://nmap.org/dist/nmap-6.40.tar.bz2'
   sha1 'ee1bec1bb62045c7c1fc69ff183b2ae9b97bd0eb'
 
+  bottle do
+    sha1 "d8af2a607b7b5c82f0ee473926775b1c35189e6f" => :mavericks
+    sha1 "fd02109bc86c32e1ca894c9b863b6911c28d80af" => :mountain_lion
+    sha1 "c4b59e3015dba87ceeb0013d5e1ef46c0b6a199d" => :lion
+  end
+
   depends_on "openssl"
 
   conflicts_with 'ndiff', :because => 'both install `ndiff` binaries'
