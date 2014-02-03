e55a7fbc1813e712b4941c47768f9f6238842df7
Homebrew <brew@robotunicorn.local>
2014-02-03 22:01:40 +0000

    leptonica: update 1.70 bottle.

--- Library/Formula/leptonica.rb
+++ Library/Formula/leptonica.rb
@@ -5,6 +5,13 @@ class Leptonica < Formula
   url 'http://www.leptonica.com/source/leptonica-1.70.tar.gz'
   sha1 '2c7a893c48aec7844d6b2c7123a0ede7a0d8300e'
 
+  bottle do
+    cellar :any
+    sha1 "91e781c14c53d1f3dec15755242482cc13b81b03" => :mavericks
+    sha1 "4a8007b1a63057fce87123679d575cce679ba7bc" => :mountain_lion
+    sha1 "902877f85361f13975690a158a65bfa5be507cea" => :lion
+  end
+
   option 'check', 'Run the build checks'
 
   depends_on :libpng => :recommended
