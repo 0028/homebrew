1e0924662d8bf58822369c1203989457f7315911
Homebrew <brew@robotunicorn.local>
2014-02-04 15:49:49 +0000

    gettext: update 0.18.3.2 bottle.

--- Library/Formula/gettext.rb
+++ Library/Formula/gettext.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Gettext < Formula
   homepage 'http://www.gnu.org/software/gettext/'
   url 'http://ftpmirror.gnu.org/gettext/gettext-0.18.3.2.tar.gz'
+  bottle do
+    sha1 "b051e525a42aa11242dc80afd19aa914d38b1e4b" => :mavericks
+    sha1 "a1e9a0835d6f2ac2134ac3583e40ac3e4315c5d0" => :mountain_lion
+    sha1 "674f284e9fb6be58df47b788a84eaa5a0c64d195" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/gettext/gettext-0.18.3.2.tar.gz'
   sha256 'd1a4e452d60eb407ab0305976529a45c18124bd518d976971ac6dc7aa8b4c5d7'
 
