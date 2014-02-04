e7cad561a27fc0fa2a83f41ea60fb42bf1874198
Homebrew <brew@robotunicorn.local>
2014-02-04 20:21:57 +0000

    youtube-dl: update 2014.02.04 bottle.

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -5,6 +5,13 @@ class YoutubeDl < Formula
   url 'https://yt-dl.org/downloads/2014.02.04/youtube-dl-2014.02.04.tar.gz'
   sha1 '120be66ea26168e3e5f1a0861bfab905b21782e3'
 
+  bottle do
+    cellar :any
+    sha1 "05a7ffe04c5639aee6f814b10645baadbf1e7b30" => :mavericks
+    sha1 "1e2e8abcb1a87c9258d64aea76d7d105ad48d3b7" => :mountain_lion
+    sha1 "580c13b2cababe2b1f6b932be774dee8279af631" => :lion
+  end
+
   def install
     system "make", "youtube-dl", "PREFIX=#{prefix}"
     bin.install 'youtube-dl'
