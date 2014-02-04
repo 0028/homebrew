3cea79ac7825cac4e4df110d80ea39144ebbf5b2
Homebrew <brew@robotunicorn.local>
2014-02-04 21:01:54 +0000

    fdupes: update 1.51 bottle.

--- Library/Formula/fdupes.rb
+++ Library/Formula/fdupes.rb
@@ -5,6 +5,13 @@ class Fdupes < Formula
   url 'http://fdupes.googlecode.com/files/fdupes-1.51.tar.gz'
   sha1 '8276b39026f57a2f9503d7af18efca0a7d42b8ec'
 
+  bottle do
+    cellar :any
+    sha1 "cff74e27812d6e9e0f9f0fc2576e94572791942b" => :mavericks
+    sha1 "9c4aaeec6b92fe67aaebe98d9ec5041ea0862ae8" => :mountain_lion
+    sha1 "54c47cba85c5617deba26c8ded8864bc6ddb8213" => :lion
+  end
+
   def install
     inreplace "Makefile", "gcc", "#{ENV.cc} #{ENV.cflags}"
     system "make fdupes"
