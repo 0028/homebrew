ce4582b35aef364dfb89c6470fccec036f93cfe0
Homebrew <brew@robotunicorn.local>
2014-02-04 21:41:52 +0000

    htmlcleaner: update 2.7 bottle.

--- Library/Formula/htmlcleaner.rb
+++ Library/Formula/htmlcleaner.rb
@@ -5,6 +5,12 @@ class Htmlcleaner < Formula
   url 'http://downloads.sourceforge.net/project/htmlcleaner/htmlcleaner/htmlcleaner%20v2.7/htmlcleaner-2.7.zip'
   sha1 'dc9032cbce6dfa2abd2eae060aa146a0d6af9d9f'
 
+  bottle do
+    sha1 "2d4e2bcee5ec77e15bc8cf8d873472582f3397bf" => :mavericks
+    sha1 "fbf9ee59e25c029c4ef5cf274d67cacb2ca4570c" => :mountain_lion
+    sha1 "b09f85e9da98f11d66bc0789969e8bc45846fc87" => :lion
+  end
+
   def install
     libexec.install "htmlcleaner-#{version}.jar"
     bin.write_jar_script libexec/"htmlcleaner-#{version}.jar", "htmlcleaner"
