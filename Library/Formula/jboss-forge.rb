82ba8ba19fae063f0d9a8681d926b378b624779a
Homebrew <brew@robotunicorn.local>
2014-02-04 17:57:10 +0000

    jboss-forge: update 1.4.4.Final bottle.

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -6,6 +6,13 @@ class JbossForge < Formula
   version '1.4.4.Final'
   sha1 '7c5218f9ed9c2e4986ee450f3c958d78dc3c09fd'
 
+  bottle do
+    cellar :any
+    sha1 "d0e2e37ef55e54525d8f5bf90ff0ebeb56aeefcc" => :mavericks
+    sha1 "df61797f53c31b563afc7566cdea34765e8e982e" => :mountain_lion
+    sha1 "64a31d8cb6ea3381f02c915f3dd9b8233e4b46e6" => :lion
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w{ bin modules jboss-modules.jar }
