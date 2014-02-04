833752a5f5e31da5584a53388d7a616bf3cb5d0c
Homebrew <brew@robotunicorn.local>
2014-02-04 06:32:20 +0000

    libyaml: update 0.1.4 bottle.

--- Library/Formula/libyaml.rb
+++ Library/Formula/libyaml.rb
@@ -5,6 +5,13 @@ class Libyaml < Formula
   url 'https://bitbucket.org/xi/libyaml/get/0.1.4.tar.gz'
   sha1 'ec13162bff400b4c65f563e5ac226961c8a44446'
 
+  bottle do
+    cellar :any
+    sha1 "2155c5a51fc3a150c2808faab36125e12095a1f2" => :mavericks
+    sha1 "f65fec71c749b98785ebe1cad9a1e3c13c4a63d1" => :mountain_lion
+    sha1 "45a8ea1a57025934864124261d604957275e863f" => :lion
+  end
+
   option :universal
 
   depends_on :autoconf
