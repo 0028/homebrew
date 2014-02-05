ab25547f40dff7c6790097ed36b7920f73ff5678
Homebrew <brew@robotunicorn.local>
2014-02-05 01:11:24 +0000

    duplicity: update 0.6.23 bottle.

--- Library/Formula/duplicity.rb
+++ Library/Formula/duplicity.rb
@@ -5,6 +5,12 @@ class Duplicity < Formula
   url 'http://code.launchpad.net/duplicity/0.6-series/0.6.23/+download/duplicity-0.6.23.tar.gz'
   sha1 '0b8664d55c24957b3b19e903c0d69673f2bfa166'
 
+  bottle do
+    sha1 "dbad7e81934b68e6ef33045fc422d9159878b91e" => :mavericks
+    sha1 "e96bb21c09f672f7932be50e190121095d8cb2ae" => :mountain_lion
+    sha1 "aa0a964b06bf9d260f20758104febf3a697eebcc" => :lion
+  end
+
   depends_on :python
   depends_on 'librsync'
   depends_on 'gnupg'
