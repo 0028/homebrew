c9d61c7ab21b1b064db9e9da3c19465be9d39f52
Homebrew <brew@robotunicorn.local>
2014-02-05 00:21:08 +0000

    cdecl: update 2.5 bottle.

--- Library/Formula/cdecl.rb
+++ Library/Formula/cdecl.rb
@@ -5,6 +5,13 @@ class Cdecl < Formula
   url 'http://cdecl.org/files/cdecl-blocks-2.5.tar.gz'
   sha1 '3fb349970859dfd32a7fb502cecbd3c6d8656af1'
 
+  bottle do
+    cellar :any
+    sha1 "20660bb4e163d839bca2c801272e147e4fad1f3e" => :mavericks
+    sha1 "528a94ef23a571d242dd584bcb419940c89bbd79" => :mountain_lion
+    sha1 "700af256830ce4057bc215d68f934497e4bd12af" => :lion
+  end
+
   def install
     # Fix namespace clash with Lion's getline
     inreplace "cdecl.c", "getline", "cdecl_getline"
