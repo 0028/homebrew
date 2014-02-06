401356d9f2bff329e44ca12de8fa6877eb13552c
Homebrew <brew@robotunicorn.local>
2014-02-06 17:56:59 +0000

    dos2unix: update 6.0.4 bottle.

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -5,6 +5,12 @@ class Dos2unix < Formula
   url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.4.tar.gz'
   sha1 '93d73148c09908a42dcbf5339312c9aa1f18ba7c'
 
+  bottle do
+    sha1 "c62a4913cd379b6303be6b544c7e14ac78019c21" => :mavericks
+    sha1 "b377248570cafecc5105050a1b1c42bfe4ea2475" => :mountain_lion
+    sha1 "5e564922932f9cc20b5e82f70ed037f0f8254b4c" => :lion
+  end
+
   depends_on 'gettext'
 
   devel do
