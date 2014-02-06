d4d4aab90645cb2d30a0c5de75c218c8de5bfd0f
Homebrew <brew@robotunicorn.local>
2014-02-06 09:51:06 +0000

    mysql-connector-c++: update 1.1.3 bottle.

--- Library/Formula/mysql-connector-c++.rb
+++ Library/Formula/mysql-connector-c++.rb
@@ -5,6 +5,13 @@ class MysqlConnectorCxx < Formula
   url 'http://mysql.he.net/Downloads/Connector-C++/mysql-connector-c++-1.1.3.tar.gz'
   sha1 'b817dccf3a4e340b6a972028ceb7eededaaebd6f'
 
+  bottle do
+    cellar :any
+    sha1 "0bc8f29a9a095d8f39b8a6179474c598fc2a1a3d" => :mavericks
+    sha1 "e727869f542bfb8ff8c97cd486f53b4a2d83b068" => :mountain_lion
+    sha1 "519947a5d532c6cccc3a9d0a66a7380bea5ff0ff" => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'boost' => :build
   depends_on 'mysql'
