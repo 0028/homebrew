6b2fa03467708e3f3edb9f24a2a3e9de3f71adab
Homebrew <brew@robotunicorn.local>
2014-02-06 16:27:43 +0000

    cfitsio: update 3.360 bottle.

--- Library/Formula/cfitsio.rb
+++ Library/Formula/cfitsio.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Cfitsio < Formula
   homepage 'http://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html'
   url 'http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3360.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "843ddcdb5cd41e0b50d5bfdb23d7dca136ce9ffe" => :mavericks
+    sha1 "4966c90fed5c9f00d793c40899ab7530e95c9b9e" => :mountain_lion
+    sha1 "0300a3bc392df3cf57090f7369192a79b56a7d15" => :lion
+  end
+
   mirror 'https://downloads.sourceforge.net/project/machomebrew/mirror/cfitsio-3.360.tar.gz'
   sha1 '946dce52528b0e4eb865db75c4e103cdd8cf51f8'
   version '3.360'
