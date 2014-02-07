03fd042b4eaa989c2d329a4a96ea5ec55ad7d373
Homebrew <brew@robotunicorn.local>
2014-02-07 20:32:47 +0000

    cc65: update 2.13.3 bottle.

--- Library/Formula/cc65.rb
+++ Library/Formula/cc65.rb
@@ -6,6 +6,12 @@ class Cc65 < Formula
   url 'ftp://ftp.musoftware.de/pub/uz/cc65/cc65-sources-2.13.3.tar.bz2'
   sha1 '925c6edfcef7057e24ecb0704fa07210faec07bc'
 
+  bottle do
+    sha1 "f75dfe5805bf2ddfdae3d2edba8722f8586e09c3" => :mavericks
+    sha1 "0d8f6e4f8e845b36b4f2aaa123dc96a9729b631c" => :mountain_lion
+    sha1 "b95045ce73a1fb9c99de97330a170d3275e59923" => :lion
+  end
+
   conflicts_with 'grc', :because => 'both install `grc` binaries'
 
   def install
