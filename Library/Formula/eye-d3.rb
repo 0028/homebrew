daf0ece479c8b457d9a2a0501ee1df00b96160d6
Homebrew <brew@robotunicorn.local>
2014-02-07 00:06:54 +0000

    eye-d3: update 0.6.18 bottle.

--- Library/Formula/eye-d3.rb
+++ Library/Formula/eye-d3.rb
@@ -5,6 +5,13 @@ class EyeD3 < Formula
   url 'http://eyed3.nicfit.net/releases/eyeD3-0.6.18.tar.gz'
   sha1 'd8887f7b75306bd293e0b0d46a977e73225ae7b5'
 
+  bottle do
+    cellar :any
+    sha1 "7b1dcbf7a9283cce2847e252b7a3df5f53ab98b5" => :mavericks
+    sha1 "ce4a386f3132306900f6bea9445b8289c5d7b423" => :mountain_lion
+    sha1 "a97cb6e9bf6578a565d75beea2899a676e6cc720" => :lion
+  end
+
   def install
     man1.install "doc/eyeD3.1.in" => "eyeD3.1"
 
