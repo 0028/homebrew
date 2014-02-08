e480a2f16582c1c48e06d2d83a02c7cc1b3de19a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-08 17:58:27 +0000

    pce: update 0.2.2 bottle.

--- Library/Formula/pce.rb
+++ Library/Formula/pce.rb
@@ -7,6 +7,12 @@ class Pce < Formula
 
   head 'git://git.hampa.ch/pce.git'
 
+  bottle do
+    sha1 "549813533a94ade526ecfae13c607b6749617324" => :mavericks
+    sha1 "fdd98fba8aa16ebacc975612acd4e2ae60c30139" => :mountain_lion
+    sha1 "248e1d049b6677ac4150a09d74d83a51fa1a0427" => :lion
+  end
+
   devel do
     url 'http://www.hampa.ch/pub/pce/pre/pce-20131222-fb58522.tar.gz'
     sha1 '318199f4e06bc45d4c5585d77bb2309de3f5095a'
