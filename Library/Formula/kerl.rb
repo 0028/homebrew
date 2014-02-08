8ad5e2c215be9d4d9026f3f49eccbeea348eed7b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-08 21:42:01 +0000

    kerl: update 20140201 bottle.

--- Library/Formula/kerl.rb
+++ Library/Formula/kerl.rb
@@ -6,6 +6,13 @@ class Kerl < Formula
   version '20140201'
   sha1 '9ac26009c2b4b5f059cd5ae338db123127e390e4'
 
+  bottle do
+    cellar :any
+    sha1 "5bfc8b42e072a67be9e61b19df06c58bd8b98592" => :mavericks
+    sha1 "24aa30e6b1453c332f51ee7b04dc7ba1171f221c" => :mountain_lion
+    sha1 "bc78695e6adfdab2560e28ce45b4248fed285f94" => :lion
+  end
+
   def install
     bin.install 'kerl'
     bash_completion.install 'bash_completion/kerl'
