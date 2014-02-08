07909b049c2c57681d2ab5b9a1e754a514880349
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-08 19:57:28 +0000

    ngircd: update 21 bottle.

--- Library/Formula/ngircd.rb
+++ Library/Formula/ngircd.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Ngircd < Formula
   homepage 'http://ngircd.barton.de'
   url 'ftp://ftp.berlios.de/pub/ngircd/ngircd-21.tar.gz'
+  bottle do
+    sha1 "e3a3c54acf5d719b8d8b0a6eebe77fc2f9e30036" => :mavericks
+    sha1 "ab119138d4bab88856b54d7274f54e38dcd47f1c" => :mountain_lion
+    sha1 "cd350ada5cf2a335ab26e2b61393e800900b5b5a" => :lion
+  end
+
   mirror 'http://ngircd.barton.de/pub/ngircd/ngircd-21.tar.gz'
   sha256 '0edbd41304186e43f9d907a7017b40520cc90c2ce29b1339bdcd7622fffe19a0'
 
