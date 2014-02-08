a6e3208dd68d82297a0676037a45d03d11b41ed0
Homebrew <brew@robotunicorn.local>
2014-02-08 12:52:11 +0000

    etcd: update 0.3.0 bottle.

--- Library/Formula/etcd.rb
+++ Library/Formula/etcd.rb
@@ -6,6 +6,12 @@ class Etcd < Formula
   sha1 '0f191070268f7c87ca044ad23179995b9290e3c0'
   head 'https://github.com/coreos/etcd.git'
 
+  bottle do
+    sha1 "aebe5bbf052979796fd20b40c494ab10b956bd61" => :mavericks
+    sha1 "5f372de7ac09fc0f0845b22b4f0ae9c99cc7bb72" => :mountain_lion
+    sha1 "6f6a0fbef839925c7eca67467b5181cbf42708d4" => :lion
+  end
+
   depends_on 'go' => :build
 
   def install
