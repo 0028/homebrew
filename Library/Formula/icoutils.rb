8b9f7a6857a4c2bd83ad5b73df41bc7dc0c21872
Homebrew <brew@robotunicorn.local>
2014-02-08 05:12:19 +0000

    icoutils: update 0.31.0 bottle.

--- Library/Formula/icoutils.rb
+++ Library/Formula/icoutils.rb
@@ -5,6 +5,13 @@ class Icoutils < Formula
   url 'http://savannah.nongnu.org/download/icoutils/icoutils-0.31.0.tar.bz2'
   sha1 '2712acd33c611588793562310077efd2ff35dca5'
 
+  bottle do
+    cellar :any
+    sha1 "2a0469e803091d50e32fb79a2138d0614b5ec228" => :mavericks
+    sha1 "b0564354c2d10d48ec556288d9487b752e45cb71" => :mountain_lion
+    sha1 "b7731129dfa5a6abdb2d0e77b0e496fe848a18b9" => :lion
+  end
+
   depends_on :libpng
 
   def install
