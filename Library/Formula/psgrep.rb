c1371eb0af15b57ceb49eef944c2a54698d2935a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-09 19:07:11 +0000

    psgrep: add 1.0.6 bottle.

--- Library/Formula/psgrep.rb
+++ Library/Formula/psgrep.rb
@@ -7,6 +7,13 @@ class Psgrep < Formula
 
   head 'http://psgrep.googlecode.com/hg/'
 
+  bottle do
+    cellar :any
+    sha1 "30b863360090ead98fe6936db34ab40f7cfdf2a9" => :mavericks
+    sha1 "ff1575b19bf56cadd5c0f131643a0c2145d67238" => :mountain_lion
+    sha1 "4f423c63f7dee7093fff7eb553dc9341126425c3" => :lion
+  end
+
   def install
     bin.install "psgrep"
     man1.install "psgrep.1"
