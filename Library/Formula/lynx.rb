1e8a2a25d861a971f911d17c14aea20c0afa8fc1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-09 15:48:06 +0000

    lynx: add 2.8.7 bottle.

--- Library/Formula/lynx.rb
+++ Library/Formula/lynx.rb
@@ -5,6 +5,12 @@ class Lynx < Formula
   url 'http://lynx.isc.org/release/lynx2.8.7.tar.bz2'
   sha1 'a34978f7f83cd46bd857cb957faa5a9120458afa'
 
+  bottle do
+    sha1 "857044fd386a915c9d7c910d0e5c68fdd7b4cf70" => :mavericks
+    sha1 "5925f8a23f19481fb4f8a3430c18bba3517cb10d" => :mountain_lion
+    sha1 "8688ca86715b08b322a997741b3cea7784521376" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
