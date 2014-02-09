bd602d163bcb765860d8f54940c85a159ec9d475
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-09 14:26:35 +0000

    etcdctl: update 0.3.0 bottle.

--- Library/Formula/etcdctl.rb
+++ Library/Formula/etcdctl.rb
@@ -6,6 +6,12 @@ class Etcdctl < Formula
   sha1 '3bc5fae34d40c1717e82a7af4ed177c62dbe22af'
   head 'https://github.com/coreos/etcdctl.git'
 
+  bottle do
+    sha1 "a5cca1e2eb5b0aaae49cc4f26ff0d9fe32c1467e" => :mavericks
+    sha1 "feb89bc282993c253f8b27c79bed4b6ab23624f0" => :mountain_lion
+    sha1 "bcbd098008659960d8effa7d2e0ac03413ddc384" => :lion
+  end
+
   depends_on 'go' => :build
 
   def install
