d910d0570f4ff949f03a27b328ba65afab6ebb2d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-10 01:55:35 +0000

    groonga: add 4.0.0 bottle.

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -5,6 +5,12 @@ class Groonga < Formula
   url 'http://packages.groonga.org/source/groonga/groonga-4.0.0.tar.gz'
   sha1 '9a64d4c98eda4db563bcaf9464504d24dfdb3ff4'
 
+  bottle do
+    sha1 "c9660c3cdba06030f7f69107b6f0fe82d0f9b03f" => :mavericks
+    sha1 "f3bd19f271becb5c8c15cdfc4e3f5effecfb84ef" => :mountain_lion
+    sha1 "24fe5fa178270010045d4c19b3e55caa1720f842" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
   depends_on 'msgpack'
