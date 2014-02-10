563d005af1c1ab3f9be4cb58baaa2fd6c28c5d41
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-10 13:12:22 +0000

    boot2docker: add 0.5.4 bottle.

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -5,6 +5,13 @@ class Boot2docker < Formula
   url 'https://github.com/steeve/boot2docker/archive/v0.5.4.tar.gz'
   sha1 '881630491b335230d62a679b8dfbb52c3e249fb8'
 
+  bottle do
+    cellar :any
+    sha1 "9390eb2df380674c0a2ce94dad723bcbaf8fc06c" => :mavericks
+    sha1 "686df36fc50b078e2cc7952699f8e0ed50da5320" => :mountain_lion
+    sha1 "0e5034edc3b88be3b0bbdb97c0c5d535a38e4b33" => :lion
+  end
+
   depends_on "docker"
 
   def install
