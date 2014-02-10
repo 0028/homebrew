831af80a611e804e79ec652447b7585767958913
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-10 17:28:26 +0000

    libssh: add 0.6.1 bottle.

--- Library/Formula/libssh.rb
+++ Library/Formula/libssh.rb
@@ -5,6 +5,12 @@ class Libssh < Formula
   url 'http://git.libssh.org/projects/libssh.git/snapshot/libssh-0.6.1.tar.gz'
   sha1 '2ad5f0afc9983291d3639812212f0f8babc1ba81'
 
+  bottle do
+    sha1 "44c674c2f5472e590973f6bee2dae1645d066d53" => :mavericks
+    sha1 "9528741b1a90725aeaa51ff725014b8ba015cff1" => :mountain_lion
+    sha1 "1163d974126b8d017fca007cf456df22363d2ff1" => :lion
+  end
+
   depends_on 'cmake' => :build
 
   def install
