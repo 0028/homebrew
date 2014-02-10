eb438edd789bc03a602b581262c7eefd0887b8e3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-10 14:37:42 +0000

    wrk: add 3.1.0 bottle.

--- Library/Formula/wrk.rb
+++ Library/Formula/wrk.rb
@@ -6,6 +6,13 @@ class Wrk < Formula
   sha1 '3ddc1e591a846b92899a534abf4bc49d2c6cd98e'
   head 'https://github.com/wg/wrk.git'
 
+  bottle do
+    cellar :any
+    sha1 "fc53ca6e999d01e92f6046a01aab603131b83f80" => :mavericks
+    sha1 "0d0f982a8f065c57eada5aececa02d59e30ca7d2" => :mountain_lion
+    sha1 "083fba8b6773f4c8f0aed6b52d8f84321edd8fc4" => :lion
+  end
+
   depends_on 'openssl'
 
   conflicts_with 'wrk-trello', :because => 'both install `wrk` binaries'
