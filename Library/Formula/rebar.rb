fdfb2c51659401d4ccea0d4f71fcc0e28137d781
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-10 22:39:52 +0000

    rebar: add 2.2.0 bottle.

--- Library/Formula/rebar.rb
+++ Library/Formula/rebar.rb
@@ -5,6 +5,13 @@ class Rebar < Formula
   url 'https://github.com/rebar/rebar/archive/2.2.0.zip'
   sha1 '48e3b6e4293129b990c4e35dc0be5f822c66e08d'
 
+  bottle do
+    cellar :any
+    sha1 "fa948cad98565d58ab3eaf51c9383d7581c9401e" => :mavericks
+    sha1 "7875f4b50e6f8dd0bf2719e85702d9be2640bfce" => :mountain_lion
+    sha1 "5457ff2fa1eda66167c54088b10a87f4f7cba5ec" => :lion
+  end
+
   head "https://github.com/basho/rebar.git", :branch => "master"
 
   depends_on 'erlang'
