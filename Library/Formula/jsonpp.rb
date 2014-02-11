0f4b8eb858392ea6c3280a699e81bef596ee69ad
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-11 01:14:21 +0000

    jsonpp: add 1.2.0 bottle.

--- Library/Formula/jsonpp.rb
+++ Library/Formula/jsonpp.rb
@@ -6,6 +6,13 @@ class Jsonpp < Formula
   version '1.2.0'
   sha1 '422d5b2cefa92923d2fbef9afe1324d72134509e'
 
+  bottle do
+    cellar :any
+    sha1 "b065285a7b4b668ce7b403a8041b747528c2d2b5" => :mavericks
+    sha1 "fe557c110dc20cb4bee14be705b494ad9b52aa8a" => :mountain_lion
+    sha1 "d51cc69ce06153433ced256722bf3af7c7acfd78" => :lion
+  end
+
   def install
     bin.install 'jsonpp'
   end
