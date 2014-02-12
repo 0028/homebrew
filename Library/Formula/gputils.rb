528bf2e9db242ad3a73424581d300614de688616
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 20:23:12 +0000

    gputils: add 1.2.0 bottle.

--- Library/Formula/gputils.rb
+++ Library/Formula/gputils.rb
@@ -5,6 +5,12 @@ class Gputils < Formula
   url 'http://downloads.sourceforge.net/project/gputils/gputils/1.2.0/gputils-1.2.0.tar.gz'
   sha1 '9f3a5d9ee7e2f4f897cd5f8ac56d6679b7c4faba'
 
+  bottle do
+    sha1 "8d2ff751888f6eda93c5ed2624a3f99e26ba284a" => :mavericks
+    sha1 "cb6a3484c68d14feba6c8d95b7ff400ed1311982" => :mountain_lion
+    sha1 "004135fe7a4192d4f0d719163c42bba11999e1dd" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
