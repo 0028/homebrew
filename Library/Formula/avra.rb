551d01cc2d1c52b5c27f539629fa4cfa39bdcb22
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 21:12:39 +0000

    avra: add 1.3.0 bottle.

--- Library/Formula/avra.rb
+++ Library/Formula/avra.rb
@@ -5,6 +5,13 @@ class Avra < Formula
   url 'http://downloads.sourceforge.net/project/avra/1.3.0/avra-1.3.0.tar.bz2'
   sha1 '7ad7d168b02107d4f2d72951155798c2fd87d5a9'
 
+  bottle do
+    cellar :any
+    sha1 "6a9ff58bb501b3438cf24a4a5b1d7ee7fb903009" => :mavericks
+    sha1 "e9173b7383b2bb7261a33a24d173ba2b85906715" => :mountain_lion
+    sha1 "0184b38c56f8c527adea2e5e4b74b797b16413c0" => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
 
