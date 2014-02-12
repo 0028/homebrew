d994d52cd5d44369b4a810823e246225f7c24556
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 18:18:15 +0000

    s3fs: add 1.76 bottle.

--- Library/Formula/s3fs.rb
+++ Library/Formula/s3fs.rb
@@ -5,6 +5,13 @@ class S3fs < Formula
   url 'https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.76.tar.gz'
   sha1 '478aa3230b5d85bfe95d9962ee2f1d8cd35fa070'
 
+  bottle do
+    cellar :any
+    sha1 "af3fb1b76982e594b3699a10fa37dd2bf4037aca" => :mavericks
+    sha1 "affb93d6de2c157abc2ee73eb97c87647aa4cd18" => :mountain_lion
+    sha1 "4ca9229c65fd17ac63792cb61721d0b0ffbbf85d" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on :autoconf
   depends_on :automake
