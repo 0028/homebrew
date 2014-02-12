ee925d62252b583288720b48378b00c0d06952c5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 04:38:40 +0000

    lorem: add 0.6.1 bottle.

--- Library/Formula/lorem.rb
+++ Library/Formula/lorem.rb
@@ -6,6 +6,13 @@ class Lorem < Formula
   version '0.6.1'
   sha1 'aa6ef66e5ee1151397f19b358d772af316cf333b'
 
+  bottle do
+    cellar :any
+    sha1 "6ea5fa1af4aeb3ae2c7a57326f89efa6192b3cd9" => :mavericks
+    sha1 "617896156f683b1acb00eac00c79ea7dc7adea71" => :mountain_lion
+    sha1 "decd35d89d870c947197156de22f7ecfc300763c" => :lion
+  end
+
   def install
     inreplace "lorem", "!/usr/bin/python", "!/usr/bin/env python"
     bin.install "lorem"
