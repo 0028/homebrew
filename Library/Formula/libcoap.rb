5b5d4627e038fa4a0fff5e91f829630a36f1462f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 09:35:24 +0000

    libcoap: add 4.1.1 bottle.

--- Library/Formula/libcoap.rb
+++ Library/Formula/libcoap.rb
@@ -5,6 +5,13 @@ class Libcoap < Formula
   url 'http://downloads.sourceforge.net/project/libcoap/coap-18/libcoap-4.1.1.tar.gz'
   sha1 '137c0bfe25c735b4a85dde65fdded13e8ef0f48f'
 
+  bottle do
+    cellar :any
+    sha1 "70b478a670a94af742d512a1b18183f365759b11" => :mavericks
+    sha1 "14d2db8386987be704a05e0efb0824301fad0531" => :mountain_lion
+    sha1 "540ab888e39fd7d3bbfb6c07e402e7e8dfcf633b" => :lion
+  end
+
   depends_on 'doxygen' => :build
 
   def install
