7ede1c3000472b79815cf7a0e2ca2e5f834a536b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 18:19:00 +0000

    histo: add 0.0.2 bottle.

--- Library/Formula/histo.rb
+++ Library/Formula/histo.rb
@@ -6,6 +6,13 @@ class Histo < Formula
   sha1 "0eec89b51716cb8a91cf9088590909fdf340662c"
   head "https://github.com/visionmedia/histo.git"
 
+  bottle do
+    cellar :any
+    sha1 "0b0f06ee1b2e274824b65b7be1d56aff341572a6" => :mavericks
+    sha1 "84eec4e03c9d963c2be6e0ef5dd8c605c688cbfc" => :mountain_lion
+    sha1 "a334bd6ac6d1bc4e8182c0d5f59e5ec75718fbc0" => :lion
+  end
+
   def install
     system "make"
     bin.install 'histo'
