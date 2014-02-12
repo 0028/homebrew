17ec99bf3300b751e0272fd26f9e10fba5c2460b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 18:19:43 +0000

    rolldice: add 1.14 bottle.

--- Library/Formula/rolldice.rb
+++ Library/Formula/rolldice.rb
@@ -5,6 +5,13 @@ class Rolldice < Formula
   url 'https://github.com/sstrickl/rolldice/archive/v1.14.tar.gz'
   sha1 '56d1abbf6d84b3392f51c2c5a25c7227c2835c5b'
 
+  bottle do
+    cellar :any
+    sha1 "e6559a4a7077afa5a6290f37bf18a2b2d7690352" => :mavericks
+    sha1 "c04d9853830a70584bf0de657ea23d2ef7b06512" => :mountain_lion
+    sha1 "664174ecb2441fbce42e0c5c2df7a0106cef4376" => :lion
+  end
+
   def install
     system "make", "CC=#{ENV.cc}"
     bin.install "rolldice"
