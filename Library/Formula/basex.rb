46bccad8a356323ccdcff38b9a81c807a3c587ea
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-12 21:13:17 +0000

    basex: add 7.8 bottle.

--- Library/Formula/basex.rb
+++ Library/Formula/basex.rb
@@ -6,6 +6,13 @@ class Basex < Formula
   version '7.8'
   sha1 'a0aa765aa5090dfff803a777fee15d3e04bdbfad'
 
+  bottle do
+    cellar :any
+    sha1 "cde1317808b129dfe1d77e70b8e449110b0c6f12" => :mavericks
+    sha1 "0847aac78c86a54101b903e822d8c960aecc0f5d" => :mountain_lion
+    sha1 "e6df71ded49ff7bb72753b564af22f811c64b1f2" => :lion
+  end
+
   def install
     rm Dir['bin/*.bat']
     rm_rf "repo"
