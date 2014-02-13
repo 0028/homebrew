704b92577f82c7059a215d0f72bdd60fac86d8ee
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-13 18:27:18 +0000

    wildfly: add 8.0.0.Final bottle.

--- Library/Formula/wildfly.rb
+++ Library/Formula/wildfly.rb
@@ -5,6 +5,13 @@ class Wildfly < Formula
   url 'http://download.jboss.org/wildfly/8.0.0.Final/wildfly-8.0.0.Final.tar.gz'
   sha1 '594f78aa04dd35c936615563ff3777a67228ba9d'
 
+  bottle do
+    cellar :any
+    sha1 "917f6b02896313861a499f4adb3e1bf089548000" => :mavericks
+    sha1 "0d6154485065e446629d52f6e3206f533bc17f52" => :mountain_lion
+    sha1 "837b6d77f2bdcfc514386060915bd061babd7f3e" => :lion
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install Dir['*']
