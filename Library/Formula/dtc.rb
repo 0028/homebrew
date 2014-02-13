ea375fd72d236f458c19fe7892f9b963a5007195
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-13 12:07:05 +0000

    dtc: add tree bottle.

--- Library/Formula/dtc.rb
+++ Library/Formula/dtc.rb
@@ -5,6 +5,13 @@ class Dtc < Formula
   url "http://ftp.debian.org/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.4.0+dfsg.orig.tar.gz"
   sha1 "2f9697aa7dbc3036f43524a454bdf28bf7e0f701"
 
+  bottle do
+    cellar :any
+    sha1 "39d8aeaa42ce6de22bc4f1d6acc0952cf8d55bce" => :mavericks
+    sha1 "5c13d170f6be8d9aee0a195cae247e1753ee0ba2" => :mountain_lion
+    sha1 "6692808e91e508cc7f9c53366cce27e7391cba3c" => :lion
+  end
+
   def install
     system "make"
     system "make", "DESTDIR=#{prefix}", "PREFIX=", "install"
