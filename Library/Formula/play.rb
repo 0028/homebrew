7488ddd95dbc84449b47e859367084f78cf1210b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-13 08:48:16 +0000

    play: add 2.2.1 bottle.

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -6,6 +6,13 @@ class Play < Formula
   url 'http://downloads.typesafe.com/play/2.2.1/play-2.2.1.zip'
   sha1 'e4567f3cf61536908d66e85bde48d7e953f0a01f'
 
+  bottle do
+    cellar :any
+    sha1 "fee40b9aac89f34bb8ccfcb12cb540206ac03944" => :mavericks
+    sha1 "eb7aee422e4a73a5f20f1b3de4b0b5bac3853d40" => :mountain_lion
+    sha1 "6b5ca0f853e091b545b1ef0326ba16c95e07adb1" => :lion
+  end
+
   conflicts_with 'sox', :because => 'both install `play` binaries'
 
   devel do
