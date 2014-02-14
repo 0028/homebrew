88a627711e1ef0f4d5ab748a21aeb0304862b9d3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-14 04:02:18 +0000

    crm114: add 20100106 bottle.

--- Library/Formula/crm114.rb
+++ Library/Formula/crm114.rb
@@ -5,6 +5,13 @@ class Crm114 < Formula
   url 'http://crm114.sourceforge.net/tarballs/crm114-20100106-BlameMichelson.src.tar.gz'
   sha1 '621106ff14fa66a5a878a2c8fb0251ec483fc17b'
 
+  bottle do
+    cellar :any
+    sha1 "21aa20f92e48a6cab0c6005d99e8c55a70f254dd" => :mavericks
+    sha1 "48b1ae428858e344d6c62243402a3f7aab626a2d" => :mountain_lion
+    sha1 "caf886f0508ebc29eb50e8166f90eefe4b337571" => :lion
+  end
+
   depends_on 'tre'
 
   def install
