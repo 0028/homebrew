825e337dad0d796c8e0df6d34995513ee3ed53bb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-14 15:32:09 +0000

    drush: add 6.2.0 bottle.

--- Library/Formula/drush.rb
+++ Library/Formula/drush.rb
@@ -6,6 +6,12 @@ class Drush < Formula
   url 'https://github.com/drush-ops/drush/archive/6.2.0.tar.gz'
   sha1 '7e13d5264f362ec09efbe8218e13dcd646ba75b3'
 
+  bottle do
+    sha1 "c3438401ab86080f793323fca11aa8002567e107" => :mavericks
+    sha1 "6d8d1248f5a1c81d7ab1f8c8a80b2c6f9febd202" => :mountain_lion
+    sha1 "4ccded25e119806c7e8fc7cbca492389d5098bd2" => :lion
+  end
+
   def install
     prefix.install_metafiles
     libexec.install Dir['*'] -['drush.bat']
