a9956aec4a845a8e408a21cc85844d84e5723543
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-14 07:52:05 +0000

    ruby-install: add 0.4.0 bottle.

--- Library/Formula/ruby-install.rb
+++ Library/Formula/ruby-install.rb
@@ -7,6 +7,13 @@ class RubyInstall < Formula
 
   head 'https://github.com/postmodern/ruby-install.git'
 
+  bottle do
+    cellar :any
+    sha1 "7735c432cb4b7f5092e1b9d82b1d87ac2f2e49db" => :mavericks
+    sha1 "646e3ebb32537601a11925e9a55adc1121c677cf" => :mountain_lion
+    sha1 "878ca85096834a80f897e5137b76d8b3613c9d36" => :lion
+  end
+
   def install
     system 'make', 'install', "PREFIX=#{prefix}"
   end
