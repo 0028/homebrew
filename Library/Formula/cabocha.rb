e4727ce383f601e16df11c98acb3e1249680f015
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-14 18:19:37 +0000

    cabocha: add 0.67 bottle.

--- Library/Formula/cabocha.rb
+++ Library/Formula/cabocha.rb
@@ -5,6 +5,12 @@ class Cabocha < Formula
   url 'http://cabocha.googlecode.com/files/cabocha-0.67.tar.bz2'
   sha1 '457a9bd0d264a1146a5eb1c5a504dd90a8b51fb8'
 
+  bottle do
+    sha1 "5437d57ea060e165e2782cfdd67afbeae635838a" => :mavericks
+    sha1 "cb1972d9ba322adb1c16ab44967305765d337379" => :mountain_lion
+    sha1 "28160d3ecc5ab6e59b6daf4912b3518907779873" => :lion
+  end
+
   depends_on 'crf++'
   depends_on 'mecab'
 
