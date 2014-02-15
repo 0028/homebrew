a268b26c8a937ba0f1506b7c11b6a29d9c53a6b3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:17:36 +0000

    masscan: add 1.0.3 bottle.

--- Library/Formula/masscan.rb
+++ Library/Formula/masscan.rb
@@ -6,6 +6,13 @@ class Masscan < Formula
   sha1 'a10a2475e97c7d2b2999e4c9ce9e19863782dc8f'
   head 'https://github.com/kaizoku/masscan.git'
 
+  bottle do
+    cellar :any
+    sha1 "a6ed76dbdb6b6d0450b257890e08340c98348644" => :mavericks
+    sha1 "20ff2aba75033bdd4a797673b24c4a5148b314bf" => :mountain_lion
+    sha1 "58033b3f9260761567fc45cacabc79736ab0e37c" => :lion
+  end
+
   def install
     system "make"
     bin.install "bin/masscan"
