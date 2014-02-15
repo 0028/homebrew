3ddb5593cb50b4f155ddaa8271acc0f55d090524
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:31:43 +0000

    glm: add 0.9.5.2 bottle.

--- Library/Formula/glm.rb
+++ Library/Formula/glm.rb
@@ -6,6 +6,13 @@ class Glm < Formula
   sha1 '647736ff89bde9543ce20c1a4497674f9fbf6ab9'
   head 'https://github.com/Groovounet/glm.git'
 
+  bottle do
+    cellar :any
+    sha1 "c2e5a2482c2ee48573355d55660b1eac00710f6c" => :mavericks
+    sha1 "396ac38db88b5e3a0d5a50a937b780acbebbf481" => :mountain_lion
+    sha1 "c1bbb9756480021947e2f7e7b5f92eeb2583af0e" => :lion
+  end
+
   def install
     include.install 'glm'
   end
