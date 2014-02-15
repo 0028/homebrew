ea9e70e1e7205baa4f3a661ae40eaaf202e996c8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:31:43 +0000

    ngspice: add 26 bottle.

--- Library/Formula/ngspice.rb
+++ Library/Formula/ngspice.rb
@@ -5,6 +5,12 @@ class Ngspice < Formula
   url 'http://downloads.sourceforge.net/project/ngspice/ng-spice-rework/26/ngspice-26.tar.gz'
   sha1 '7c043c604b61f76ad1941defeeac6331efc48ad2'
 
+  bottle do
+    sha1 "ab14b4f5ffb92c3f23a2e62fc43292972ba72c89" => :mavericks
+    sha1 "1fef813ef69d1df0938b43ce3f515b3736efcd00" => :mountain_lion
+    sha1 "00291274494611ab007603bdaba0e7bd431b4447" => :lion
+  end
+
   option "with-x", "Build with X support"
   option "without-xspice", "Build without x-spice extensions"
 
