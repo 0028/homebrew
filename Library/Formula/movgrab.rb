5dc9c93a56acc7997a81895b651c733f783ad90b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:52:54 +0000

    movgrab: add 1.2.1 bottle.

--- Library/Formula/movgrab.rb
+++ Library/Formula/movgrab.rb
@@ -5,6 +5,13 @@ class Movgrab < Formula
   url 'https://sites.google.com/site/columscode/files/movgrab-1.2.1.tar.gz'
   sha1 'f5f2f115dfdc2bb3d637f12d6b311910826e2123'
 
+  bottle do
+    cellar :any
+    sha1 "6551b10919c65f4641fecb83e4738c5f6740a434" => :mavericks
+    sha1 "f3fe1118b4896246636880252ca88eee76e7e090" => :mountain_lion
+    sha1 "af2237dda7e319174ebade4a8e09680afe4ad1d0" => :lion
+  end
+
   def install
     system './configure', "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system 'make'
