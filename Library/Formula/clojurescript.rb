5617e63f1e575c3a3a9ec3646e2cd150a4c542dc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:17:36 +0000

    clojurescript: add 2156 bottle.

--- Library/Formula/clojurescript.rb
+++ Library/Formula/clojurescript.rb
@@ -6,6 +6,12 @@ class Clojurescript < Formula
   url 'https://github.com/clojure/clojurescript/archive/r2156.tar.gz'
   sha1 'e95d37ed1c93af606a5825826688be801c65194d'
 
+  bottle do
+    sha1 "ecba574f249fdacdb1210526396a56e547f6ef5f" => :mavericks
+    sha1 "f978bcd15806f9b78dc7faf9952380e958e0ae56" => :mountain_lion
+    sha1 "f554f6368d9d58b4efb940d305ff4ee83e5c2544" => :lion
+  end
+
   def install
     system "./script/bootstrap"
     inreplace %w(bin/cljsc script/repl script/repljs script/browser-repl),
