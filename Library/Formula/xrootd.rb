736b24748b59f176507005074d48138413f33aef
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 22:48:06 +0000

    xrootd: add 3.3.6 bottle.

--- Library/Formula/xrootd.rb
+++ Library/Formula/xrootd.rb
@@ -5,6 +5,13 @@ class Xrootd < Formula
   url 'http://xrootd.org/download/v3.3.6/xrootd-3.3.6.tar.gz'
   sha1 'be1fca000a517933c73c68e40cee6203fd2b6ad6'
 
+  bottle do
+    cellar :any
+    sha1 "4d7f398fea8e8e6738a7d45c4df438fe0a6ffa2d" => :mavericks
+    sha1 "f313aacf825306db68beda32b27297dfb9b589fe" => :mountain_lion
+    sha1 "1141dae9f458620f4b813bed08cefa189d58bb49" => :lion
+  end
+
   depends_on 'cmake' => :build
 
   def install
