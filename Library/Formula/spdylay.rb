dbdb6793cfd0e087b67346eea4ddf5979be72238
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:17:36 +0000

    spdylay: add 1.2.3 bottle.

--- Library/Formula/spdylay.rb
+++ Library/Formula/spdylay.rb
@@ -5,6 +5,13 @@ class Spdylay < Formula
   url 'https://github.com/tatsuhiro-t/spdylay/archive/v1.2.3.tar.gz'
   sha1 '96a04068ca0b5df10e37f8d60379b2cb9c5f67d6'
 
+  bottle do
+    cellar :any
+    sha1 "b4ea51a408cc7bfd8fce296a10bff2494a1b5ba3" => :mavericks
+    sha1 "66efafe445a919b0de1d780e5fcb8798dd918eda" => :mountain_lion
+    sha1 "11bc3a716e25a11b6548a87195cebfc3e7a8221b" => :lion
+  end
+
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
