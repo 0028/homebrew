3403c6aa339e9be81dc5f89ee2056a2e54bd5c07
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:17:36 +0000

    libcec: add 2.1.4 bottle.

--- Library/Formula/libcec.rb
+++ Library/Formula/libcec.rb
@@ -5,6 +5,13 @@ class Libcec < Formula
   url 'https://github.com/Pulse-Eight/libcec/archive/libcec-2.1.4.tar.gz'
   sha1 '3ee241201b3650b97ec4fc41b0f5dd33476080f9'
 
+  bottle do
+    cellar :any
+    sha1 "e407a524e1fbebefac7fc5f77e313bfd05bec23d" => :mavericks
+    sha1 "a604ea61aa83adab30fd0e356a1d51952f68d1c0" => :mountain_lion
+    sha1 "cf82b90968203972979ddff06594b25c6dd7477a" => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
