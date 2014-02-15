fb0a0042cb8b415731e65cafd22156146db4e1a2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:31:43 +0000

    finch: add 2.10.9 bottle.

--- Library/Formula/finch.rb
+++ Library/Formula/finch.rb
@@ -5,6 +5,12 @@ class Finch < Formula
   url 'http://downloads.sourceforge.net/project/pidgin/Pidgin/2.10.9/pidgin-2.10.9.tar.bz2'
   sha1 'f3de8fd94dba1f4c98d5402a02430f9f323e665a'
 
+  bottle do
+    sha1 "173f5f6bfe61978404824a7bc713f68ce65f6ac9" => :mavericks
+    sha1 "49afcc1eb9720a18e0c074ebd4e785125d8efa14" => :mountain_lion
+    sha1 "b08ac72c895c4715d345e7f583f9c478bcd7ba66" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
   depends_on 'gettext'
