6291856f0d89571f526f9cd03cea704c5511133b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:31:43 +0000

    cutter: add 1.2.3 bottle.

--- Library/Formula/cutter.rb
+++ Library/Formula/cutter.rb
@@ -5,6 +5,12 @@ class Cutter < Formula
   url 'http://downloads.sourceforge.net/project/cutter/cutter/1.2.3/cutter-1.2.3.tar.gz'
   sha1 '97446319f0a4892433912ab91633b6e0ffa2d09c'
 
+  bottle do
+    sha1 "368ff243c89fb9baad05cce9f93fd5e0a396fd08" => :mavericks
+    sha1 "6dcf31f6227d02022806e707ca860f28c14a9757" => :mountain_lion
+    sha1 "b1b1460237d54e95dbaa70353bbd524e67a0d610" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
   depends_on 'glib'
