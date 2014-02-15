df13389ea8f43aef7374ba985f7375319d4c27a2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 18:17:37 +0000

    n: add 1.2.1 bottle.

--- Library/Formula/n.rb
+++ Library/Formula/n.rb
@@ -6,6 +6,13 @@ class N < Formula
   url 'https://github.com/visionmedia/n/archive/1.2.1.tar.gz'
   sha1 'c8a54313ee8ff43f3b6cdb03f140bc200f548f9f'
 
+  bottle do
+    cellar :any
+    sha1 "a44d9084794af65733c5206315326e331305573d" => :mavericks
+    sha1 "14d57e47d4dc3116cdeffb5c6534e184dc4ab8da" => :mountain_lion
+    sha1 "7125e729f2ed657c915796bc4a87eb6609a21db9" => :lion
+  end
+
   def install
     bin.mkdir
     system "make", "PREFIX=#{prefix}", "install"
