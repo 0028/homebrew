ff3b824803c911abe445407a783d92804a528eb7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-15 19:22:05 +0000

    checkstyle: add 5.7 bottle.

--- Library/Formula/checkstyle.rb
+++ Library/Formula/checkstyle.rb
@@ -5,6 +5,12 @@ class Checkstyle < Formula
   url 'http://downloads.sourceforge.net/project/checkstyle/checkstyle/5.7/checkstyle-5.7-bin.tar.gz'
   sha1 '232d317391b58d118a0102e8ff289fbaebd0064a'
 
+  bottle do
+    sha1 "eee225fe34c45979e38c0192b3e663198850f4e2" => :mavericks
+    sha1 "5198584584226d550f66f5f9c68de38c826415e1" => :mountain_lion
+    sha1 "a1e1f87002ea58f497ba0cd36ada6d920b4e3fa9" => :lion
+  end
+
   def install
     libexec.install "checkstyle-#{version}-all.jar", "sun_checks.xml"
     bin.write_jar_script libexec/"checkstyle-#{version}-all.jar", "checkstyle"
