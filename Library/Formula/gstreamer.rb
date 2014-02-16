34202d63d5aceaa3afb59a744a23a0341071645b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-16 18:44:00 +0000

    gstreamer: add 1.2.3 bottle.

--- Library/Formula/gstreamer.rb
+++ Library/Formula/gstreamer.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Gstreamer < Formula
   homepage 'http://gstreamer.freedesktop.org/'
   url 'http://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.2.3.tar.xz'
+  bottle do
+    sha1 "628f8e79fc64b8d4e747d783a127aff4301c6ec0" => :mavericks
+    sha1 "a20494d33eafc7b3fede3a3b2622a8fe970f4e81" => :mountain_lion
+    sha1 "415dd18f8ab19a1dc2ef1e5d403d9a4417e8007e" => :lion
+  end
+
   mirror 'http://ftp.osuosl.org/pub/blfs/svn/g/gstreamer-1.2.3.tar.xz'
   sha256 '0f9a9817a384b3448c368c23345e5122435caef9c00f1c40d7b1953827b0d8eb'
 
