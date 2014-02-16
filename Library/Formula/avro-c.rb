d31e8ff6218e286eb4e8b0477bcc7cf977eb48d9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-16 04:19:51 +0000

    avro-c: add 1.7.6 bottle.

--- Library/Formula/avro-c.rb
+++ Library/Formula/avro-c.rb
@@ -5,6 +5,13 @@ class AvroC < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=avro/avro-1.7.6/c/avro-c-1.7.6.tar.gz'
   sha1 '890fb6e2fd5c12018e47b8fff49900a361a44a17'
 
+  bottle do
+    cellar :any
+    sha1 "692de30aefe697db5f5db1c7a4d06c53b91a2a23" => :mavericks
+    sha1 "f114ffd32595d6eb0f1f46cf9dbed321c133b74f" => :mountain_lion
+    sha1 "ed4f9a6923d97a61064081a74b8f21433a1121fd" => :lion
+  end
+
   # probably should be an optional dep
   conflicts_with 'xz'
 
