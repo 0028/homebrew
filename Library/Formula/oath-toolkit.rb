3413f943a4c9d94d25830eeba015dee6731f3a5e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-16 04:19:51 +0000

    oath-toolkit: add 2.4.1 bottle.

--- Library/Formula/oath-toolkit.rb
+++ Library/Formula/oath-toolkit.rb
@@ -3,6 +3,12 @@ require 'formula'
 class OathToolkit < Formula
   homepage 'http://www.nongnu.org/oath-toolkit/'
   url 'http://download.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.4.1.tar.gz'
+  bottle do
+    sha1 "2f9573253300eecce2eca48827e14c09d9862ecc" => :mavericks
+    sha1 "2b10ae41d85018bdce4b6c1a3cbc0f447c28bd89" => :mountain_lion
+    sha1 "4826277abcbd4db83e9ba2fc1ebc9cf03edd5254" => :lion
+  end
+
   mirror 'http://download-mirror.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.4.1.tar.gz'
   sha1 'b0ca4c5f89c12c550f7227123c2f21f45b2bf969'
 
