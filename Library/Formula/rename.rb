1bb1f84042133da049b88b06b44229281449e647
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-16 18:52:43 +0000

    rename: add 1.600 bottle.

--- Library/Formula/rename.rb
+++ Library/Formula/rename.rb
@@ -5,6 +5,13 @@ class Rename < Formula
   url 'https://github.com/ap/rename/archive/v1.600.tar.gz'
   sha1 'a7946ce3602e3810aaa70300674ccb26832634ed'
 
+  bottle do
+    cellar :any
+    sha1 "a0b2c50794ec69825d01045464442e91b9eea796" => :mavericks
+    sha1 "f02ea23f5413d58f4dc44c02ee6119f0e255963f" => :mountain_lion
+    sha1 "3e38ba0240ffbf6ca6858a24fab85ba1ba719ef7" => :lion
+  end
+
   def install
     system 'pod2man', 'rename', 'rename.1'
     bin.install 'rename'
