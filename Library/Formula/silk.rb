7c7aadf2360dbd2bb227d39dae5b561d5c5c87d1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-16 04:19:51 +0000

    silk: add 3.8.1 bottle.

--- Library/Formula/silk.rb
+++ Library/Formula/silk.rb
@@ -5,6 +5,12 @@ class Silk < Formula
   url 'http://tools.netsa.cert.org/releases/silk-3.8.1.tar.gz'
   sha1 '85787cd0bf8c7ce9b11013fff2ac3eea1ff5bd13'
 
+  bottle do
+    sha1 "75672884d6e178c110f2abb511ddf27c879cd908" => :mavericks
+    sha1 "7433fbcf314afcbb2fbfcf9a27a9a9353ad252bb" => :mountain_lion
+    sha1 "2494e4b461f761c0ce70f27e9ed8edca77017bb3" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'libfixbuf'
