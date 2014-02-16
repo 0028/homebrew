a0d3faf1d1dbdba7e500356afefc0efcf48bcc85
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-16 22:17:16 +0000

    md5deep: add 4.3 bottle.

--- Library/Formula/md5deep.rb
+++ Library/Formula/md5deep.rb
@@ -5,6 +5,13 @@ class Md5deep < Formula
   url 'http://downloads.sourceforge.net/project/md5deep/md5deep/md5deep-4.3/md5deep-4.3.tar.gz'
   sha1 'b9dd6444f07c9fc344ebef201baebdf71bda337f'
 
+  bottle do
+    cellar :any
+    sha1 "1eec0ef8b6c8acf311aac9fc705549d4f5c2476d" => :mavericks
+    sha1 "3d01f5de5c467ba6830b88cb5e83b78f1e285909" => :mountain_lion
+    sha1 "86239596e42d7d0beb5bcdad563b23b95c62f4cb" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
