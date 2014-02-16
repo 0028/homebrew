36c9c3dc020f78182a34fca901729be1157e576c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-16 04:19:51 +0000

    openconnect: add 5.03 bottle.

--- Library/Formula/openconnect.rb
+++ Library/Formula/openconnect.rb
@@ -5,6 +5,12 @@ class Openconnect < Formula
   url 'ftp://ftp.infradead.org/pub/openconnect/openconnect-5.03.tar.gz'
   sha1 '40344fc910a19c8781a79204808f1229acaee2a4'
 
+  bottle do
+    sha1 "9d91e3eb553bc8d40f0af5606a6a87f01df623b1" => :mavericks
+    sha1 "a318a5dca74cd40fe98f6fcd35dde52dd84075d6" => :mountain_lion
+    sha1 "d69ccb96c7c0ad74b30a3616677b18da29d512f2" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
 
