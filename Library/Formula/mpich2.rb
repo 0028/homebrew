8bac108d9ad72d996cdb676bde185cebd482a082
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-17 21:53:55 +0000

    mpich2: add 3.0.4 bottle.

--- Library/Formula/mpich2.rb
+++ Library/Formula/mpich2.rb
@@ -5,6 +5,12 @@ require 'formula'
 class Mpich2 < Formula
   homepage 'http://www.mpich.org/'
   url 'http://www.mpich.org/static/downloads/3.0.4/mpich-3.0.4.tar.gz'
+  bottle do
+    sha1 "e5a5c61092590f2ed52ad8122a202b019fbbc6c0" => :mavericks
+    sha1 "227ccb4733d948d90ac9ee57ac3452ec6f31e1b8" => :mountain_lion
+    sha1 "f2c01deb47d3e77b01f870037c1bb7a4c685c9a8" => :lion
+  end
+
   mirror 'http://fossies.org/linux/misc/mpich-3.0.4.tar.gz'
   sha1 'e89cc8de89d18d5718f7b881f3835b5a0943f897'
 
