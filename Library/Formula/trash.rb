3da371e7b8ebb514d7d9528a2a344ddcbde00ae2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-17 19:22:13 +0000

    trash: add 0.8.4 bottle.

--- Library/Formula/trash.rb
+++ Library/Formula/trash.rb
@@ -5,6 +5,13 @@ class Trash < Formula
   url 'http://github.com/ali-rantakari/trash/archive/v0.8.4.zip'
   sha1 '233773bcaed903e296f20635808c3dce7ef93c4b'
 
+  bottle do
+    cellar :any
+    sha1 "768f072da2ca4ebee3ee1d77db112f062555c51c" => :mavericks
+    sha1 "ded40d63d253025b775d97735b92f2eaf9dff748" => :mountain_lion
+    sha1 "64396e7894bba72905903ead4f26902124209a93" => :lion
+  end
+
   conflicts_with 'osxutils', :because => 'both install a trash binary'
 
   def install
