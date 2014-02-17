bec612bb5db893c6aa6f7e3cf6237123684f9e89
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-17 23:52:07 +0000

    multitail: add 6.2.1 bottle.

--- Library/Formula/multitail.rb
+++ Library/Formula/multitail.rb
@@ -5,6 +5,12 @@ class Multitail < Formula
   url 'http://www.vanheusden.com/multitail/multitail-6.2.1.tgz'
   sha1 '30bd77c4f542cd6ad6f1a0351f7d9e0a6cef6585'
 
+  bottle do
+    sha1 "29e02c45afe35da4ca80968514dd491de62c361d" => :mavericks
+    sha1 "387e36935e8c2934f7efd4e0710936f36588fe06" => :mountain_lion
+    sha1 "8b22f2eadf94e0145e01229542a1fa2981936403" => :lion
+  end
+
   def install
     ENV['DESTDIR'] = prefix
     system "make", "-f", "makefile.macosx", "multitail"
