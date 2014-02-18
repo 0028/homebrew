8769bb0fbe28d739d820a2948b9b54c0c7c6f346
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-18 17:03:48 +0000

    charm-tools: add 1.2.9-1 bottle.

--- Library/Formula/charm-tools.rb
+++ Library/Formula/charm-tools.rb
@@ -5,6 +5,12 @@ class CharmTools < Formula
   url 'https://launchpad.net/charm-tools/1.2/1.2.9/+download/charm-tools-1.2.9-1.tar.gz'
   sha1 '9da948b85dc2fc547335d28669f694338b0b77e8'
 
+  bottle do
+    sha1 "e2b9b9738a7ae62aa4ee078acde400944aa9062e" => :mavericks
+    sha1 "62624fb23a7b673dfa68eb3b49ab8f3610636a49" => :mountain_lion
+    sha1 "87a0956bbd255b187947dc66cb4829a730bc94ab" => :lion
+  end
+
   depends_on :python
   depends_on 'libyaml'
 
