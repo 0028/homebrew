038aa3d88b6c40ebedbfcef64f58c1597710b45c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-18 16:42:14 +0000

    ncdc: add 1.19 bottle.

--- Library/Formula/ncdc.rb
+++ Library/Formula/ncdc.rb
@@ -5,6 +5,13 @@ class Ncdc < Formula
   url 'http://dev.yorhel.nl/download/ncdc-1.19.tar.gz'
   sha1 '7f478b7daf09202586b40899cc6beabeb0d23178'
 
+  bottle do
+    cellar :any
+    sha1 "a0fe53da7896f6de5ead46286a06588fb861971b" => :mavericks
+    sha1 "3089cba872f438a48c7334009a11d76f33b39b3c" => :mountain_lion
+    sha1 "d4b6905cae2758e972b384e5157de943475e7d77" => :lion
+  end
+
   option 'with-geoip', "Build with geoip support"
 
   depends_on 'glib'
