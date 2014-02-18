11673e66aea5a71f2b1adf7d2eb502a3241dc4f4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-18 19:02:22 +0000

    pngcrush: add 1.7.69 bottle.

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -8,6 +8,13 @@ class Pngcrush < Formula
   url 'http://downloads.sourceforge.net/project/pmt/pngcrush/old-versions/1.7/1.7.69/pngcrush-1.7.69.tar.gz'
   sha1 'ccc2a4c678da44673087dd63c98fafca68b1670d'
 
+  bottle do
+    cellar :any
+    sha1 "c0f5670624219277e975cf61da3461adc60520be" => :mavericks
+    sha1 "d6cb8007bf693b1525ab76eadd97997dbf0bf285" => :mountain_lion
+    sha1 "b6d97704ed807db97c9c424aa4d8f35efbba39e4" => :lion
+  end
+
   def install
     # Required to successfully build the bundled zlib 1.2.6
     ENV.append_to_cflags "-DZ_SOLO"
