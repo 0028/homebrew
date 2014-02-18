5b8a6449b0e05cb24faa55f5b26203800393ed8b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-18 21:39:14 +0000

    mercurial: add 2.9 bottle.

--- Library/Formula/mercurial.rb
+++ Library/Formula/mercurial.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Mercurial < Formula
   homepage 'http://mercurial.selenic.com/'
   url 'http://mercurial.selenic.com/release/mercurial-2.9.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "80f32de5a76d5cb2ccc62494fad12c7a18f11e24" => :mavericks
+    sha1 "081422c3f68e3e2e51ba38b4caf83c2dbc02510d" => :mountain_lion
+    sha1 "8126ab127a977cfa9442a998547854f4dfaf83e3" => :lion
+  end
+
   mirror 'http://fossies.org/linux/misc/mercurial-2.9.tar.gz'
   sha1 '9b7d8259434f6aae29f6eee3dd5665b516857cf3'
 
