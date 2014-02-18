ab14867f126ed68db76c558e9f60ff2925f7312e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-18 16:43:45 +0000

    openrtsp: add 2014.02.17 bottle.

--- Library/Formula/openrtsp.rb
+++ Library/Formula/openrtsp.rb
@@ -5,6 +5,13 @@ class Openrtsp < Formula
   url 'http://live555.com/liveMedia/public/live.2014.02.17.tar.gz'
   sha1 '6a769a3541a6e94a56fd788843240a860d777bb1'
 
+  bottle do
+    cellar :any
+    sha1 "47919d2fc69f3ac7c31729f0e3466a77fd5b5543" => :mavericks
+    sha1 "b39c593b6177c9d23f3c6cb05e25607bff77c061" => :mountain_lion
+    sha1 "95773bd0b5e96a8fc05a0745170a7e168ee42547" => :lion
+  end
+
   option "32-bit"
 
   def install
