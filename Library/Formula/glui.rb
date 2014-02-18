fe3b26a84a425f3e13af9fcfd60e9341890d7b0d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-18 00:57:15 +0000

    glui: add 2.36 bottle.

--- Library/Formula/glui.rb
+++ Library/Formula/glui.rb
@@ -5,6 +5,13 @@ class Glui < Formula
   url 'http://downloads.sourceforge.net/project/glui/Source/2.36/glui-2.36.tgz'
   sha256 'c1ef5e83cf338e225ce849f948170cd681c99661a5c2158b4074515926702787'
 
+  bottle do
+    cellar :any
+    sha1 "4a5c5336a8e5a7396bccfb5613d5a7bd9b2d9e33" => :mavericks
+    sha1 "2721583daa14eb6f48fc717492dbf1229744d048" => :mountain_lion
+    sha1 "d40bd8019ee00e2ed6a131b1b48753dc2cd377a9" => :lion
+  end
+
   # Fix compiler warnings in glui.h. Reported upstream:
   # http://sourceforge.net/p/glui/patches/12/
   def patches; DATA; end
