c5f13a0c360d45570955fcf23530ccf75fb7cdc2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-19 03:07:10 +0000

    youtube-dl: add 2014.02.19.1 bottle.

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -5,6 +5,13 @@ class YoutubeDl < Formula
   url 'https://yt-dl.org/downloads/2014.02.19.1/youtube-dl-2014.02.19.1.tar.gz'
   sha1 '6d02e9b07ffee6b898349bc2532d0ab9e653f6d4'
 
+  bottle do
+    cellar :any
+    sha1 "3011f2fe039c2222b0ff01945d77ed7dc7ce4e30" => :mavericks
+    sha1 "70b2ef3c54eec2f207b790e99f53e89c0ff356af" => :mountain_lion
+    sha1 "1a4a6738c85ecc82956da06eaf6cb4d8ed16d70c" => :lion
+  end
+
   def install
     system "make", "youtube-dl", "PREFIX=#{prefix}"
     bin.install 'youtube-dl'
