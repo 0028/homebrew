47353718c5980d446d9fa922f46c3ca5bc5f6960
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-19 15:42:31 +0000

    tomcat: add 7.0.52 bottle.

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -5,6 +5,13 @@ class Tomcat < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.52/bin/apache-tomcat-7.0.52.tar.gz'
   sha1 'adb171c420a6b0a042e2102a76fb70ac4da06a44'
 
+  bottle do
+    cellar :any
+    sha1 "cd0f7156d5d3da326ccfbec48be8c0c747870b53" => :mavericks
+    sha1 "27bacc6c1f1dc4d079a59625d0250c1af7431bc6" => :mountain_lion
+    sha1 "8f2409ec72d6875af72db03680d6e1b9332b50c0" => :lion
+  end
+
   option "with-fulldocs", "Install full documentation locally"
 
   devel do
