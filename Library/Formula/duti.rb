5b1d8d91cede33af75affcbc3bc6008a43233235
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-19 00:27:12 +0000

    duti: add 1.5.2 bottle.

--- Library/Formula/duti.rb
+++ Library/Formula/duti.rb
@@ -6,6 +6,13 @@ class Duti < Formula
   url 'https://github.com/moretension/duti/archive/duti-1.5.2.tar.gz'
   sha1 '1833c0a56646a132fa09bcb31c557d4393f19a3b'
 
+  bottle do
+    cellar :any
+    sha1 "4f00c0593fb28723880810e5996474e24aeb02c0" => :mavericks
+    sha1 "a09f4b71427a6178f61b89ba32aeab369973ae64" => :mountain_lion
+    sha1 "833ef0363275a7854e2b225c876c80583eda8097" => :lion
+  end
+
   depends_on :autoconf
 
   def install
