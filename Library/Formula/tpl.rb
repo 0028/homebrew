8346bf4dbeefc6acbc23efbca4ba2059bb10c26c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-20 18:35:59 +0000

    tpl: add 1.6.1 bottle.

--- Library/Formula/tpl.rb
+++ Library/Formula/tpl.rb
@@ -6,6 +6,13 @@ class Tpl < Formula
   sha1 "2ee92627e8f67400061d8fc606b601988ed90217"
   head "https://github.com/troydhanson/tpl.git"
 
+  bottle do
+    cellar :any
+    sha1 "a000286df1e78d11d5a4276a599c41ebea0a14dc" => :mavericks
+    sha1 "eb7112d9f3b0a0264b7cb5813dd5b5d76ebb6f36" => :mountain_lion
+    sha1 "80fd43f2f007f5154baf7930ed2468aeec3ff555" => :lion
+  end
+
   option "with-tests", "Verify the build using the test suite."
 
   depends_on :autoconf
