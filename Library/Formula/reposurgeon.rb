3c79e832b2d5fe581ef178e2530ab448e046329e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-20 04:12:50 +0000

    reposurgeon: add 3.6 bottle.

--- Library/Formula/reposurgeon.rb
+++ Library/Formula/reposurgeon.rb
@@ -5,6 +5,13 @@ class Reposurgeon < Formula
   url 'http://www.catb.org/~esr/reposurgeon/reposurgeon-3.6.tar.gz'
   sha256 'c07354319813095f5db6c416fd491eb8efd4d7c67db7161c88fd2c3afe0d5386'
 
+  bottle do
+    cellar :any
+    sha1 "16a03797b63724f96062b9411061c7c8b2e732c5" => :mavericks
+    sha1 "f2df3c3797c8f24fce3ae017253a49f502dc504b" => :mountain_lion
+    sha1 "a05aa6907919a3939436e8d7c846192b690db249" => :lion
+  end
+
   depends_on 'asciidoc'
   depends_on 'xmlto'
 
