8ad7d84c2d6d284f1ff76df7511e2aa98d027883
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-20 10:57:08 +0000

    boost-build: add 1.55.0 bottle.

--- Library/Formula/boost-build.rb
+++ Library/Formula/boost-build.rb
@@ -7,6 +7,13 @@ class BoostBuild < Formula
 
   head 'https://github.com/boostorg/build.git'
 
+  bottle do
+    cellar :any
+    sha1 "ec7c604323b4ca5567c3c689571487b62a87ff6d" => :mavericks
+    sha1 "9f7a28a1f9454c71a5567f615899b3a78c8042c6" => :mountain_lion
+    sha1 "c15aa8370df6f663dbccb751990c13cb6ed7f61d" => :lion
+  end
+
   def install
     if build.head?
       system "./bootstrap.sh"
