dc1d9588ea29c05a532f523ff61083eaf76780bc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-20 11:02:01 +0000

    hr: add 1.1 bottle.

--- Library/Formula/hr.rb
+++ Library/Formula/hr.rb
@@ -5,6 +5,13 @@ class Hr < Formula
   url 'https://github.com/LuRsT/hr/archive/1.1.tar.gz'
   sha1 '72e0a7836fe5181205de816bb5e0d44be3a8961f'
 
+  bottle do
+    cellar :any
+    sha1 "4dd08997dc4651ec959b781521d4ed1613e7dc47" => :mavericks
+    sha1 "0e9eeb28d78f7ca63d1653c087b76224c7fc4639" => :mountain_lion
+    sha1 "73aae0fa5409e6887377a83116e7e3864f6244a1" => :lion
+  end
+
   head 'https://github.com/LuRsT/hr', :branch => 'master'
 
   def install
