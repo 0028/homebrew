4e9bf59d6fba53b749f1caa23c12f17c96743067
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-21 05:01:37 +0000

    lpc21isp: add 1.97 bottle.

--- Library/Formula/lpc21isp.rb
+++ Library/Formula/lpc21isp.rb
@@ -6,6 +6,13 @@ class Lpc21isp < Formula
   sha1 '7e83c13889d56c20ab1a807975f212eac3bfdfd1'
   version '1.97'
 
+  bottle do
+    cellar :any
+    sha1 "c0506230cc80b3b467a50edb0f3792f63f82a8e6" => :mavericks
+    sha1 "86085b897d89ccb94495f57cd50aaf8a88ba3ab1" => :mountain_lion
+    sha1 "fb6558c5b70a545b5ae22f6e59580dbee76b275e" => :lion
+  end
+
   def install
     system "make"
     bin.install ["lpc21isp"]
