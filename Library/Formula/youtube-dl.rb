c136f4e593c0745045d1a0a5f0e83f1820f7bbfd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-21 17:53:17 +0000

    youtube-dl: add 2014.02.21.1 bottle.

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -5,6 +5,13 @@ class YoutubeDl < Formula
   url 'https://yt-dl.org/downloads/2014.02.21.1/youtube-dl-2014.02.21.1.tar.gz'
   sha1 'e40cd96a578c93407d2691520756115a135a443c'
 
+  bottle do
+    cellar :any
+    sha1 "47cbfae7e6b1c22274302fa915049f426df93c3e" => :mavericks
+    sha1 "6324669bb20a70565bc822e302678656d525e1f3" => :mountain_lion
+    sha1 "d9d4a7217c968686e1e801032665198aa27c3c1b" => :lion
+  end
+
   depends_on 'rtmpdump' => :optional
 
   def install
