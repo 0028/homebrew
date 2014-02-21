298418ec9d20080c9d123b1271a7145c6cf418ba
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-21 07:22:16 +0000

    irrlicht: add 1.8.1 bottle.

--- Library/Formula/irrlicht.rb
+++ Library/Formula/irrlicht.rb
@@ -6,6 +6,13 @@ class Irrlicht < Formula
   url 'http://downloads.sourceforge.net/irrlicht/irrlicht-1.8.1.zip'
   sha1 '231cd2cf2eefe43bde7c40537ece055a3d6e09cb'
 
+  bottle do
+    cellar :any
+    sha1 "562765abcbb5b9d7d6977a96378738c3ff129b06" => :mavericks
+    sha1 "25c5068a2ab06688e15f930fef49d28ff460540d" => :mountain_lion
+    sha1 "6f2f15bac9364a00ae243aa00ddd3e70b1d162e4" => :lion
+  end
+
   depends_on :xcode
 
   def install
