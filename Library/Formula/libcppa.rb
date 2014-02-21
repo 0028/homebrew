69c41932a8b694bcb23ea57caa882ce75e3d90c6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-21 17:44:59 +0000

    libcppa: add 0.8.1 bottle.

--- Library/Formula/libcppa.rb
+++ Library/Formula/libcppa.rb
@@ -5,6 +5,13 @@ class Libcppa < Formula
   url 'http://github.com/Neverlord/libcppa/archive/V0.8.1.tar.gz'
   sha1 'd4f096aae2bb72e254ad6df45edf3fb62370acaa'
 
+  bottle do
+    cellar :any
+    sha1 "6a100dc620b046f8790a492467020b6d64cc6d12" => :mavericks
+    sha1 "f6bc519f92c6b69e7a4e5cdae9e5de8a5cdb4fde" => :mountain_lion
+    sha1 "78c2eee261ef013458914f22a272ebee2cccda35" => :lion
+  end
+
   depends_on :macos => :lion
   depends_on 'cmake' => :build
 
