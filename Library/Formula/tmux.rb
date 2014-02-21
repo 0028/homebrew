64a77cd6c2f74a10905a4fee267419fd9336d825
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-21 17:42:36 +0000

    tmux: add 1.9 bottle.

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -5,6 +5,13 @@ class Tmux < Formula
   url 'http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.9/tmux-1.9.tar.gz'
   sha1 '43197e69716a0430a9e856c13df8ceae31783078'
 
+  bottle do
+    cellar :any
+    sha1 "8d7515585f2e20dc477659f83d2cb959cb98ff57" => :mavericks
+    sha1 "2e97e0b51f5a67ea26f1206eb82fecb88f03e736" => :mountain_lion
+    sha1 "e5c09379534e61df5f83fea099015357d6d97956" => :lion
+  end
+
   head do
     url 'git://git.code.sf.net/p/tmux/tmux-code'
 
