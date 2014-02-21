6cb2cc2b056b62b43eb24c4a759e331dd169adfa
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-21 21:11:33 +0000

    chrome-cli: add 1.4.1 bottle.

--- Library/Formula/chrome-cli.rb
+++ Library/Formula/chrome-cli.rb
@@ -7,6 +7,12 @@ class ChromeCli < Formula
 
   head "https://github.com/prasmussen/chrome-cli.git"
 
+  bottle do
+    cellar :any
+    sha1 "8776c7c4b2ff336ae8ff700a2752ba12d8c34910" => :mavericks
+    sha1 "46a67026c24bc43e7395f0db3f826e529c911836" => :mountain_lion
+  end
+
   depends_on :xcode
   depends_on :macos => :mountain_lion # strange error with lion
 
