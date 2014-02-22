0f0929f40e0f27ce18cf063e7b203cc47b15e0bb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 03:37:14 +0000

    libextractor: add 1.3 bottle.

--- Library/Formula/libextractor.rb
+++ Library/Formula/libextractor.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Libextractor < Formula
   homepage 'http://www.gnu.org/software/libextractor/'
   url 'http://ftpmirror.gnu.org/libextractor/libextractor-1.3.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "d2ca870d0703267a0b7aca702aba83af60a040bd" => :mavericks
+    sha1 "67e8f6fe230a85660e0459b70e3c67a78825a28e" => :mountain_lion
+    sha1 "87e3557555a8b1838eefb3d35313ab49ff90c85e" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/libextractor/libextractor-1.3.tar.gz'
   sha1 '613d0b80e83c79c3e05e073bcda0d0d0bd1f3336'
 
