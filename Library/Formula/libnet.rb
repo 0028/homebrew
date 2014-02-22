2ac0706e9626cfab9f282c310b1f5306edd39192
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 19:55:17 +0000

    libnet: add 1.1.6 bottle.

--- Library/Formula/libnet.rb
+++ Library/Formula/libnet.rb
@@ -5,6 +5,13 @@ class Libnet < Formula
   url 'http://downloads.sourceforge.net/project/libnet-dev/libnet-1.1.6.tar.gz'
   sha1 'dffff71c325584fdcf99b80567b60f8ad985e34c'
 
+  bottle do
+    cellar :any
+    sha1 "18228eccdde5cafe8be0fd5279f15eb778ec3eab" => :mavericks
+    sha1 "2bb59666d0940f83f6c63b92ec6fc6ddc87b8120" => :mountain_lion
+    sha1 "a8906edb6a1da7bb73053b2a3cba253a263d2ad1" => :lion
+  end
+
   # MacPorts does an autoreconf to get raw sockets working
   depends_on :automake
   depends_on :autoconf
