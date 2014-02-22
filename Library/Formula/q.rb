8f3a0aca6b5c2a19b4adc6e0f67e55ca12c902ac
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 12:01:29 +0000

    q: add 1.2.0 bottle.

--- Library/Formula/q.rb
+++ Library/Formula/q.rb
@@ -5,6 +5,13 @@ class Q < Formula
   url 'https://github.com/harelba/q/archive/1.2.0.tar.gz'
   sha1 '5c83cb62a9b24f92fe32ef6a137f83191641ff79'
 
+  bottle do
+    cellar :any
+    sha1 "1a69846d6a1fbd14cf5f1b59655b1c8910537d73" => :mavericks
+    sha1 "914e771167586645ba4a2cf3dd3a5a4016cef0a9" => :mountain_lion
+    sha1 "514468b658c4e9283294ad543fe810efc1b82f50" => :lion
+  end
+
   def install
     bin.install 'q'
   end
