5bc31b212fba3f1ec574919882374658d0e30f0c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 10:25:58 +0000

    tiger-vnc: add 1.3.0 bottle.

--- Library/Formula/tiger-vnc.rb
+++ Library/Formula/tiger-vnc.rb
@@ -5,6 +5,12 @@ class TigerVnc < Formula
   url 'http://downloads.sourceforge.net/project/tigervnc/tigervnc/1.3.0/tigervnc-1.3.0.tar.bz2'
   sha1 'eda373336bee1bcfa806df1ea6389d918945a258'
 
+  bottle do
+    sha1 "efca878d19fc3293eb1e3ff59c63ca079137e321" => :mavericks
+    sha1 "a411d2fef9d3bd7074e24c7f4340d8228a4bd697" => :mountain_lion
+    sha1 "ef10282ef66fa73820cce2db66a8bcecf9e3a949" => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'gnutls' => :recommended
   depends_on 'jpeg-turbo'
