ecd3636b5495f1d37ef0e20c8e916e7bd23072d0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 17:32:08 +0000

    libssh2: add 1.4.3 bottle.

--- Library/Formula/libssh2.rb
+++ Library/Formula/libssh2.rb
@@ -5,6 +5,13 @@ class Libssh2 < Formula
   url 'http://www.libssh2.org/download/libssh2-1.4.3.tar.gz'
   sha1 'c27ca83e1ffeeac03be98b6eef54448701e044b0'
 
+  bottle do
+    cellar :any
+    sha1 "0d38ac3b4db82e310fc9b55c847c87c0c797fa3a" => :mavericks
+    sha1 "715c2ea94385aa3a10d645d5999f4a9a9af0ca73" => :mountain_lion
+    sha1 "ad97a3c0e175383d1717e5108497f460252f1b89" => :lion
+  end
+
   depends_on 'openssl' => :recommended
 
   def install
