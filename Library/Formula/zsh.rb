13b5681d75831fdfd9d8d3b5ab837efd716397db
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 22:57:58 +0000

    zsh: add 5.0.5 bottle.

--- Library/Formula/zsh.rb
+++ Library/Formula/zsh.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Zsh < Formula
   homepage 'http://www.zsh.org/'
   url 'http://downloads.sourceforge.net/project/zsh/zsh/5.0.5/zsh-5.0.5.tar.bz2'
+  bottle do
+    sha1 "9db87d89c4edf2a66744a53bb24c29864a27c0b9" => :mavericks
+    sha1 "c2508775487b4dac5ee926e6b1604235a91728e1" => :mountain_lion
+    sha1 "7987b1bdfcdb24c72b5cf6cc3df328e67fa28085" => :lion
+  end
+
   mirror 'http://www.zsh.org/pub/zsh-5.0.5.tar.bz2'
   sha1 '75426146bce45ee176d9d50b32f1ced78418ae16'
 
