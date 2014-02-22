c6b1ee726bb99c3c3619b9282cdecb0d14b6f14c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 17:30:10 +0000

    xvid: add 1.3.2 bottle.

--- Library/Formula/xvid.rb
+++ Library/Formula/xvid.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Xvid < Formula
   homepage 'http://www.xvid.org'
   url 'http://fossies.org/unix/privat/xvidcore-1.3.2.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "744ecfcf206d6915261a0152f7c91276168d37de" => :mavericks
+    sha1 "c76a75402e357334c34fbde45820759ebb141bc7" => :mountain_lion
+    sha1 "d534eeeac7340e16195aac046104e9564fa8b11c" => :lion
+  end
+
   # Official download takes a long time to fail, so set it as the mirror for now
   mirror 'http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz'
   sha1 '56e065d331545ade04c63c91153b9624b51d6e1b'
