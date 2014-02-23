78b9be66d1e4e1029af906d9519d8668fed5e357
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-23 19:25:13 +0000

    typesafe-activator: add 1.0.13 bottle.

--- Library/Formula/typesafe-activator.rb
+++ Library/Formula/typesafe-activator.rb
@@ -5,6 +5,12 @@ class TypesafeActivator < Formula
   url 'http://downloads.typesafe.com/typesafe-activator/1.0.13/typesafe-activator-1.0.13.zip'
   sha1 '34e655823a7931c7f61a978c7d9cbd20881267ac'
 
+  bottle do
+    sha1 "49c0a8e114b0f1a36a927ef077f5165c512857af" => :mavericks
+    sha1 "caf8e312447f94269975a88b0c5e76f9a76c39ce" => :mountain_lion
+    sha1 "c000b3415ee73aa3d4d31b5ba4bbd42add5fd31d" => :lion
+  end
+
   def install
     # remove Windows .bat files
     rm Dir['*.bat']
