0ff0de0fae5d4dc5504562d9a53f39705063ee95
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-23 12:05:19 +0000

    monetdb: add 11.17.9 bottle.

--- Library/Formula/monetdb.rb
+++ Library/Formula/monetdb.rb
@@ -5,6 +5,12 @@ class Monetdb < Formula
   url 'http://www.monetdb.org/downloads/sources/Jan2014/MonetDB-11.17.9.zip'
   sha1 '4669b54fa9a74bba068756ac3902fd8e362a151b'
 
+  bottle do
+    sha1 "6a68751845bcd0c7d58563ae634b6f21ceafc059" => :mavericks
+    sha1 "ac865714ff7f34bd080c4425df923ef33ddac216" => :mountain_lion
+    sha1 "58d480defe5ab75cba7a4f847ffa08bf442ae0e7" => :lion
+  end
+
   head 'http://dev.monetdb.org/hg/MonetDB', :using => :hg
 
   depends_on 'pkg-config' => :build
