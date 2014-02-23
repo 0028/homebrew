77fd7be314a54cd5fe13b0259260969560d6de3c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-23 21:31:46 +0000

    rancid: add 2.3.8 bottle.

--- Library/Formula/rancid.rb
+++ Library/Formula/rancid.rb
@@ -5,6 +5,12 @@ class Rancid < Formula
   url 'ftp://ftp.shrubbery.net/pub/rancid/rancid-2.3.8.tar.gz'
   sha1 '7469d7f9e39e9f86f977f1f0963300e5d183088f'
 
+  bottle do
+    sha1 "1789b63dfea664c4348ccd1943b7be4d3a55fa75" => :mavericks
+    sha1 "5b3af21c2deb5dd6a13cdf5f03202d685e244270" => :mountain_lion
+    sha1 "bbd59ef83d44703653d66abf9787e3e9cfdff77d" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--exec-prefix=#{prefix}", "--mandir=#{man}"
     system "make"
