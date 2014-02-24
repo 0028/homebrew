b71e489b573038a460f188df2d6adfe85fc2845a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-24 15:11:24 +0000

    libidn: add 1.28 bottle.

--- Library/Formula/libidn.rb
+++ Library/Formula/libidn.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Libidn < Formula
   homepage 'http://www.gnu.org/software/libidn/'
   url 'http://ftpmirror.gnu.org/libidn/libidn-1.28.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "23cb37001f1fb723b86b11717fb4df76f58934ff" => :mavericks
+    sha1 "0ee166fdc8fd9a0ad245efd89323868209968235" => :mountain_lion
+    sha1 "866251da871fe76c180f5b7247d4458175778313" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/libidn/libidn-1.28.tar.gz'
   sha256 'dd357a968449abc97c7e5fa088a4a384de57cb36564f9d4e0d898ecc6373abfb'
 
