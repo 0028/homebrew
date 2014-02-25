6ce6300dd3f7bf1ec0c65d6f30d40096d785cfc8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-25 16:06:29 +0000

    gringo: add 4.2.1 bottle.

--- Library/Formula/gringo.rb
+++ Library/Formula/gringo.rb
@@ -5,6 +5,11 @@ class Gringo < Formula
   url 'http://downloads.sourceforge.net/project/potassco/gringo/4.2.1/gringo-4.2.1-source.tar.gz'
   sha1 'e27790e7d27c54ebb993d97debc01df5c2a4ddd6'
 
+  bottle do
+    cellar :any
+    sha1 "3d52d8c19f3864e639594f1658f94cc6aa2d1936" => :mavericks
+  end
+
   depends_on 're2c'  => :build
   depends_on 'scons' => :build
   depends_on 'bison' => :build
