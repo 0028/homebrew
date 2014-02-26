ba71c27ef71066918a7661baba985a842e106e03
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-26 18:41:29 +0000

    vassh: add 0.2 bottle.

--- Library/Formula/vassh.rb
+++ Library/Formula/vassh.rb
@@ -5,6 +5,13 @@ class Vassh < Formula
   url "https://github.com/x-team/vassh/archive/0.2.tar.gz"
   sha1 "b7be90ab0a20f3edebe45dab2f7a048684871068"
 
+  bottle do
+    cellar :any
+    sha1 "57e9af54feea9e2523418574d9d0b79f882c1be1" => :mavericks
+    sha1 "df38ed71919537240eacc3b615b606f90e156c6d" => :mountain_lion
+    sha1 "83415c8e9e0dc5cb0d5a08401e48f0c4fb035159" => :lion
+  end
+
   def install
     bin.install "vassh.sh", "vasshin", "vassh"
   end
