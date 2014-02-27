c4148a3be752b3c4fb5d2ceb381df0729630da0f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-27 21:41:57 +0000

    ktoblzcheck: add 1.45 bottle.

--- Library/Formula/ktoblzcheck.rb
+++ Library/Formula/ktoblzcheck.rb
@@ -5,6 +5,12 @@ class Ktoblzcheck < Formula
   url 'http://downloads.sourceforge.net/project/ktoblzcheck/ktoblzcheck-1.45.tar.gz'
   sha1 'a552012bb219ac24dcdbac977cf94c951bfe31bc'
 
+  bottle do
+    sha1 "6b86a711bc389549cfa69bc3f5458cd7c125ef07" => :mavericks
+    sha1 "397fb987b6295e8e9bd9c0c532510923d9e09adf" => :mountain_lion
+    sha1 "d459e497fb3fff523468ad84de66ccecd1ca8097" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
