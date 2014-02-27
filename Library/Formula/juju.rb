ffb2df1d73612495907f933a383f0c83d4453a2b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-27 21:32:53 +0000

    juju: add 1.16.6 bottle.

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -5,6 +5,12 @@ class Juju < Formula
   url 'https://launchpad.net/juju-core/1.16/1.16.6/+download/juju-core_1.16.6.tar.gz'
   sha1 '0483d7a4d3fda0981f822d540cded855eb5afbda'
 
+  bottle do
+    sha1 "79d508f3c7ab22c4ed597baf2e12f7e318b3c3f3" => :mavericks
+    sha1 "c22acc1b554fdabef1f8a70f674d540aa33bb7b9" => :mountain_lion
+    sha1 "3e25bea63f01d6181fbb7c5ef423822eb5ee1ce8" => :lion
+  end
+
   devel do
     url  'https://launchpad.net/juju-core/trunk/1.17.3/+download/juju-core_1.17.3.tar.gz'
     sha1 'a4579476a6fb83722da869228b887c39e8ec8c7e'
