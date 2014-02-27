5e8fe97ae61d3e6cd0393a1bf0b33ea578a6f5ae
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-27 07:21:40 +0000

    nexus: add 2.7.2-03 bottle.

--- Library/Formula/nexus.rb
+++ Library/Formula/nexus.rb
@@ -6,6 +6,12 @@ class Nexus < Formula
   version '2.7.2-03'
   sha1 '701302e45daaafd058d8044258c7447f18de0058'
 
+  bottle do
+    sha1 "79dcb3c76e00baedfb0c26472babbb73c9a3a177" => :mavericks
+    sha1 "68059d3e25b6b85acbd10c005254371fb9d9b641" => :mountain_lion
+    sha1 "e45819f1561ab47f3a0f208ed57f7ec16c2d1f80" => :lion
+  end
+
   def install
     rm_f Dir['bin/*.bat']
     # Put the sonatype-work directory in the var directory, to persist across version updates
