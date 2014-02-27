833ebbc321f62af84e245147b8d467dabf916bcd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-27 01:36:31 +0000

    hexcurse: add 1.58 bottle.

--- Library/Formula/hexcurse.rb
+++ Library/Formula/hexcurse.rb
@@ -5,6 +5,13 @@ class Hexcurse < Formula
   url 'https://github.com/LonnyGomes/hexcurse/archive/hexcurse-1.58.tar.gz'
   sha1 '6a07324a4782007dcea800b8b545f9f07e8eae8f'
 
+  bottle do
+    cellar :any
+    sha1 "5c76016fe2c0f33e896e9482ccfa8724325e7a73" => :mavericks
+    sha1 "782b45e271134ab30914315aa1ffa9814f2e53eb" => :mountain_lion
+    sha1 "ff4f8f6487e7eb8e81af5902fbd8b86bc723e58c" => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
