980e5cd433d90cd466a25ae1feba78b0365f935a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-28 17:58:38 +0000

    go-app-engine-32: add 1.9.0 bottle.

--- Library/Formula/go-app-engine-32.rb
+++ Library/Formula/go-app-engine-32.rb
@@ -5,6 +5,13 @@ class GoAppEngine32 < Formula
   url 'https://commondatastorage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.0.zip'
   sha1 '08ab3cddd752d7db9c84153e4e93bf2205707935'
 
+  bottle do
+    cellar :any
+    sha1 "edc78ddc4b0b32017ab14f8bbbea36f4ea6faa2d" => :mavericks
+    sha1 "a7ae23100173c3b1aa479235105f7f2c2ff2cddb" => :mountain_lion
+    sha1 "50b3e3b2e2082c2eff07b131026cb3fe1fc6f27d" => :lion
+  end
+
   def install
     cd '..'
     share.install 'go_appengine' => name
