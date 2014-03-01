9d8b595504f6bf15ef2bdf38c1bc0ca42023866a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-01 00:21:29 +0000

    hub: add 1.12.0 bottle.

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -6,6 +6,13 @@ class Hub < Formula
   sha1 '195b7c92b49286623baa4d01ab53b8a898b484a2'
   head 'https://github.com/github/hub.git'
 
+  bottle do
+    cellar :any
+    sha1 "22dd09fc04a0897bcce570fd022d4ed64071b1cf" => :mavericks
+    sha1 "d3559d6a5b23e745cb9fc7ee106573929754c250" => :mountain_lion
+    sha1 "c5a98c10daaeb565a83c781cc93fb35ef6b0d657" => :lion
+  end
+
   option 'without-completions', 'Disable bash/zsh completions'
 
   def install
