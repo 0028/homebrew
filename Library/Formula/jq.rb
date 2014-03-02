db6a285fd62cd31705b3b30039e77fb79935616e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-02 18:44:06 +0000

    jq: add 1.3 bottle.

--- Library/Formula/jq.rb
+++ Library/Formula/jq.rb
@@ -5,6 +5,13 @@ class Jq < Formula
   url 'http://stedolan.github.io/jq/download/source/jq-1.3.tar.gz'
   sha1 'ac1f19e5d9921683af25251e97c2c4bfee895ca2'
 
+  bottle do
+    cellar :any
+    sha1 "c0d42b8b3e93fb8ad2c289488dd6f1ca71c3a957" => :mavericks
+    sha1 "ddebced7fc28afdfc5c36f234dd1da3d9a5e3a72" => :mountain_lion
+    sha1 "8c77890c2829d42a175d0bd2f5edc287e8f0ba28" => :lion
+  end
+
   head do
     url 'https://github.com/stedolan/jq.git'
 
