dd169d9d1abb9d56982c4e707ac5ed5020215ec8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-02 18:44:06 +0000

    htmlcleaner: add 2.7 bottle.

--- Library/Formula/htmlcleaner.rb
+++ Library/Formula/htmlcleaner.rb
@@ -5,6 +5,12 @@ class Htmlcleaner < Formula
   url 'https://downloads.sourceforge.net/project/htmlcleaner/htmlcleaner/htmlcleaner%20v2.7/htmlcleaner-2.7.zip'
   sha1 'dc9032cbce6dfa2abd2eae060aa146a0d6af9d9f'
 
+  bottle do
+    sha1 "27aab69bdcb94e70e3a8f667ae350ef454399f63" => :mavericks
+    sha1 "1119bd5936e0541c4b8081a0168719949058315f" => :mountain_lion
+    sha1 "2d46040fd2b8428473c26c11c25fbe892f53665d" => :lion
+  end
+
   def install
     libexec.install "htmlcleaner-#{version}.jar"
     bin.write_jar_script libexec/"htmlcleaner-#{version}.jar", "htmlcleaner"
