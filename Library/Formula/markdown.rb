0c4dea4bf8a5bb346137bc48abee3fec053cabb7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-02 18:44:06 +0000

    markdown: add 1.0.1 bottle.

--- Library/Formula/markdown.rb
+++ Library/Formula/markdown.rb
@@ -5,6 +5,13 @@ class Markdown < Formula
   url 'http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip'
   sha1 '7e6d1d9224f16fec5631bf6bc5147f1e64715a4b'
 
+  bottle do
+    cellar :any
+    sha1 "14f835d74610979f2197f58f464489d1a4ab11f0" => :mavericks
+    sha1 "fcfe9a3e6cc1934a4d72f565906d21c1d7ce91a2" => :mountain_lion
+    sha1 "68693cf20b561579f0c80e1b323d06e2720cd087" => :lion
+  end
+
   conflicts_with 'discount',
     :because => 'both markdown and discount ship a `markdown` executable.'
 
