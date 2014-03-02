7b9387acd30e970042113288a82bd7b1c0739e41
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-02 18:44:06 +0000

    udis86: add 1.7.2 bottle.

--- Library/Formula/udis86.rb
+++ Library/Formula/udis86.rb
@@ -5,6 +5,13 @@ class Udis86 < Formula
   url 'https://downloads.sourceforge.net/udis86/udis86-1.7.2.tar.gz'
   sha1 'f55dec2d5319aac9d0a7ae2614ddcc7aa73d3744'
 
+  bottle do
+    cellar :any
+    sha1 "16556d89995f0939dbead981577b112c0209b6d9" => :mavericks
+    sha1 "9d6e8172511ae730ca04ec32f6d50ae4d64c9575" => :mountain_lion
+    sha1 "14c64d02ffb353189d4473ff7efe0cc65b97d0b4" => :lion
+  end
+
   option :universal
 
   def install
