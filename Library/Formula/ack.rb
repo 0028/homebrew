ea622e515a23dffe3ae8b30972f877af1c75fb74
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-02 18:44:06 +0000

    ack: add 2.12 bottle.

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -6,6 +6,13 @@ class Ack < Formula
   sha1 '667b5f2dd83143848a5bfa47f7ba848cbe556e93'
   version '2.12'
 
+  bottle do
+    cellar :any
+    sha1 "c078efc059e8a0a391f79f59842990d16b3be18b" => :mavericks
+    sha1 "6977a2ace4a3d5f8d64e5713b0b621a89572439a" => :mountain_lion
+    sha1 "6ed23ce20f99f12eaa714d5c665ddc7e6f64e16f" => :lion
+  end
+
   def install
     bin.install "ack-2.12-single-file" => "ack"
     system "pod2man", "#{bin}/ack", "ack.1"
