f892fecb425b42fccbf4b68724d0dedc9872b97e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-02 03:36:52 +0000

    gawk: add 4.1.0 bottle.

--- Library/Formula/gawk.rb
+++ Library/Formula/gawk.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Gawk < Formula
   homepage 'http://www.gnu.org/software/gawk/'
   url 'http://ftpmirror.gnu.org/gawk/gawk-4.1.0.tar.xz'
+  bottle do
+    sha1 "590c3eb5410fe251ecd2ac54308d9ffc880941ec" => :mavericks
+    sha1 "9e0c07d0869b966f650874c96b0044ee8bb5bef7" => :mountain_lion
+    sha1 "a90b6da3752130deb44fef635143a04dcb65e162" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/gawk/gawk-4.1.0.tar.xz'
   sha1 'caabca3c1a59d05807c826c45a4639b82cad612a'
 
