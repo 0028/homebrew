a4da78c15d5574b0a1d4c954c35f9f560491b5db
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-02 19:11:29 +0000

    deheader: add 0.8 bottle.

--- Library/Formula/deheader.rb
+++ Library/Formula/deheader.rb
@@ -6,6 +6,13 @@ class Deheader < Formula
   sha1 '4527b4675a7b06d728cfa989a3b7844cdf091b40'
   head 'https://git.gitorious.org/deheader/deheader.git'
 
+  bottle do
+    cellar :any
+    sha1 "66c983dfb0963dcd207a093cc1b456611714744e" => :mavericks
+    sha1 "e18ae38ac4347bdbd10418c80bad3ecc2d668362" => :mountain_lion
+    sha1 "be8aa4e2d9aba58a406a24091828263a67ebf271" => :lion
+  end
+
   depends_on :python
 
   def install
