8fa365f6314308f2954df8917ae6867b6435fea2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-04 08:24:55 +0000

    freeipmi: add 1.4.1 bottle.

--- Library/Formula/freeipmi.rb
+++ Library/Formula/freeipmi.rb
@@ -5,6 +5,12 @@ class Freeipmi < Formula
   url 'http://ftpmirror.gnu.org/freeipmi/freeipmi-1.4.1.tar.gz'
   sha1 'd59fd66e6fe9a58437d55a4bdd96ee431a8348e4'
 
+  bottle do
+    sha1 "60a47f55640500a8dd59b134e80c71121b5d7e93" => :mavericks
+    sha1 "c7a7f84c153c339822968f54e435ccf556899c61" => :mountain_lion
+    sha1 "148c4b0c38c5567950f00888182cabe792be0e07" => :lion
+  end
+
   depends_on 'argp-standalone'
   depends_on 'libgcrypt'
 
