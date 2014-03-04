5be8297a094bf0108ae3b688472c41b0ddd2e9f0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-04 07:57:37 +0000

    pure: add 0.59 bottle.

--- Library/Formula/pure.rb
+++ Library/Formula/pure.rb
@@ -5,6 +5,12 @@ class Pure < Formula
   url 'https://bitbucket.org/purelang/pure-lang/downloads/pure-0.59.tar.gz'
   sha1 '22614d77fff9937a53cf513767fdc5e8eeb4aae1'
 
+  bottle do
+    sha1 "1a84facf775c972077f6358b6248de79dbb8d6bd" => :mavericks
+    sha1 "eb6eaa93951ac7de8ed3318a763be17b5b117e82" => :mountain_lion
+    sha1 "9e26e112e0d57fea88c44e32307b5b1d14898c08" => :lion
+  end
+
   depends_on :automake
   depends_on :libtool
 
