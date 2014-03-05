5d1f32a9b4e67200d55afd411de16ae49094c8c4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-05 01:57:25 +0000

    griffon: add 1.5.0 bottle.

--- Library/Formula/griffon.rb
+++ Library/Formula/griffon.rb
@@ -5,6 +5,13 @@ class Griffon < Formula
   url 'http://dl.bintray.com/content/aalmiray/Griffon/griffon-1.5.0-bin.tgz'
   sha1 'de28b792c37cf103b5745f2f323403ec6990c58a'
 
+  bottle do
+    cellar :any
+    sha1 "1eb93e3f566a1c8efb41cdae3eaedff821773b7f" => :mavericks
+    sha1 "ea6f7f294f39f8b92d2e60eb82a3f32e14148851" => :mountain_lion
+    sha1 "f511e3e7057fa2c7983a599d0cad727a28d4b6f5" => :lion
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
 
