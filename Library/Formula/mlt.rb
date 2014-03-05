70ac83c51a13e5af548a59944459559892ecdf73
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-05 23:02:29 +0000

    mlt: add 0.9.0 bottle.

--- Library/Formula/mlt.rb
+++ Library/Formula/mlt.rb
@@ -5,6 +5,12 @@ class Mlt < Formula
   url 'https://downloads.sourceforge.net/mlt/mlt/mlt-0.9.0.tar.gz'
   sha1 '76bf18f4442801fae963c5a676d6626e383d1617'
 
+  bottle do
+    sha1 "22b1d5209ba4548c64d6ef2ad6f982f961a093d9" => :mavericks
+    sha1 "097854e7644e62c99487bc8c16b06b0a2d979855" => :mountain_lion
+    sha1 "569728be1bc542fba31db989cf2ac04bf619f8f1" => :lion
+  end
+
   depends_on "pkg-config" => :build
 
   depends_on "atk"
