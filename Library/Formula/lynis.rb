8c84449c7534f189777d98527707d5303679667d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-05 18:31:32 +0000

    lynis: add 1.4.4 bottle.

--- Library/Formula/lynis.rb
+++ Library/Formula/lynis.rb
@@ -5,6 +5,13 @@ class Lynis < Formula
   url 'http://cisofy.com/files/lynis-1.4.4.tar.gz'
   sha1 '54e68b60a305c13f5a6eda14626e6c80e1ea3b50'
 
+  bottle do
+    cellar :any
+    sha1 "16dbfb727719222d6696e6111ca065e3021384f5" => :mavericks
+    sha1 "f585d8c155ccb1265cc1acca3b14e6fb2abe5df9" => :mountain_lion
+    sha1 "1486a2b01224389dc8d889066b1e7a1d92b5029d" => :lion
+  end
+
   def install
     inreplace 'lynis' do |s|
       s.gsub! 'tINCLUDE_TARGETS="/usr/local/include/lynis /usr/local/lynis/include /usr/share/lynis/include ./include"', 'tINCLUDE_TARGETS="' + (prefix/'include') + '"'
