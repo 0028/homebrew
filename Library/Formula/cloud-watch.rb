668a61bdc495c17117cab89127d62bedfa39ad49
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-06 23:51:51 +0000

    cloud-watch: add 1.0.20.0 bottle.

--- Library/Formula/cloud-watch.rb
+++ Library/Formula/cloud-watch.rb
@@ -6,6 +6,13 @@ class CloudWatch < AmazonWebServicesFormula
   version '1.0.20.0'
   sha1 'c3f5b856b3ff8e1ae06d0ad5db5dd27c214c4881'
 
+  bottle do
+    cellar :any
+    sha1 "01516c1ad96f478d54124fc97c8e8e415780cdaa" => :mavericks
+    sha1 "3dcae59ddd421e972318dd63dbb8be91ec07c5a7" => :mountain_lion
+    sha1 "4731ecdb378410d9b91e616b6325830154b94896" => :lion
+  end
+
   def install
     standard_install
   end
