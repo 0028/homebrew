0644528feda8c282cbc33d866fdfd0fb43b037a9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-06 21:03:09 +0000

    grails: add 2.3.7 bottle.

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -5,6 +5,13 @@ class Grails < Formula
   url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.7.zip'
   sha1 '295f37fe989e8b3784258fcc14d2749737d8a2d0'
 
+  bottle do
+    cellar :any
+    sha1 "42e9f1a42e3ca3c3a4f0ba2df2a86d2cd8673c2d" => :mavericks
+    sha1 "f427ded4db11d2b45a13efca8b77d2d06738a5b6" => :mountain_lion
+    sha1 "2496689a5a7f82df2abe7181ce934400177f4261" => :lion
+  end
+
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
     prefix.install_metafiles
