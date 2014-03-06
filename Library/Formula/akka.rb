ef98377c190f434e1c44cc32be59d6e001f8fabf
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-06 10:56:28 +0000

    akka: add 2.3.0 bottle.

--- Library/Formula/akka.rb
+++ Library/Formula/akka.rb
@@ -5,6 +5,12 @@ class Akka < Formula
   url 'http://downloads.typesafe.com/akka/akka-2.3.0.tgz'
   sha1 'f2128ce170bb4f61b0e981f5611b85207938e460'
 
+  bottle do
+    sha1 "f3d27d475eb6d7c6a02eedf5eb171fadecd4e41e" => :mavericks
+    sha1 "ed8aa779c40d094bb87c7d3e7fe93583c281be82" => :mountain_lion
+    sha1 "ed936e095022456a4ff7bf95ab0e163164360495" => :lion
+  end
+
   def install
     # Remove Windows files
     rm "bin/akka.bat"
