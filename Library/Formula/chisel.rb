c019c4660522df04304dec7b15a780bbf51f8cbd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-07 04:36:39 +0000

    chisel: add 1.0.0 bottle.

--- Library/Formula/chisel.rb
+++ Library/Formula/chisel.rb
@@ -5,6 +5,13 @@ class Chisel < Formula
   url "https://github.com/facebook/chisel/archive/1.0.0.zip"
   sha1 "78e1c10d6e1e625291377aac1b27487d210bb04e"
 
+  bottle do
+    cellar :any
+    sha1 "38cde96827e2ec3ddd694fc9e98f0434d1c4449c" => :mavericks
+    sha1 "64633e5a9d9061d3e3896bb9d17f0812a07af39f" => :mountain_lion
+    sha1 "0854b2396b98e1259b0633884cce64427d9bee13" => :lion
+  end
+
   def install
     libexec.install Dir["*.py", "commands"]
     prefix.install "PATENTS"
