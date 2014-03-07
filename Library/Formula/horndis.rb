d8b937e1a140e74ad17ebb0382e46b02bb462199
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-07 11:16:28 +0000

    horndis: add 5 bottle.

--- Library/Formula/horndis.rb
+++ Library/Formula/horndis.rb
@@ -5,6 +5,13 @@ class Horndis < Formula
   url "https://github.com/jwise/HoRNDIS/archive/rel5.tar.gz"
   sha1 "5f01c62ae61554252c0fe727e414edcb8e060106"
 
+  bottle do
+    cellar :any
+    sha1 "b198a56898ec9bd603102a54aab34ecb4cc24b24" => :mavericks
+    sha1 "7926f4ef892082e4b2ad8c59421d6e04b96d35b6" => :mountain_lion
+    sha1 "f462cc30b522d3891b4ddc0af062026cf3325109" => :lion
+  end
+
   depends_on :xcode
 
   def install
