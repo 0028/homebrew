57fbf27873283487d68ced6b309eea9bf07baf9c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-07 20:36:29 +0000

    ghi: add 0.9.2 bottle.

--- Library/Formula/ghi.rb
+++ Library/Formula/ghi.rb
@@ -6,6 +6,13 @@ class Ghi < Formula
   head 'https://github.com/stephencelis/ghi.git'
   sha1 'c62c8d0e8e510db82ca3880a13c4f965c728cfb5'
 
+  bottle do
+    cellar :any
+    sha1 "89cb2b203ef734ef6848e01deb795e008c3196f3" => :mavericks
+    sha1 "5103d2d1e90f57c3d7059f52bc7ad06ea764f533" => :mountain_lion
+    sha1 "b76e74cfd2d44de53abe2e3664674a55adfecbce" => :lion
+  end
+
   def install
     bin.install "ghi"
     man1.install "man/ghi.1"
