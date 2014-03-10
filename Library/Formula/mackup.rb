cea31088cc545270e7bf1e4528c4cf29282a46f7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-10 04:56:29 +0000

    mackup: add 0.7.1 bottle.

--- Library/Formula/mackup.rb
+++ Library/Formula/mackup.rb
@@ -7,6 +7,12 @@ class Mackup < Formula
 
   head 'https://github.com/lra/mackup.git'
 
+  bottle do
+    sha1 "df8f1598ea5713a5c760e45ac152db79cc3dfe00" => :mavericks
+    sha1 "bfea937f346abe8a5b20c363f0fae9e6e2a455ce" => :mountain_lion
+    sha1 "0d35a1b8a6566c9fd7504fd927f2ce9325955e02" => :lion
+  end
+
   depends_on :python
 
   def install
