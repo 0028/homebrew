7ebe074e6033236f13fe9655476d8c473d027be6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-11 15:58:05 +0000

    reclass: add 1.3 bottle.

--- Library/Formula/reclass.rb
+++ Library/Formula/reclass.rb
@@ -7,6 +7,12 @@ class Reclass < Formula
 
   head 'https://github.com/madduck/reclass.git'
 
+  bottle do
+    sha1 "065e38b0c0dfc30dd370bab23d00a4e30b483f18" => :mavericks
+    sha1 "c8485906956f6be93ab5af183270d99755c42b0f" => :mountain_lion
+    sha1 "737119021847321c018152f63f3a5be8ea34af4c" => :lion
+  end
+
   depends_on :python
 
   def install
