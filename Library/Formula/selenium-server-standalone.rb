9bf3acacc7c715a1ceae400b8ca07c7ec2ee2fa3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-11 16:43:14 +0000

    selenium-server-standalone: add 2.40.0 bottle.

--- Library/Formula/selenium-server-standalone.rb
+++ Library/Formula/selenium-server-standalone.rb
@@ -5,6 +5,12 @@ class SeleniumServerStandalone < Formula
   url 'https://selenium-release.storage.googleapis.com/2.40/selenium-server-standalone-2.40.0.jar'
   sha1 '165f54ca53e5eefc6f27c26ac03d18bae99a544e'
 
+  bottle do
+    sha1 "dcbcdd0f706f62d6ba6760eb9b0ed8b667af041d" => :mavericks
+    sha1 "800ab2f4e36fa19107670dc582634600a7c2780c" => :mountain_lion
+    sha1 "b09446eafc55ff0cd52a464a8504e089b7e9a1b9" => :lion
+  end
+
   def install
     libexec.install "selenium-server-standalone-#{version}.jar"
     bin.write_jar_script libexec/"selenium-server-standalone-#{version}.jar", "selenium-server"
