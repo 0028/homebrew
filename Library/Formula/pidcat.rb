d749898a0125f831771bcd4eb83b24d61bf8f645
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-11 19:16:44 +0000

    pidcat: add 1.4.1 bottle.

--- Library/Formula/pidcat.rb
+++ Library/Formula/pidcat.rb
@@ -6,6 +6,13 @@ class Pidcat < Formula
   sha1 '89f806ae1fa3375ce188851c8c95fc1097467b82'
   head 'https://github.com/JakeWharton/pidcat.git'
 
+  bottle do
+    cellar :any
+    sha1 "c135280b01b1f77e1f498bf9ec04f32949a36094" => :mavericks
+    sha1 "737ed0639b907d221b9b7fcae2ea5fe94a005993" => :mountain_lion
+    sha1 "81da34be71cca2ff80168017cca19b1596321d3a" => :lion
+  end
+
   def install
     bin.install 'pidcat.py' => 'pidcat'
   end
