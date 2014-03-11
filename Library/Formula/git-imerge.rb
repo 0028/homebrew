9c6eccaf271169d0644a31ecd0fc68e63c5e4693
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-11 22:38:59 +0000

    git-imerge: add 0.7.0 bottle.

--- Library/Formula/git-imerge.rb
+++ Library/Formula/git-imerge.rb
@@ -5,6 +5,13 @@ class GitImerge < Formula
   url "https://github.com/mhagger/git-imerge/archive/0.7.0.tar.gz"
   sha1 "b3bab94743a79426ea79c1b5e503020ef7fbf2ec"
 
+  bottle do
+    cellar :any
+    sha1 "45a02912894c19eb87a3429c049fb0e0fc131cea" => :mavericks
+    sha1 "58f2fac073de49465860cf8d6920bdb1b0434af3" => :mountain_lion
+    sha1 "3c768831458407f7d9a14dcb8d87b97a080d378e" => :lion
+  end
+
   def install
     bin.install "git-imerge"
   end
