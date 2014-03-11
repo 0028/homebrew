d60218433f752dc658cb50940e045cb452f8e2cc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-11 16:05:44 +0000

    mysql-connector-c++: add 1.1.3 bottle.

--- Library/Formula/mysql-connector-c++.rb
+++ Library/Formula/mysql-connector-c++.rb
@@ -5,6 +5,13 @@ class MysqlConnectorCxx < Formula
   url 'http://mysql.he.net/Downloads/Connector-C++/mysql-connector-c++-1.1.3.tar.gz'
   sha1 'b817dccf3a4e340b6a972028ceb7eededaaebd6f'
 
+  bottle do
+    cellar :any
+    sha1 "7b0006f667454c533ca71ac7f31e328000fe1b1f" => :mavericks
+    sha1 "7787d0ad5c9d1a4df086ea0462a8e0d0648404ff" => :mountain_lion
+    sha1 "a1c5f6fe5bc18890e5ed8c0cb8bb57b174f86105" => :lion
+  end
+
   class MysqlClient < Requirement
     fatal true
     default_formula "mysql"
