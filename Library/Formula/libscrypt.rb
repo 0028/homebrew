0e8c812faaee677a1ec7f3675da68cffa7e88ddd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-11 16:46:58 +0000

    libscrypt: add 1.18 bottle.

--- Library/Formula/libscrypt.rb
+++ Library/Formula/libscrypt.rb
@@ -5,6 +5,13 @@ class Libscrypt < Formula
   url "https://github.com/technion/libscrypt/archive/v1.18.tar.gz"
   sha1 "ff6b0cda00afcb582ca57699322b94166e5f3ab6"
 
+  bottle do
+    cellar :any
+    sha1 "33a1952fb20cb58063c453b9c6b5079bb2250601" => :mavericks
+    sha1 "3c953409fab9f06b95d195ef41a24cf660091cfa" => :mountain_lion
+    sha1 "09f1b97211d6865a805790b7ea8fbda8590ae7db" => :lion
+  end
+
   def install
     system "make", "install-osx", "PREFIX=#{prefix}", "LDFLAGS=", "CFLAGS_EXTRA="
     system "make", "check", "LDFLAGS=", "CFLAGS_EXTRA="
