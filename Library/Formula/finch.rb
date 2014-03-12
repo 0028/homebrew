22f904d66ddcb185200ab6f0e67e482add9ed5d9
Homebrew <brew@robotunicorn.local>
2014-03-12 21:35:20 +0000

    finch: add 2.10.9 bottle.

--- Library/Formula/finch.rb
+++ Library/Formula/finch.rb
@@ -4,6 +4,12 @@ class Finch < Formula
   homepage 'http://developer.pidgin.im/wiki/Using%20Finch'
   url 'https://downloads.sourceforge.net/project/pidgin/Pidgin/2.10.9/pidgin-2.10.9.tar.bz2'
   sha1 'f3de8fd94dba1f4c98d5402a02430f9f323e665a'
+  bottle do
+    sha1 "efe612ec4cc0516934ab6d584cfcb93e1658c70e" => :mavericks
+    sha1 "964f644c6a1f9918de2d988978c397cb2582e3ec" => :mountain_lion
+    sha1 "b2338ab253d46054874d3b7e432114d988d41ef1" => :lion
+  end
+
   revision 1
 
   depends_on 'pkg-config' => :build
