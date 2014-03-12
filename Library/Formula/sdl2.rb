948ef66097db21b02c014088e6edf8213c7b5f41
Homebrew <brew@robotunicorn.local>
2014-03-12 16:31:01 +0000

    sdl2: add 2.0.2 bottle.

--- Library/Formula/sdl2.rb
+++ Library/Formula/sdl2.rb
@@ -5,6 +5,13 @@ class Sdl2 < Formula
   url 'http://libsdl.org/release/SDL2-2.0.2.tar.gz'
   sha1 '304c7cd3dddca98724a3e162f232a8a8f6e1ceb3'
 
+  bottle do
+    cellar :any
+    sha1 "fa675277587f0772e559872d1d222857ae3e7052" => :mavericks
+    sha1 "7446d8e0a633fa399f3e2b37cb7a76ed9967cd15" => :mountain_lion
+    sha1 "4d845eddccd3f6c323967b5f9a124905254bd5fa" => :lion
+  end
+
   head do
     url 'http://hg.libsdl.org/SDL', :using => :hg
 
