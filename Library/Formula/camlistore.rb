3b6013f9ffb9a99b4da3486f4a20e6dc0632a286
Homebrew <brew@robotunicorn.local>
2014-03-12 19:35:32 +0000

    camlistore: add 0.7 bottle.

--- Library/Formula/camlistore.rb
+++ Library/Formula/camlistore.rb
@@ -6,6 +6,12 @@ class Camlistore < Formula
   url 'https://github.com/bradfitz/camlistore/archive/0.7.tar.gz'
   sha1 'fae4e18f03f545f6232c24861e1f13c4bb653b34'
 
+  bottle do
+    sha1 "671a4994b3763f52038355341ea174c6bc3711a6" => :mavericks
+    sha1 "3407b29787dfcc67780e6c09f15d7ed38869a910" => :mountain_lion
+    sha1 "782b43d7e6316be64764038738630be3d61aa570" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'go' => :build
   depends_on 'sqlite'
