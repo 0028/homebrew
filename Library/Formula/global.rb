ddae769c88359d9a14aa51952f4fb6020d53d001
Homebrew <brew@robotunicorn.local>
2014-03-12 22:28:35 +0000

    global: add 6.2.11 bottle.

--- Library/Formula/global.rb
+++ Library/Formula/global.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Global < Formula
   homepage 'http://www.gnu.org/software/global/'
   url 'http://ftpmirror.gnu.org/global/global-6.2.11.tar.gz'
+  bottle do
+    sha1 "600c47170d9d5f262a1cb3d65fe494606e4ff3e1" => :mavericks
+    sha1 "44eda682ece49783fb583d5056688873b0effdb6" => :mountain_lion
+    sha1 "203319d0548c3ab26839f12c2c13ab49fb939f5e" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/global/global-6.2.11.tar.gz'
   sha1 'b0f50213680ec3288988354c34e3b3ae1a42719e'
 
