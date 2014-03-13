4d6e721399218ad00c7d5c40922e7bd8cb203802
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-13 19:49:12 +0000

    ec2-ami-tools: add 1.5.1 bottle.

--- Library/Formula/ec2-ami-tools.rb
+++ Library/Formula/ec2-ami-tools.rb
@@ -5,6 +5,13 @@ class Ec2AmiTools < AmazonWebServicesFormula
   url 'http://ec2-downloads.s3.amazonaws.com/ec2-ami-tools-1.5.1.zip'
   sha1 '65deb85e452a3341ff12204db8eba0dcfd9db6a3'
 
+  bottle do
+    cellar :any
+    sha1 "cee1347f7e76530f8d6f91fa18af668e2ae930b0" => :mavericks
+    sha1 "8950a0d4b051717ccbc6c6ae265f13e29e1a3a5f" => :mountain_lion
+    sha1 "3eef20225a85f30ad2eff8bfcc760110c41f74d0" => :lion
+  end
+
   def install
     standard_install
   end
