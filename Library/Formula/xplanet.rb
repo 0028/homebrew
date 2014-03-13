6185e32bc8215e63ca06e63ab60c30f7e4a6d0aa
Homebrew <brew@robotunicorn.local>
2014-03-13 02:54:34 +0000

    xplanet: add 1.3.0 bottle.

--- Library/Formula/xplanet.rb
+++ Library/Formula/xplanet.rb
@@ -5,6 +5,12 @@ class Xplanet < Formula
   url 'https://downloads.sourceforge.net/project/xplanet/xplanet/1.3.0/xplanet-1.3.0.tar.gz'
   sha1 '7c5208b501b441a0184cbb334a5658d0309d7dac'
 
+  bottle do
+    sha1 "662807113af4148a7f605dc8350de7bcc39e61c8" => :mavericks
+    sha1 "07009a2e03b1bc1cde35383ab31b733f5808aa9b" => :mountain_lion
+    sha1 "485e0afb43d6edf8e277c7e8d4a0e1ae94be7d66" => :lion
+  end
+
   option "with-x", "Build for X11 instead of Aqua"
   option "with-all", "Build with default Xplanet configuration dependencies except JPL CSPICE (manual install)"
 
