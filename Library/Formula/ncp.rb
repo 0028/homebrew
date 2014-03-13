a2ce7657630ad0069ba9ceca8692c4d893a5e428
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-13 15:18:23 +0000

    ncp: add 1.2.4 bottle.

--- Library/Formula/ncp.rb
+++ Library/Formula/ncp.rb
@@ -6,6 +6,13 @@ class Ncp < Formula
   sha1 "bd33e5311c249948559d17bfd59df93ae72e1f17"
   head "cvs://:pserver:cvs:@cvs.fefe.de:/cvs:ncp"
 
+  bottle do
+    cellar :any
+    sha1 "6658494b65d93e43a6b8e93455121fd4efda1d42" => :mavericks
+    sha1 "2975fb41a28b95caab03ade23fcaf2d4038029e5" => :mountain_lion
+    sha1 "b73257e35a758e6b6a1615536bfea9f2ad424ebc" => :lion
+  end
+
   depends_on "libowfat"
 
   def patches
