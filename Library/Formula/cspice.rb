27ee5112e1ff0a88b99f19472df9a3eea6b906b7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-14 01:00:27 +0000

    cspice: add 64 bottle.

--- Library/Formula/cspice.rb
+++ Library/Formula/cspice.rb
@@ -6,6 +6,13 @@ class Cspice < Formula
   sha1 "e5546a72a2d0c7e337850a10d208014efb57d78d"
   version "64"
 
+  bottle do
+    cellar :any
+    sha1 "db2673dd52a49c1bdf5a0b47b235556f684698a4" => :mavericks
+    sha1 "d7beca492dea34ab3866d9ff8b1cb767a676b039" => :mountain_lion
+    sha1 "547cb69a69917f48b4e8bbed0748468b36d90ca5" => :lion
+  end
+
   def install
     rm_f Dir["lib/*"]
     rm_f Dir["exe/*"]
