27efe4e639b795d14a65d6c5de9fa462dce00cff
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-14 18:06:42 +0000

    portaudio: add 19.20111121 bottle.

--- Library/Formula/portaudio.rb
+++ Library/Formula/portaudio.rb
@@ -5,6 +5,13 @@ class Portaudio < Formula
   url 'http://www.portaudio.com/archives/pa_stable_v19_20111121.tgz'
   sha1 'f07716c470603729a55b70f5af68f4a6807097eb'
 
+  bottle do
+    cellar :any
+    sha1 "ac496b4a9900382d74603a7467c6cb1b2dd51149" => :mavericks
+    sha1 "c156002f0326a6d88ed0d8d2a5ac2fcf3e9fb71d" => :mountain_lion
+    sha1 "58a2327d1bfafaced97883ed35c2ebb65a3deea3" => :lion
+  end
+
   head 'https://subversion.assembla.com/svn/portaudio/portaudio/trunk/', :using => :svn
 
   depends_on 'pkg-config' => :build
