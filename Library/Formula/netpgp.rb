26bcb4828f4c646aa0c4872006283159d311504a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-15 13:53:19 +0000

    netpgp: add 3.99.14 bottle.

--- Library/Formula/netpgp.rb
+++ Library/Formula/netpgp.rb
@@ -6,6 +6,13 @@ class Netpgp < Formula
   sha1 "de61bdaaace4778608ab89be1ef6da9bbf5e18ee"
   version "3.99.14"
 
+  bottle do
+    cellar :any
+    sha1 "f260b7b15271e4eb0271ae46ec3a2984ce235d5e" => :mavericks
+    sha1 "3aded2f9c9fd0e775840fad8f63748ce37c6864e" => :mountain_lion
+    sha1 "efbd085000c402afe1f652e6a7129a35f47552b5" => :lion
+  end
+
   def install
     ENV["CFLAGS"] = "-Wno-deprecated-declarations"
     system "./configure", "--prefix=#{prefix}",
