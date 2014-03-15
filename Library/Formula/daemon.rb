4f1a9a0077ba06690b2b6aa66c1a421b35537ab3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-15 20:13:01 +0000

    daemon: add 0.6.4 bottle.

--- Library/Formula/daemon.rb
+++ Library/Formula/daemon.rb
@@ -5,6 +5,13 @@ class Daemon < Formula
   url 'http://libslack.org/daemon/download/daemon-0.6.4.tar.gz'
   sha1 'fa6298f05f868d54660a7ed70c05fb7a0963a24b'
 
+  bottle do
+    cellar :any
+    sha1 "daba24f1ac885ceaf3e491ff90f9959887d4528d" => :mavericks
+    sha1 "308c6df7776b2d1d99a0eca4869b983575f18b37" => :mountain_lion
+    sha1 "65073e4711f6b18d6b0790c070555a52ef19e0e6" => :lion
+  end
+
   def patches
     # fixes for mavericks strlcpy/strlcat: https://trac.macports.org/ticket/42845
     {:p1 => "https://trac.macports.org/raw-attachment/ticket/42845/daemon-0.6.4-ignore-strlcpy-strlcat.patch"} if MacOS.version == :mavericks
