ef6eaac63306dcf80856784b2d2ac2e85e0c9db2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-15 02:41:55 +0000

    plan9port: add 20140306 bottle.

--- Library/Formula/plan9port.rb
+++ Library/Formula/plan9port.rb
@@ -5,6 +5,12 @@ class Plan9port < Formula
   url "https://plan9port.googlecode.com/files/plan9port-20140306.tgz"
   sha1 "b778a96558e2b67be28ae80cab4482ef3082ec0e"
 
+  bottle do
+    sha1 "211dffa87ebbdeed81ef75715a10ecb8cfe96118" => :mavericks
+    sha1 "c4bc003967b9ee70e29dcbced3cfc165cceafd52" => :mountain_lion
+    sha1 "37af63c0b7f544677779e73ec984382b725458f2" => :lion
+  end
+
   def install
     ENV["PLAN9_TARGET"] = libexec
     system "./INSTALL"
