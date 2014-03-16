d678dd16243e23766c722d4e866a2b9c5fb10722
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-16 03:37:56 +0000

    latex2rtf: add 2.3.6 bottle.

--- Library/Formula/latex2rtf.rb
+++ Library/Formula/latex2rtf.rb
@@ -5,6 +5,12 @@ class Latex2rtf < Formula
   url 'https://downloads.sourceforge.net/project/latex2rtf/latex2rtf-unix/2.3.6/latex2rtf-2.3.6.tar.gz'
   sha1 'd0436c52ec4d82f379cdbd7c8b00ddac4f015da5'
 
+  bottle do
+    sha1 "75fe10a28189f272408f7c7f896c6d0a98b5bd2d" => :mavericks
+    sha1 "023c1b84690607a4c83e43764307abd2ba6b5d8d" => :mountain_lion
+    sha1 "e214fabac0431fb75cef5c3d0ed60276802299a1" => :lion
+  end
+
   def install
     inreplace 'Makefile', "cp -p doc/latex2rtf.html $(DESTDIR)$(SUPPORTDIR)", "cp -p doc/web/* $(DESTDIR)$(SUPPORTDIR)"
     system "make", "DESTDIR=",
