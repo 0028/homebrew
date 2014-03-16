691176c31353c3e98e9b091c3b28f4827565b31f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-16 03:37:56 +0000

    rkhunter: add 1.4.2 bottle.

--- Library/Formula/rkhunter.rb
+++ Library/Formula/rkhunter.rb
@@ -5,6 +5,12 @@ class Rkhunter < Formula
   url 'https://downloads.sourceforge.net/project/rkhunter/rkhunter/1.4.2/rkhunter-1.4.2.tar.gz'
   sha1 'da01bc6757e14549560ad6ea46d1e93dbf5ac90f'
 
+  bottle do
+    sha1 "710ec1a4ae4abe0c6c65612eeb305c0ce07a40a1" => :mavericks
+    sha1 "6466aad79fcd9be7a64a750c5aeadc0f95191476" => :mountain_lion
+    sha1 "699537efe059bb8878bb95fe75f62798479129ff" => :lion
+  end
+
   def install
     system "./installer.sh", "--layout", "custom", prefix, "--install"
   end
