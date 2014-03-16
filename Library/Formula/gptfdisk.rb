22226312469f99cb33cc07c6c6afe140d9e73139
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-16 03:37:55 +0000

    gptfdisk: add 0.8.10 bottle.

--- Library/Formula/gptfdisk.rb
+++ Library/Formula/gptfdisk.rb
@@ -5,6 +5,13 @@ class Gptfdisk < Formula
   url 'https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/0.8.10/gptfdisk-0.8.10.tar.gz'
   sha1 '1708e232220236b6bdf299b315e9bc2205c01ba5'
 
+  bottle do
+    cellar :any
+    sha1 "78b2b0bf4c0f1e80e9ad89b13a6dfbbdc263b458" => :mavericks
+    sha1 "01943676ce7a3f47e8904de6cea286d219d65281" => :mountain_lion
+    sha1 "2636882005224db7aadfd58b564bbaaab990c0ad" => :lion
+  end
+
   depends_on 'popt'
   depends_on 'icu4c'
 
