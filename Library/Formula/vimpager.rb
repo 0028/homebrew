bff32f12bf79c86df5d5d1fd44ae7d382524b55d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-16 15:59:17 +0000

    vimpager: add 1.8.7 bottle.

--- Library/Formula/vimpager.rb
+++ Library/Formula/vimpager.rb
@@ -6,6 +6,13 @@ class Vimpager < Formula
   sha1 '796bb69e30a0b87ee65bf29c5ae800a16033f85f'
   head 'https://github.com/rkitover/vimpager.git'
 
+  bottle do
+    cellar :any
+    sha1 "75dcd00e5a872314fcd95d559b293f7f15daf4db" => :mavericks
+    sha1 "edc3077e89b737167ba42f695307b9a3a2984bf6" => :mountain_lion
+    sha1 "14c21df4485f454166bc651ada4f00d728fc770b" => :lion
+  end
+
   def install
     inreplace 'vimpager.1', '~/bin/', ''
 
