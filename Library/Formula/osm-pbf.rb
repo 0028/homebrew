cabc940d6fe7711f8b4681b839e849bbd67abd45
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-16 15:59:17 +0000

    osm-pbf: add 1.3.3 bottle.

--- Library/Formula/osm-pbf.rb
+++ Library/Formula/osm-pbf.rb
@@ -5,6 +5,13 @@ class OsmPbf < Formula
   url 'https://github.com/scrosby/OSM-binary/archive/v1.3.3.tar.gz'
   sha1 '639e3eecc00041e6326b69da6c200fabe7ad2895'
 
+  bottle do
+    cellar :any
+    sha1 "38c4ce5a81b79779b1d03bb8ecde1d3944299bb2" => :mavericks
+    sha1 "0a86a3694de2e3ec46c82b5ceabb29143db84c05" => :mountain_lion
+    sha1 "181c21d60ce34fead9e52b2e8bc27ebdff6fd9bb" => :lion
+  end
+
   depends_on 'protobuf'
 
   def install
