75fe53f3c4fdd981e5353598603abba1f375c3e9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-16 03:37:56 +0000

    mpop: add 1.0.29 bottle.

--- Library/Formula/mpop.rb
+++ Library/Formula/mpop.rb
@@ -5,6 +5,13 @@ class Mpop < Formula
   url 'https://downloads.sourceforge.net/project/mpop/mpop/1.0.29/mpop-1.0.29.tar.bz2'
   sha1 '489ff6ae4d662af4179d9affc86e79db94ed95b5'
 
+  bottle do
+    cellar :any
+    sha1 "3f219c35676c82ca5c410379fab835083d63aa1b" => :mavericks
+    sha1 "ea23c59c844cdf0c55e1bd834f660456c1f83329" => :mountain_lion
+    sha1 "bbf6f9eb47b44888835c22706df2a621b4ea640d" => :lion
+  end
+
   depends_on 'pkg-config' => :build
 
   def install
