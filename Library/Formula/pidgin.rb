3cd90585078ea23b6b523b0050cbd8a0327f3626
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-16 03:37:55 +0000

    pidgin: add 2.10.9 bottle.

--- Library/Formula/pidgin.rb
+++ Library/Formula/pidgin.rb
@@ -4,6 +4,12 @@ class Pidgin < Formula
   homepage 'http://pidgin.im/'
   url 'https://downloads.sourceforge.net/project/pidgin/Pidgin/2.10.9/pidgin-2.10.9.tar.bz2'
   sha1 'f3de8fd94dba1f4c98d5402a02430f9f323e665a'
+  bottle do
+    sha1 "e8cb985e74dd8e596518906b0930c7358297faec" => :mavericks
+    sha1 "c1532e9ca452c54c07e9e3893019488f38839f65" => :mountain_lion
+    sha1 "a6f818534c770c8bb8e77382bda49f6d2949d70d" => :lion
+  end
+
   revision 1
 
   depends_on :x11
