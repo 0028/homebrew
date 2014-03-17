d5f64a4779f22650972b476bd552881ac8777f01
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-17 00:01:01 +0000

    recoverjpeg: add 2.3 bottle.

--- Library/Formula/recoverjpeg.rb
+++ Library/Formula/recoverjpeg.rb
@@ -5,6 +5,13 @@ class Recoverjpeg < Formula
   url 'http://www.rfc1149.net/download/recoverjpeg/recoverjpeg-2.3.tar.gz'
   sha1 'ed16b9852d85d7a07710d1ca41a03427642f3f38'
 
+  bottle do
+    cellar :any
+    sha1 "400447d911b1dc743c4824ef7a1644634fd164f1" => :mavericks
+    sha1 "a3e406823dca2001f5a47c2389b5bfc33a2bcd99" => :mountain_lion
+    sha1 "01e1f45f64dd1442a08aee017a2147a4e1e15d3c" => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
