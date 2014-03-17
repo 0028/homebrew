58074b55c3dc8d264458225616b35f837e104f8f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-17 00:01:01 +0000

    languagetool: add 2.4.1 bottle.

--- Library/Formula/languagetool.rb
+++ Library/Formula/languagetool.rb
@@ -5,6 +5,13 @@ class Languagetool < Formula
   url 'https://www.languagetool.org/download/LanguageTool-2.4.1.zip'
   sha1 '47ca4ee2da377450056eb386a2b88e4a077a94a5'
 
+  bottle do
+    cellar :any
+    sha1 "7d5f8e328ec0d4c4e52da7a5f83f715129bd0635" => :mavericks
+    sha1 "c032e727821af1656a327c0165411ef5d1a24a32" => :mountain_lion
+    sha1 "92621b63b16f5540a63d20ab441d12ef35b2fa1b" => :lion
+  end
+
   def server_script server_jar; <<-EOS.undent
     #!/bin/bash
     exec java -cp #{server_jar} org.languagetool.server.HTTPServer "$@"
