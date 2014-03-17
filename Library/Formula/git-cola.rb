2387d14316d089aaee01bb71dc31fe334293b5f4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-17 00:26:12 +0000

    git-cola: add 2.0.1 bottle.

--- Library/Formula/git-cola.rb
+++ Library/Formula/git-cola.rb
@@ -7,6 +7,13 @@ class GitCola < Formula
 
   head 'https://github.com/git-cola/git-cola.git'
 
+  bottle do
+    cellar :any
+    sha1 "1f7d2ffeedc2c3f8038a06ecd371381b216c828b" => :mavericks
+    sha1 "f013804d0a12ea985a02cee3189cfcb60c9f8bba" => :mountain_lion
+    sha1 "203f08460576b6a68f339f8c596fd70775e861be" => :lion
+  end
+
   option 'with-docs', "Build man pages using asciidoc and xmlto"
 
   depends_on 'pyqt'
