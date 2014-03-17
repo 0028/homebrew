b8cc839a4719b3da873160c14559a0146a7346ee
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-17 00:01:01 +0000

    dvtm: add 0.11 bottle.

--- Library/Formula/dvtm.rb
+++ Library/Formula/dvtm.rb
@@ -6,6 +6,13 @@ class Dvtm < Formula
   sha1 'f82927afb88e3745dfed11fd39e5f6f9e956099e'
   head 'git://repo.or.cz/dvtm.git'
 
+  bottle do
+    cellar :any
+    sha1 "a4f19c41d8fff2b5bf83df29b68d6c4beb2a810a" => :mavericks
+    sha1 "edff283f057b2e775f7ed7370f44d3349dd56657" => :mountain_lion
+    sha1 "59b71d27ae4c0e9a40bdbcc707a695cd2fe8a26d" => :lion
+  end
+
   def install
     inreplace 'config.mk', 'LIBS = -lc -lutil -lncursesw', 'LIBS = -lc -lutil -lncurses'
     inreplace 'Makefile', 'strip -s', 'strip'
