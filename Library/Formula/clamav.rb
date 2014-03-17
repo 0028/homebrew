426a119dd80f4af24a9aa2fc446bde50d95c6316
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-17 01:07:26 +0000

    clamav: add 0.98.1 bottle.

--- Library/Formula/clamav.rb
+++ Library/Formula/clamav.rb
@@ -5,6 +5,12 @@ class Clamav < Formula
   url 'https://downloads.sourceforge.net/clamav/0.98/clamav-0.98.1.tar.gz'
   sha1 '9f04c0e81463c36f7e58d18f16d1b88f3332dcb8'
 
+  bottle do
+    sha1 "2e57397ad5ab1229fada5e4ee488d82d95bb8ada" => :mavericks
+    sha1 "78aaaca4e1892fc5a8da68bdbaaa4635082a2cb4" => :mountain_lion
+    sha1 "54ea8b999ef0e9e0801beb1ce18bbfe0c6785cf7" => :lion
+  end
+
   skip_clean 'share'
 
   def install
