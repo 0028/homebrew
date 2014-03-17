2d7a10cbb8fbdcbdc5cf92a9a901795620ef1653
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-17 00:01:01 +0000

    nasm: add 2.11.02 bottle.

--- Library/Formula/nasm.rb
+++ Library/Formula/nasm.rb
@@ -5,6 +5,13 @@ class Nasm < Formula
   url 'http://www.nasm.us/pub/nasm/releasebuilds/2.11.02/nasm-2.11.02.tar.bz2'
   sha256 'ece26b5ef565f94d19a72756d05965e424d2e5ca55f88b949852da70dd62f0e0'
 
+  bottle do
+    cellar :any
+    sha1 "e132eecebd975b058136e8e56b1c9a3751653368" => :mavericks
+    sha1 "8eadf122b0aa6d88dab25e18ad2f2e0645ded3bd" => :mountain_lion
+    sha1 "96d5cc39bcd7dfbd42537486b0992c7cecc6e184" => :lion
+  end
+
   option :universal
 
   def install
