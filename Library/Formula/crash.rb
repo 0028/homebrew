5930e6003c81c85fa0300c08a29f787ef5c7811f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-18 20:48:32 +0000

    crash: add 1.2.8 bottle.

--- Library/Formula/crash.rb
+++ Library/Formula/crash.rb
@@ -5,6 +5,13 @@ class Crash < Formula
   url 'https://crsh.googlecode.com/files/crash-1.2.8.tar.gz'
   sha1 '12290cd227b20eae07674f8cf0d43ee58732e34b'
 
+  bottle do
+    cellar :any
+    sha1 "68dd77c79596389ada85f79a653f4b9a92b08faf" => :mavericks
+    sha1 "2cd7a82301dbdff68ef94cc5961ec62a2eebff3c" => :mountain_lion
+    sha1 "a2a8acee2983a8cb52396ff29ca4b5797f8de587" => :lion
+  end
+
   devel do
     url 'http://search.maven.org/remotecontent?filepath=org/crashub/crash.distrib/1.3.0-beta15/crash.distrib-1.3.0-beta15.tar.gz'
     sha1 '1622804e40debec430a06e6354dcbff7b455741b'
