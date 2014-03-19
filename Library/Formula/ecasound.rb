8f90f61c01d8acfdf9f99ae783be55a04653d2b7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 15:58:48 +0000

    ecasound: add 2.9.1 bottle.

--- Library/Formula/ecasound.rb
+++ Library/Formula/ecasound.rb
@@ -5,6 +5,12 @@ class Ecasound < Formula
   url 'http://ecasound.seul.org/download/ecasound-2.9.1.tar.gz'
   sha1 '048fc2487deb3c94d92814b54255435b2acee1d8'
 
+  bottle do
+    sha1 "c4884937a2e83b06d70f25b83bd01758f1b808de" => :mavericks
+    sha1 "dbc5b390d252c6fe91c695f9d8a7be1f0e1e2521" => :mountain_lion
+    sha1 "951701e3de68854a6413bfc3cd5b9d934418bca6" => :lion
+  end
+
   option "with-ruby", "Compile with ruby support"
 
   def install
