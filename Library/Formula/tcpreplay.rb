af052149eb5c5c27fe035e7ce670ce4db088e7b5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 18:05:18 +0000

    tcpreplay: add 4.0.3 bottle.

--- Library/Formula/tcpreplay.rb
+++ Library/Formula/tcpreplay.rb
@@ -5,6 +5,13 @@ class Tcpreplay < Formula
   url 'https://downloads.sourceforge.net/project/tcpreplay/tcpreplay/4.0.3/tcpreplay-4.0.3.tar.gz'
   sha1 '1410ddf0cd239ef825380cc8d1f495df39106404'
 
+  bottle do
+    cellar :any
+    sha1 "10207d1399fa246ecaa8b6d03c8e2276c1b61cad" => :mavericks
+    sha1 "d34f3f45041579b0eb30d04b5ff690ab1c0dabf7" => :mountain_lion
+    sha1 "debcb7552b242ab3c7fc498c537d77f31972640d" => :lion
+  end
+
   # Hard-code use of dylib instead of so
   patch :DATA
 
