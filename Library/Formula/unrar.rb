5fabb7f27a83e2fc886b931de3280273f579a37e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 15:58:48 +0000

    unrar: add 5.1.1 bottle.

--- Library/Formula/unrar.rb
+++ Library/Formula/unrar.rb
@@ -5,6 +5,13 @@ class Unrar < Formula
   url 'http://www.rarlab.com/rar/unrarsrc-5.1.1.tar.gz'
   sha1 '7d1a8691336208d1d9b85812ba7d5336d2986376'
 
+  bottle do
+    cellar :any
+    sha1 "4927cfc26f7c32f0065770c6022609e1557bb759" => :mavericks
+    sha1 "7e7a46c43086ad5b38e23f631e8b1395e7d191e2" => :mountain_lion
+    sha1 "ac64b20e1651559605403de406ebe0cc030c8424" => :lion
+  end
+
   def install
     system "make"
     bin.install 'unrar'
