8353b1f9301ecbc2577f54e9e6ee44457206b702
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 16:37:59 +0000

    screenfetch: update 3.2.2 bottle.

--- Library/Formula/screenfetch.rb
+++ Library/Formula/screenfetch.rb
@@ -8,9 +8,10 @@ class Screenfetch < Formula
 
   bottle do
     cellar :any
-    sha1 "a0af0c7172066ca31cf6eb542a4e7b642dd79dc7" => :mavericks
-    sha1 "5ee15d688ee29db874a8171869bf0d8c970d8458" => :mountain_lion
-    sha1 "ae7d892faa753aca39548ad14a5f949216c76051" => :lion
+    revision 1
+    sha1 "ee7f7c49752cc2f6a7591285ed12c5674a5d7e19" => :mavericks
+    sha1 "ffdaa973304f31d0bcc9776ffe617eb64688dfb8" => :mountain_lion
+    sha1 "0467fc21111dae838a016293b9e3564b42e58568" => :lion
   end
 
   def install
