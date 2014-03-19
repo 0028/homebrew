62d1330a6ca205b94ff5015e6df4399e89c82d66
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 06:39:23 +0000

    fping: add 3.9 bottle.

--- Library/Formula/fping.rb
+++ Library/Formula/fping.rb
@@ -7,6 +7,13 @@ class Fping < Formula
 
   head 'https://github.com/schweikert/fping.git'
 
+  bottle do
+    cellar :any
+    sha1 "c9f11c991743cdf9d90926d06d119acfdf0f8805" => :mavericks
+    sha1 "9399e57d606a1bf69ef9a9bc32d980c3c1bb5e63" => :mountain_lion
+    sha1 "74dc26bd04293cf8bcf22ba4a3e823f0a084876c" => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
