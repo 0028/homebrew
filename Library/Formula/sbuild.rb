1c4b9fbdb13aac94cb9e13f7b8988a43f6932681
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 14:04:14 +0000

    sbuild: add 0.7.3 bottle.

--- Library/Formula/sbuild.rb
+++ Library/Formula/sbuild.rb
@@ -5,6 +5,13 @@ class Sbuild < Formula
   url 'http://sbuild.org/uploads/sbuild/0.7.3/sbuild-0.7.3-dist.zip'
   sha1 'd41d88ddfccbe6745d7520af40cdf44257ad710c'
 
+  bottle do
+    cellar :any
+    sha1 "c7970b4c69927dce99b8a511fde902a33cd02025" => :mavericks
+    sha1 "e83f9cc0212e8b9f72c3474ede490fdc841563e4" => :mountain_lion
+    sha1 "bf5e2251288287e8f2e60faa98a01ceffa2ae3ba" => :lion
+  end
+
   def install
     libexec.install Dir['*']
     system "chmod +x #{libexec}/bin/sbuild"
