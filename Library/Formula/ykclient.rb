525cdccfbb7f9cfd73fd107f0f2b90b21f8dfb78
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-21 19:09:56 +0000

    ykclient: add 2.12 bottle.

--- Library/Formula/ykclient.rb
+++ Library/Formula/ykclient.rb
@@ -5,6 +5,13 @@ class Ykclient < Formula
   url 'http://yubico.github.io/yubico-c-client/releases/ykclient-2.12.tar.gz'
   sha1 '518ce53ba9ef61a619f9150778f19fad23014a9c'
 
+  bottle do
+    cellar :any
+    sha1 "164c03e105ba955ce29dae3403f4318e10567ecd" => :mavericks
+    sha1 "060f75b95761d4640645c18df6c189ca291ecd80" => :mountain_lion
+    sha1 "b6a9d202a783cf884a5068b28a1e9cdb4bce5093" => :lion
+  end
+
   option :universal
 
   depends_on 'pkg-config' => :build
