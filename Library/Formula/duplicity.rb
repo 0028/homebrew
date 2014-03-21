e6eae4f6b11ac7f1ec82e8557ab2e7c8d05da81f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-21 16:23:21 +0000

    duplicity: add 0.6.23 bottle.

--- Library/Formula/duplicity.rb
+++ Library/Formula/duplicity.rb
@@ -5,6 +5,13 @@ class Duplicity < Formula
   url 'http://code.launchpad.net/duplicity/0.6-series/0.6.23/+download/duplicity-0.6.23.tar.gz'
   sha1 '0b8664d55c24957b3b19e903c0d69673f2bfa166'
 
+  bottle do
+    cellar :any
+    sha1 "1753ac0a9f3e148416ba95cbea62bd3bf1bf30cb" => :mavericks
+    sha1 "c18facae71e2dcfe9aeb5b1afde2db5936c3e5f9" => :mountain_lion
+    sha1 "1d839511700f987c9ece3ed14110be35ffcfc859" => :lion
+  end
+
   depends_on :python
   depends_on 'librsync'
   depends_on 'gnupg'
