585defa452d46be8a8c51e71f014733a5b0b7698
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-21 17:28:58 +0000

    vowpal-wabbit: add 7.5 bottle.

--- Library/Formula/vowpal-wabbit.rb
+++ Library/Formula/vowpal-wabbit.rb
@@ -5,6 +5,13 @@ class VowpalWabbit < Formula
   url 'https://github.com/JohnLangford/vowpal_wabbit/archive/7.5.tar.gz'
   sha1 '2f7602a54df2e6aa2d8a54baaaceb47098a74dce'
 
+  bottle do
+    cellar :any
+    sha1 "0c7c647bc7897d04ed86e466054bfef118260ea5" => :mavericks
+    sha1 "e6d39082b170109767879459dca6c3125a8fd5bd" => :mountain_lion
+    sha1 "92e4b0362a5304c961fe4b9c380e133c17e6864f" => :lion
+  end
+
   head do
     url 'https://github.com/JohnLangford/vowpal_wabbit.git'
 
