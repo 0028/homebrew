43ac5925090c46927fb09c1e2e50d39867324ddc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-22 20:48:03 +0000

    dotwrp: add 1.2 bottle.

--- Library/Formula/dotwrp.rb
+++ Library/Formula/dotwrp.rb
@@ -5,6 +5,13 @@ class Dotwrp < Formula
   url 'https://github.com/mcg1969/dotwrp/archive/v1.2.tar.gz'
   sha1 'ec832b1ba160b6c5028af92bc77e725a6588b8bc'
   head 'https://github.com/mcg1969/dotwrp.git'
+  bottle do
+    cellar :any
+    sha1 "15e1740a4bf03c8b6946c0814ec0f64a486967ea" => :mavericks
+    sha1 "b0f6630b9175420a79427deb3e6b435d10868ea9" => :mountain_lion
+    sha1 "5b64047f89baf4cd65795a59a3d101ef4dd2051b" => :lion
+  end
+
   def install
     system "#{ENV.cc} #{ENV.cflags} -c dotwrp.c"
     system "ar -cru libdotwrp.a dotwrp.o"
