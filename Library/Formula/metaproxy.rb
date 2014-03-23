f33124efff910d55a3aad803423c98d8ac980b3d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-23 14:03:56 +0000

    metaproxy: add 1.4.6 bottle.

--- Library/Formula/metaproxy.rb
+++ Library/Formula/metaproxy.rb
@@ -5,6 +5,13 @@ class Metaproxy < Formula
   url "http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.4.6.tar.gz"
   sha1 "bb36efb7d2f2d16aec1ee9c62dda10e6e3c4b995"
 
+  bottle do
+    cellar :any
+    sha1 "b8868673569456d50b9aac9cc8613332621a7776" => :mavericks
+    sha1 "00bdc31823daad6e1c570da1b2896fe84e7017b7" => :mountain_lion
+    sha1 "5e1f7485bea02ddeedc6e9345b4dcef120e47853" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "yazpp"
   depends_on "boost"
