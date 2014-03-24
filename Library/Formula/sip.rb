d1631ce3ddf2d11ab6be03cdc981e36f8cb38a58
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-24 12:59:33 +0000

    sip: add 4.15.5 bottle.

--- Library/Formula/sip.rb
+++ Library/Formula/sip.rb
@@ -5,6 +5,12 @@ class Sip < Formula
   url 'http://download.sf.net/project/pyqt/sip/sip-4.15.5/sip-4.15.5.tar.gz'
   sha1 '1e1c912981930754885ba47d708e2664d1c6ba9e'
 
+  bottle do
+    sha1 "3ea8b83c9fa5e2c189d4ecbc257b391cd8f64de6" => :mavericks
+    sha1 "143581d05e469fc508b8b7c9e4c94cedf6fdb70f" => :mountain_lion
+    sha1 "a14ea220864f3e70183b7e156303f40d26f88b99" => :lion
+  end
+
   head 'http://www.riverbankcomputing.co.uk/hg/sip', :using => :hg
 
   depends_on :python => :recommended
