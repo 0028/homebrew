0cd8907b1c4d0a3969198efc2af21b57b1db6af6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-25 23:53:10 +0000

    elb-tools: add 1.0.34.0 bottle.

--- Library/Formula/elb-tools.rb
+++ Library/Formula/elb-tools.rb
@@ -6,6 +6,13 @@ class ElbTools < AmazonWebServicesFormula
   version '1.0.34.0'
   sha1 '3d2c30a8fe8294a51463aac6ec926b908cc11f4a'
 
+  bottle do
+    cellar :any
+    sha1 "7e19bca612cdcf8bee2889c5dc6a8ac8e260aad6" => :mavericks
+    sha1 "3eb114c37a2dad2aa76eede5e44c7800264cf3a0" => :mountain_lion
+    sha1 "de5130d23efba14296f3b9989783efc351d41bc8" => :lion
+  end
+
   depends_on 'ec2-api-tools'
 
   def install
