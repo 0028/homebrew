7174f9b43dc89dfa8a9238ff3526c3caaedb6c61
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-25 00:59:39 +0000

    slicot: add 5.0+20101122 bottle.

--- Library/Formula/slicot.rb
+++ Library/Formula/slicot.rb
@@ -6,6 +6,13 @@ class Slicot < Formula
   version '5.0+20101122'
   sha1 'ec240abbf6d3d60da3a7dc21d22104abdfd86bd8'
 
+  bottle do
+    cellar :any
+    sha1 "e67d85ef83ce4eda8340323656c818a3f1432e7f" => :mavericks
+    sha1 "a1f2342437e1aea9c47e25273b12801a3554476c" => :mountain_lion
+    sha1 "b54fb3d6264f1e537c48e4a08190e4a96afef0d4" => :lion
+  end
+
   option 'with-default-integer-8', 'Build with 8-byte-wide integer type'
 
   depends_on :fortran
