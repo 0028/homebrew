496d7e11ff01732adfc2206fe3b22057c819bfb4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-26 17:34:29 +0000

    pastebinit: add 1.4.1 bottle.

--- Library/Formula/pastebinit.rb
+++ Library/Formula/pastebinit.rb
@@ -5,6 +5,11 @@ class Pastebinit < Formula
   url "https://launchpad.net/pastebinit/trunk/1.4.1/+download/pastebinit-1.4.1.tar.gz"
   sha1 "1482311559bc939bb78bbd3e05b15400105bb9c8"
 
+  bottle do
+    cellar :any
+    sha1 "9c0011579eeddede48a1220e55f1926f97d38060" => :mavericks
+  end
+
   depends_on "python3"
   depends_on "docbook2x" => :build
 
