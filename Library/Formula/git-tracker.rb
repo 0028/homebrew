c31c23daa4c034a46ff6f64be87ef31a28b98752
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-26 14:27:58 +0000

    git-tracker: add 1.6.2 bottle.

--- Library/Formula/git-tracker.rb
+++ Library/Formula/git-tracker.rb
@@ -7,6 +7,13 @@ class GitTracker < Formula
 
   head 'https://github.com/stevenharman/git_tracker.git'
 
+  bottle do
+    cellar :any
+    sha1 "e240fa5ba0495fb2da8564a8607d6376db81f315" => :mavericks
+    sha1 "0d7f79642f6639e715e5f259ffd164281ca611a0" => :mountain_lion
+    sha1 "4b3ac7c8baae2a3471c218f1027c97e474bc9a24" => :lion
+  end
+
   def install
     rake 'standalone:install', "prefix=#{prefix}"
   end
