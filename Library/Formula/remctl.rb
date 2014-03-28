8c880abd9c86ea7b2123fe83308edc1c4ef885d0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-28 19:03:15 +0000

    remctl: add 3.8 bottle.

--- Library/Formula/remctl.rb
+++ Library/Formula/remctl.rb
@@ -5,6 +5,12 @@ class Remctl < Formula
   url 'http://archives.eyrie.org/software/kerberos/remctl-3.8.tar.gz'
   sha1 '81d142cfeb1efa6582626e0c747139c83837b422'
 
+  bottle do
+    sha1 "83f9a3d09ad9a4aeebd39e877827ce5eda1ea79c" => :mavericks
+    sha1 "e1c8a776297f594e78e9c2f1cbed63c09cb14206" => :mountain_lion
+    sha1 "3a2bb15b64e98164151c29be048f5f588a7471ae" => :lion
+  end
+
   depends_on 'pcre'
   depends_on 'libevent'
 
