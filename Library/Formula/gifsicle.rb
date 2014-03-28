fb7c93cf7259991c2e85d1e95d9c27b365908074
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-28 15:23:06 +0000

    gifsicle: add 1.82 bottle.

--- Library/Formula/gifsicle.rb
+++ Library/Formula/gifsicle.rb
@@ -5,6 +5,13 @@ class Gifsicle < Formula
   url "http://www.lcdf.org/gifsicle/gifsicle-1.82.tar.gz"
   sha1 "2771af4ab39064df04a538bcb6f777d2ba3d628b"
 
+  bottle do
+    cellar :any
+    sha1 "23e5db1dbc18cc43bdb987643abfe752af34f9ef" => :mavericks
+    sha1 "704adc6ba43bb0ffcd2fc32d781b22ad387d4a4c" => :mountain_lion
+    sha1 "f3e84f5f4741c6c493d41ffd438ee109e8e551d7" => :lion
+  end
+
   depends_on :x11 => :optional
 
   def install
