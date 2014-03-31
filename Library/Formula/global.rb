5eb04f70abe0da55f0b8fa547aeadca4c64432c9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-31 20:08:18 +0100

    global: add 6.2.12 bottle.

--- Library/Formula/global.rb
+++ Library/Formula/global.rb
@@ -3,6 +3,12 @@ require 'formula'
 class Global < Formula
   homepage 'http://www.gnu.org/software/global/'
   url 'http://ftpmirror.gnu.org/global/global-6.2.12.tar.gz'
+  bottle do
+    sha1 "81160b83cf9bee1e5b97c1c1e40b10f340798f15" => :mavericks
+    sha1 "9dfc3ad14ae419089375dd44c4c6e618d1672063" => :mountain_lion
+    sha1 "8a5bc30cfd1733ddff466f28bd293a60fa175162" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/global/global-6.2.12.tar.gz'
   sha1 '1fc0948ee76185d38733750567a2bdb6b9b07304'
 
