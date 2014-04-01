a9ea54d1f5cffdcaa44a83309ada3be075e05b5c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-01 10:28:18 +0100

    ykpers: add 1.15.1 bottle.

--- Library/Formula/ykpers.rb
+++ Library/Formula/ykpers.rb
@@ -5,6 +5,13 @@ class Ykpers < Formula
   url 'http://opensource.yubico.com/yubikey-personalization/releases/ykpers-1.15.1.tar.gz'
   sha1 'dd1478cc106c624318dfe74bb2beda54f18f1fc8'
 
+  bottle do
+    cellar :any
+    sha1 "4e4f646f37ceb03cf67798daf9e8d3d9d2e32583" => :mavericks
+    sha1 "b1d126d47e703d00b975c79b9b36e3fa75665bc5" => :mountain_lion
+    sha1 "cbb4079ea22ec8b9cc75e6a47cc56144bd7d8ff9" => :lion
+  end
+
   option :universal
 
   depends_on 'libyubikey'
