6c2d9658e1ecb5e4e990846ceb5f98d1f5f51d54
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-02 23:48:34 +0100

    irssi-otr: add 0.3 bottle.

--- Library/Formula/irssi-otr.rb
+++ Library/Formula/irssi-otr.rb
@@ -5,6 +5,13 @@ class IrssiOtr < Formula
   url "ftp://download.tuxfamily.org/irssiotr/irssi-otr-0.3.tar.gz"
   sha1 "5b831dd64e7d84fcda45219b71f017233a6c327b"
 
+  bottle do
+    cellar :any
+    sha1 "d7f32df01349bcf0d1482057fe3ff48ef99830e2" => :mavericks
+    sha1 "0b8f8e605fbf2d54b57bee4e2f342392dd6d1169" => :mountain_lion
+    sha1 "d1d3755f3a2d71c88027ad6e2bf48b8f9585e362" => :lion
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on :python => :build
