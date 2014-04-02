8d4f1dbfaa04f4b69582afc27f4434a307e18d89
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-02 16:40:12 +0100

    rdup: add 1.1.14 bottle.

--- Library/Formula/rdup.rb
+++ Library/Formula/rdup.rb
@@ -4,6 +4,13 @@ class Rdup < Formula
   homepage 'http://archive.miek.nl/projects/rdup/index.html'
   url 'http://archive.miek.nl/projects/rdup/rdup-1.1.14.tar.bz2'
   sha1 '49dc7570122bfa362f36a26a2ffa8bfe8ad55182'
+  bottle do
+    cellar :any
+    sha1 "0412562abc22f96b358ffa7a6344b40d7560bd5a" => :mavericks
+    sha1 "50c9a17d307d20cc5aaef01ac1439bd404805497" => :mountain_lion
+    sha1 "1a76c16262076d33dd98696a5660d38a8fcf13a0" => :lion
+  end
+
   revision 1
 
   depends_on 'pkg-config' => :build
