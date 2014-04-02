dc57a388aafa36335502403d7c0f88e0cb7b9bda
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-02 13:37:56 +0100

    git-map: add 0.0.3 bottle.

--- Library/Formula/git-map.rb
+++ Library/Formula/git-map.rb
@@ -5,6 +5,13 @@ class GitMap < Formula
   url "https://github.com/clarkema/git-map/archive/v0.0.3.tar.gz"
   sha1 "eea43a160d2cf68d30fc421da8b8ae96dccdd89e"
 
+  bottle do
+    cellar :any
+    sha1 "27b0ac2debc02616ced7c31f2ecbb4f151648d3c" => :mavericks
+    sha1 "f162676c631eacd4ddee6c8c3163326b4fe1a15e" => :mountain_lion
+    sha1 "565e49562203c170f1a91889fe125d4bb3f46abe" => :lion
+  end
+
   def install
     system "pod2man ./bin/git-map > git-map.1"
     bin.install "bin/git-map"
