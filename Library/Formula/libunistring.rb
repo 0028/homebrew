77df622b24298c0068897010c82a4d26eaa27b36
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-03 01:33:25 +0100

    libunistring: add 0.9.3 bottle.

--- Library/Formula/libunistring.rb
+++ Library/Formula/libunistring.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Libunistring < Formula
   homepage 'http://www.gnu.org/software/libunistring/'
   url 'http://ftpmirror.gnu.org/libunistring/libunistring-0.9.3.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "fd93c4c18b2b526b77d9dfeefd11ff89a6effc02" => :mavericks
+    sha1 "4bbf7ca720de8b13e2ebac7b5bb88ef2057b21a4" => :mountain_lion
+    sha1 "ed2d278f23e772a0401897a579deff47606b5d03" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/libunistring/libunistring-0.9.3.tar.gz'
   sha1 'e1ea13c24a30bc93932d19eb5ad0704a618506dd'
 
