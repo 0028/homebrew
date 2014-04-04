2ecb395b607f88f2f1b1f778b330e04ab6c0abb0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-04 22:33:29 +0100

    mupdf: add 1.3 bottle.

--- Library/Formula/mupdf.rb
+++ Library/Formula/mupdf.rb
@@ -4,6 +4,13 @@ class Mupdf < Formula
   homepage 'http://mupdf.com'
   url 'https://mupdf.googlecode.com/files/mupdf-1.3-source.tar.gz'
   sha1 '082325aceb5565b07b82c2b6cc52a97533e03cf9'
+  bottle do
+    cellar :any
+    sha1 "ecc27a429023375ff84072ac688844a4f673e7bf" => :mavericks
+    sha1 "b5bfcc0e07686894c176524e0086df3dff035da0" => :mountain_lion
+    sha1 "84d3383690082690d2c5952aff72b3eadc69213b" => :lion
+  end
+
   revision 1
 
   depends_on :macos => :snow_leopard
