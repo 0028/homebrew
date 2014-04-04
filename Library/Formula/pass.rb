d538ce276d2357a6f71261ea12e805efc81c4abe
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-04 08:15:50 +0100

    pass: add 1.4.2 bottle.

--- Library/Formula/pass.rb
+++ Library/Formula/pass.rb
@@ -5,6 +5,13 @@ class Pass < Formula
   url 'http://git.zx2c4.com/password-store/snapshot/password-store-1.4.2.tar.xz'
   sha256 'a10a8016a3d6bb32891a9a4086ac0a049587db1a55c1ac3b431c66189bc1a302'
 
+  bottle do
+    cellar :any
+    sha1 "38ecae8e327e7cd89b9695537e7217bf3def0ef7" => :mavericks
+    sha1 "d3fa71da6a0b4451872d9ee03aee7014bd177b2f" => :mountain_lion
+    sha1 "61710a8464fd7d9a7e53b288af6581af30e26bd5" => :lion
+  end
+
   head 'http://git.zx2c4.com/password-store', :using => :git
 
   depends_on 'pwgen'
