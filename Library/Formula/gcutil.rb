931f71592ed6eafedf7e2b8a78ac84ba7db9c4e1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-04 12:32:52 +0100

    gcutil: add 1.15.0 bottle.

--- Library/Formula/gcutil.rb
+++ Library/Formula/gcutil.rb
@@ -5,6 +5,13 @@ class Gcutil < Formula
   url 'https://dl.google.com/dl/cloudsdk/release/artifacts/gcutil-1.15.0.zip'
   sha1 '3ef940bda17469e742f77f5c0918613c5377f058'
 
+  bottle do
+    cellar :any
+    sha1 "30b2578be810f08607d828b9a4e5d7130ccfee66" => :mavericks
+    sha1 "b3ecb0ebb9edfe178bdee5f59ce76f3601e8a937" => :mountain_lion
+    sha1 "e0ea7539fd6103c5b657b56d97c031e448d3b268" => :lion
+  end
+
   def install
     libexec.install 'gcutil', 'lib'
     bin.install_symlink libexec/"gcutil"
