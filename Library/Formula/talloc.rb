f368f5c58e3c58f2a44f7d62685a9f88c55e8488
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-05 04:54:25 +0100

    talloc: add 2.1.0 bottle.

--- Library/Formula/talloc.rb
+++ Library/Formula/talloc.rb
@@ -5,6 +5,13 @@ class Talloc < Formula
   url 'http://www.samba.org/ftp/talloc/talloc-2.1.0.tar.gz'
   sha1 'd079597f3f02a7ca3716645728a8a46016a1dd7d'
 
+  bottle do
+    cellar :any
+    sha1 "04f84baea93af024d37cb988bdc2d2e7078ca439" => :mavericks
+    sha1 "81fea8e0f1ef55447146d03626ffd8a922554e5b" => :mountain_lion
+    sha1 "73885a86a3825f2343385e5e92d4bdd8ffa02373" => :lion
+  end
+
   conflicts_with 'samba', :because => 'both install `include/talloc.h`'
 
   def install
