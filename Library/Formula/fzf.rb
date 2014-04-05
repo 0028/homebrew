446fd5ab046659b4c284c92cff1a14c7300cbf88
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-05 05:07:51 +0100

    fzf: add 0.8.3 bottle.

--- Library/Formula/fzf.rb
+++ Library/Formula/fzf.rb
@@ -5,6 +5,13 @@ class Fzf < Formula
   url "https://github.com/junegunn/fzf/archive/0.8.3.tar.gz"
   sha1 "9d598e42a3dbbad7df3dfae138a759042e004307"
 
+  bottle do
+    cellar :any
+    sha1 "175bf55cafc1c7c84d172fbdbd7f4caffdb336ac" => :mavericks
+    sha1 "2b78c5b463676794f1ef266ce09e0ea5677503bc" => :mountain_lion
+    sha1 "03e3262a41bc1fda4eca7172a2c6fa90e7977b84" => :lion
+  end
+
   def install
     prefix.install "install", "fzf", "fzf-completion.bash", "fzf-completion.zsh"
     bin.install_symlink prefix/"fzf"
