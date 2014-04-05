926e8fe38801634feaebef814e1069ff3734e9d2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-05 04:54:25 +0100

    tevent: add 0.9.21 bottle.

--- Library/Formula/tevent.rb
+++ Library/Formula/tevent.rb
@@ -5,6 +5,13 @@ class Tevent < Formula
   url "http://www.samba.org/ftp/tevent/tevent-0.9.21.tar.gz"
   sha1 "daa1a4f457773be2e0f645beede93db6943224a5"
 
+  bottle do
+    cellar :any
+    sha1 "6255c03addc1bbfced4dbe9a02aaf6a57e4f237a" => :mavericks
+    sha1 "efe332ac4a77fc84600cf9a84cb1cc6f7bb6b1fc" => :mountain_lion
+    sha1 "ff525cabffd1c92f520efd7fba44cc59551a18ab" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "talloc" # ">= 2.1.0"
   conflicts_with "samba", :because => "both install `include/tevent.h`"
