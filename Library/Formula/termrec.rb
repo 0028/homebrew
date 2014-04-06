9a1892815d6efc597812fa4b2e49fdacc0742375
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-07 00:01:16 +0100

    termrec: add 0.17 bottle.

--- Library/Formula/termrec.rb
+++ Library/Formula/termrec.rb
@@ -4,6 +4,13 @@ class Termrec < Formula
   homepage "http://angband.pl/termrec.html"
   url "https://github.com/kilobyte/termrec/archive/0.17.tar.gz"
   sha1 "45df1b35b7f236fafd1e4db9c45543501dfde359"
+  bottle do
+    cellar :any
+    sha1 "e191441460af04f40e96e69cfe35d4e4ab97d143" => :mavericks
+    sha1 "1f1fdd5aea93622fa6cdb21205c65afa1a596b35" => :mountain_lion
+    sha1 "53cf5ba225b89baf8127359475996b5f13e7fa3d" => :lion
+  end
+
   head "http://angband.pl/git/termrec/", :using => :git
   depends_on "libtool" => :build
   depends_on "automake" => :build
