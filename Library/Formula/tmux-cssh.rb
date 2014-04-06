ebdb4ea42dd8ba6384562f787be8a6f7de91b7bb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-06 21:46:31 +0100

    tmux-cssh: add 0.1 bottle.

--- Library/Formula/tmux-cssh.rb
+++ Library/Formula/tmux-cssh.rb
@@ -5,6 +5,13 @@ class TmuxCssh < Formula
   url 'https://github.com/dennishafemann/tmux-cssh/archive/0.1.tar.gz'
   sha1 '477a9079775ce48afe0c90699fa69f368e69cdfd'
 
+  bottle do
+    cellar :any
+    sha1 "1f5986417dad5da3cc78d34bfcd9b4e1a81cd498" => :mavericks
+    sha1 "6fa2b2a5d6bfbe32ef19d52b03c28136b6ead05c" => :mountain_lion
+    sha1 "3eeca798b0172cd100b5095fd1ade45ffe77c341" => :lion
+  end
+
   depends_on 'tmux'
 
   def install
