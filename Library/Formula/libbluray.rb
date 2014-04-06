2d6cbd5bf3a5b4bd3b3ab77147d8bb974c88e873
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-06 23:58:54 +0100

    libbluray: add 0.5.0 bottle.

--- Library/Formula/libbluray.rb
+++ Library/Formula/libbluray.rb
@@ -5,6 +5,13 @@ class Libbluray < Formula
   url 'ftp://ftp.videolan.org/pub/videolan/libbluray/0.5.0/libbluray-0.5.0.tar.bz2'
   sha1 '1a9c61daefc31438f9165e7681c563d0524b2d3e'
 
+  bottle do
+    cellar :any
+    sha1 "7b069feafe4a95547cf6a965740ca563546c127c" => :mavericks
+    sha1 "c9be414ed271a6b0766378de2e56ed0c4c74a099" => :mountain_lion
+    sha1 "9e0687bd8323a100374b2072bf41f622c3098a5a" => :lion
+  end
+
   head do
     url 'git://git.videolan.org/libbluray.git'
 
