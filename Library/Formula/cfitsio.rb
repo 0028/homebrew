d7a8770d8ebb49cbc107484bab53895ad35ab4b1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-09 04:34:34 +0100

    cfitsio: add 3.360 bottle.

--- Library/Formula/cfitsio.rb
+++ Library/Formula/cfitsio.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Cfitsio < Formula
   homepage 'http://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html'
   url 'http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3360.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "e3cb3c6cf7fee5f4a5a4e5632081d420037a0940" => :mavericks
+    sha1 "12afafd4938a4f1f91c8b6b2b1aed74910c10943" => :mountain_lion
+    sha1 "8c82ed914d6ad008f9a068dc3c04acf094de8797" => :lion
+  end
+
   mirror 'https://downloads.sourceforge.net/project/machomebrew/mirror/cfitsio-3.360.tar.gz'
   sha1 '0d3099721a6bf1e763f158c447a74c5e8192412d'
   version '3.360'
