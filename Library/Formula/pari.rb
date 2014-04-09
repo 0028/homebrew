c2e3929e96ed9803b489d5b316202b60105c0cee
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-09 16:04:44 +0100

    pari: add 2.5.5 bottle.

--- Library/Formula/pari.rb
+++ Library/Formula/pari.rb
@@ -5,6 +5,12 @@ class Pari < Formula
   url 'http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.5.5.tar.gz'
   sha1 '77637f935ee4a3b78e7015cef00146bd2f7e96bc'
 
+  bottle do
+    sha1 "e6a1b6f23ff8a26056772ef38242c5b68d81de55" => :mavericks
+    sha1 "9fc54badf087b8a26f001cdc990da37eda57a8c2" => :mountain_lion
+    sha1 "fe15d38c6eb910cdd5e2ed58cc61f3429f8458f2" => :lion
+  end
+
   depends_on 'readline'
   depends_on :x11
 
