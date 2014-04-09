8fb5a01389dfcc1b0d08658e55bb6b4227fb017c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-09 21:24:33 +0100

    dart: add 1.3.0 bottle.

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -5,6 +5,13 @@ class Dart < Formula
 
   if MacOS.prefer_64_bit?
     url 'http://storage.googleapis.com/dart-archive/channels/stable/release/34825/sdk/dartsdk-macos-x64-release.zip'
+  bottle do
+    cellar :any
+    sha1 "f04e5769f6bf5e0a00b00b18a5259ec3915552eb" => :mavericks
+    sha1 "35688ac27362e1e66bb79308cfc6bf32d1777246" => :mountain_lion
+    sha1 "b19218eb24ab5bff781d5de4b567dcb682eaaef5" => :lion
+  end
+
     sha1 '89d372cd66de991b02ac54fe85371a3ef7e59242'
   else
     url 'http://storage.googleapis.com/dart-archive/channels/stable/release/34825/sdk/dartsdk-macos-ia32-release.zip'
