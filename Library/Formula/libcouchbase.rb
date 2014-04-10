54170dd73cc9afc9f2f7bbb19598b232bd7acd3e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-10 01:49:30 +0100

    libcouchbase: add 2.3.0 bottle.

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -5,6 +5,12 @@ class Libcouchbase < Formula
   url 'http://packages.couchbase.com/clients/c/libcouchbase-2.3.0.tar.gz'
   sha1 'dbb97594582e5a9240fa8090bb756990f2d1b550'
 
+  bottle do
+    sha1 "61e3a8adfe7130cdcbdbd7da683c950b27e1d342" => :mavericks
+    sha1 "29b8f5208959010a15f3f6b72ad6c160be2bf770" => :mountain_lion
+    sha1 "0f7ce2efa5f612cfc1b4f4f304d1f8e35e5748b7" => :lion
+  end
+
   option :universal
   option 'with-libev-plugin', 'Build libev IO plugin (will pull libev dependency)'
   option 'without-libevent-plugin', 'Do not build libevent plugin (will remove libevent dependency)'
