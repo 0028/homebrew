2a565c0906c3d757c109c3567208134a0da01c02
Martin Mauch <mauch@crealytics.de>
2014-04-10 23:13:40 +0200

    Use tagged version.

--- Library/Formula/pex.rb
+++ Library/Formula/pex.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Pex < Formula
   homepage "https://github.com/petere/pex"
-  url "https://github.com/petere/pex/archive/5d3ff7b76412ead6f844318c74a2da5fc75d6b52.tar.gz"
+  url "https://github.com/petere/pex/archive/1.20140409.tar.gz"
   sha1 "3288c90946392257400fcf909432f9a50db5fae6"
   version "1.0"
 
