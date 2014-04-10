902741ce2001300baf94e0afeb0fa38a99acf10d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-10 20:00:34 +0100

    yara: add 2.1.0 bottle.

--- Library/Formula/yara.rb
+++ Library/Formula/yara.rb
@@ -5,6 +5,13 @@ class Yara < Formula
   url 'https://github.com/plusvic/yara/archive/2.1.0.tar.gz'
   sha1 '8289c281a44c933e11de25953f3910fe9f8ee82e'
 
+  bottle do
+    cellar :any
+    sha1 "6c827febf5feca960cc6971f1b289062701c7d15" => :mavericks
+    sha1 "ae455d96ee279bc1b94610f6bb9e6bab567f6433" => :mountain_lion
+    sha1 "460735bd669c698bec49b793ce349f66d3462a32" => :lion
+  end
+
   depends_on 'pcre'
   depends_on 'libtool' => :build
   depends_on 'autoconf' => :build
