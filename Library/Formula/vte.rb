59e6d8c89a5b2cfc2cdae3fe45b276657025af48
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-11 20:53:31 +0100

    vte: add 0.28.0 bottle.

--- Library/Formula/vte.rb
+++ Library/Formula/vte.rb
@@ -5,6 +5,12 @@ class Vte < Formula
   url 'http://ftp.gnome.org/pub/gnome/sources/vte/0.28/vte-0.28.0.tar.bz2'
   sha1 '49b66a0346da09c72f59e5c544cc5b50e7de9bc1'
 
+  bottle do
+    sha1 "a380d42020de823c0b2c96bfd5361ae0e9b34d00" => :mavericks
+    sha1 "8a8d532cb2f33bd08352482f6cbba6413e133801" => :mountain_lion
+    sha1 "843dd5903ea4e6ab580ffd967cce8fc25881790c" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
   depends_on 'gettext'
