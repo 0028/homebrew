9bc4388363d32999fb2fb858a7f5a6b1f2aae4e1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-12 19:59:30 +0100

    somagic: add 1.1 bottle.

--- Library/Formula/somagic.rb
+++ Library/Formula/somagic.rb
@@ -5,6 +5,13 @@ class Somagic < Formula
   url 'https://easycap-somagic-linux.googlecode.com/files/somagic-easycap_1.1.tar.gz'
   sha1 '97cda956ea319fdd9aa9be9aff222a3e804deb91'
 
+  bottle do
+    cellar :any
+    sha1 "96e1b73212686646a925cfbdaa6f35b9fbd40b7b" => :mavericks
+    sha1 "349cb0766a996a8957ad766a3a132be10cf5a002" => :mountain_lion
+    sha1 "5d910722787e9ab31198ec1ffd105f63893699ad" => :lion
+  end
+
   depends_on 'libusb'
   depends_on 'libgcrypt'
   depends_on 'somagic-tools'
