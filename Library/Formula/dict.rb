349cd077f28a2adbc03592981453888cc27c9c1c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-12 14:44:47 +0100

    dict: add 1.9.15 bottle.

--- Library/Formula/dict.rb
+++ Library/Formula/dict.rb
@@ -5,6 +5,12 @@ class Dict < Formula
   url 'http://en.sourceforge.jp/frs/g_redir.php?m=jaist&f=%2Fdict%2Fdictd%2Fdictd-1.9.15%2Fdictd-1.9.15.tar.gz'
   sha1 '081ea97a4a6a936855af30d9c2a31f5733985822'
 
+  bottle do
+    sha1 "f6d9776816f8b2324dca76f16c0508adc64a8c73" => :mavericks
+    sha1 "f238609b50eb291a49b5f23dada6583b0673c19a" => :mountain_lion
+    sha1 "5a34b380d7602cf589f6023ef87b0c434a9ddb78" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
