81b88cfaa656cb3ebdd0efb2a547ecd495073c60
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-13 20:34:24 +0100

    rzip: add 2.1 bottle.

--- Library/Formula/rzip.rb
+++ Library/Formula/rzip.rb
@@ -5,6 +5,13 @@ class Rzip < Formula
   url 'http://rzip.samba.org/ftp/rzip/rzip-2.1.tar.gz'
   sha1 'efeafc7a5bdd7daa0cea8d797ff21aa28bdfc8d9'
 
+  bottle do
+    cellar :any
+    sha1 "91091971e5d1e4546e528aa1ceb21e530c29ff27" => :mavericks
+    sha1 "86d1fb332f18559e31acf52a31cbdcae3a2db5ab" => :mountain_lion
+    sha1 "f72d63a26e4d0646e20780202ad1659a0d7fec95" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
