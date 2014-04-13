2a350946fafe40845f86f1d4c9d6b54eb98a7985
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-13 15:25:32 +0100

    gnu-apl: add 1.3 bottle.

--- Library/Formula/gnu-apl.rb
+++ Library/Formula/gnu-apl.rb
@@ -3,6 +3,10 @@ require 'formula'
 class GnuApl < Formula
   homepage 'http://www.gnu.org/software/apl/'
   url 'http://ftpmirror.gnu.org/apl/apl-1.3.tar.gz'
+  bottle do
+    sha1 "32c1bd12eddc50d11202f21c9a898d6fbaf3cbe0" => :mavericks
+  end
+
   mirror 'http://ftp.gnu.org/gnu/apl/apl-1.3.tar.gz'
   sha1 'f4cd44a716dc5c5af1cd88811e10efa03d327fd2'
 
