01a74376e919de01e8de3d6c4979a7bd1da0b384
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-13 23:49:47 +0100

    fwknop: add 2.6.1 bottle.

--- Library/Formula/fwknop.rb
+++ Library/Formula/fwknop.rb
@@ -6,6 +6,12 @@ class Fwknop < Formula
   url 'https://github.com/mrash/fwknop/archive/2.6.1.tar.gz'
   sha1 'f0bb89358262431cc4319223d582a24277a12dad'
 
+  bottle do
+    sha1 "06d8eb2c014d7bd1eb53846e613b7a911aaafa5b" => :mavericks
+    sha1 "d22e2e6127b7b32a48fb14779e0d1a56541e4784" => :mountain_lion
+    sha1 "fb913f311af13d733e5a5077d2c1478d4c907f3c" => :lion
+  end
+
   depends_on :automake
   depends_on :autoconf
   depends_on :libtool
