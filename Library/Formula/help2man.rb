67dec794116d94950afbd1e6dfbbf941f83e547b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-15 17:17:03 +0100

    help2man: add 1.45.1 bottle.

--- Library/Formula/help2man.rb
+++ Library/Formula/help2man.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Help2man < Formula
   homepage 'http://www.gnu.org/software/help2man/'
   url 'http://ftpmirror.gnu.org/help2man/help2man-1.45.1.tar.xz'
+  bottle do
+    cellar :any
+    sha1 "6953cd6a82eb8bb33601edec88cf24036ee88cbf" => :mavericks
+    sha1 "0f50faddc5280923076aa08a7b6de8c822ffb633" => :mountain_lion
+    sha1 "ae536876cbfce22ac62ce74920d0db6893e62c5e" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/help2man/help2man-1.45.1.tar.xz'
   sha256 'c70fc791e6d13240327955de355244371b00bb1c9e247d5693ed601b716467c2'
 
