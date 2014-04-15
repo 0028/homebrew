5c53d81c851649723ca55355488603b3b70c9d27
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-15 17:17:03 +0100

    wdiff: add 1.2.2 bottle.

--- Library/Formula/wdiff.rb
+++ Library/Formula/wdiff.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Wdiff < Formula
   homepage 'http://www.gnu.org/software/wdiff/'
   url 'http://ftpmirror.gnu.org/wdiff/wdiff-1.2.2.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "6e4e460fe62d0b917178fdb2da17bd0445898779" => :mavericks
+    sha1 "1c1450bb89c1d60453cd8982b011d869468a0527" => :mountain_lion
+    sha1 "9b3e267888c0f8cf7aa89368a73092262bfabd64" => :lion
+  end
+
   mirror 'http://ftp.gnu.org/gnu/wdiff/wdiff-1.2.2.tar.gz'
   sha1 'c93b5cb593257d814212e15fc371ff6c6b952c3d'
 
