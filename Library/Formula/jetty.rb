06d17939e93afb5e655a4e0640387e435ae366a1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-16 16:09:53 +0100

    jetty: add 9.1.4 bottle.

--- Library/Formula/jetty.rb
+++ Library/Formula/jetty.rb
@@ -6,6 +6,13 @@ class Jetty < Formula
   version '9.1.4'
   sha1 'f26ff39fec09892195909c417ffffe9f3c08cc72'
 
+  bottle do
+    cellar :any
+    sha1 "180fed1b95bf9d0cc0bfa6e089b568f2e4eafc85" => :mavericks
+    sha1 "a396965fff53337242e5b1ad490933dda33d28c5" => :mountain_lion
+    sha1 "c7f5dc36a4d2cc417f6faec033d3c70352b7ac81" => :lion
+  end
+
   def install
     libexec.install Dir['*']
     (libexec+'logs').mkpath
