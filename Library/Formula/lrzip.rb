286bff3ecb8c25cfd6aa5b0254bfb347911e7d2b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-17 00:39:43 +0100

    lrzip: add 0.614 bottle.

--- Library/Formula/lrzip.rb
+++ Library/Formula/lrzip.rb
@@ -5,6 +5,13 @@ class Lrzip < Formula
   url 'http://ck.kolivas.org/apps/lrzip/lrzip-0.614.tar.bz2'
   sha1 'b3c2149c9ca353f595d7f2774262244cc56a7500'
 
+  bottle do
+    cellar :any
+    sha1 "4f3b254781c6694f39f6e9d68e8044bb015600ac" => :mavericks
+    sha1 "706b462ba71735822d4a995527b3bfd58228913c" => :mountain_lion
+    sha1 "e0faf2e2d246c7ca4cbb3c89a4807a68c8d44a6d" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on "lzo"
 
