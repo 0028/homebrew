1bbe52c5537e357f46c1302ac6338e8c6f0d220c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-16 13:41:33 +0100

    hashpump: add 1.0.0 bottle.

--- Library/Formula/hashpump.rb
+++ Library/Formula/hashpump.rb
@@ -5,6 +5,13 @@ class Hashpump < Formula
   url "https://github.com/bwall/HashPump/archive/v1.0.0.tar.gz"
   sha1 "217fed29f90f224db3a2e8b8309523d56f477a41"
 
+  bottle do
+    cellar :any
+    sha1 "38e6e21bd214aaeeb0d160c4f5460a8f0dc37cd0" => :mavericks
+    sha1 "a5b9f42f7a44052a7457ac20c392fdad359f9842" => :mountain_lion
+    sha1 "2e98d65419a962f8cf5e84359ec86b1f26e49b96" => :lion
+  end
+
   def install
     bin.mkpath
     system "make", "install", "INSTALLLOCATION=#{bin}"
