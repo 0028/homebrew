a9ad6eac36d8d11e887bbca507888d8faabfb33b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-16 18:04:13 +0100

    pex: add 1.0 bottle.

--- Library/Formula/pex.rb
+++ Library/Formula/pex.rb
@@ -6,6 +6,13 @@ class Pex < Formula
   sha1 "7cc652cd89bc6c6bf2488c8eb1ee91588053262a"
   version "1.0"
 
+  bottle do
+    cellar :any
+    sha1 "4a8b85fad79d21604571e18eb15dab89d4d6100e" => :mavericks
+    sha1 "93e7b87ab4db0182310bc953a34f2260df3f0860" => :mountain_lion
+    sha1 "f3573e88998108214584b99c0510f4aaa19b8f6f" => :lion
+  end
+
   depends_on :postgresql
 
   def pex(bin=nil)
