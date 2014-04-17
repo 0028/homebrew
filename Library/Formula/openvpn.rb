598406f0597bd0c7ad8e5a167e51bc54708cfe6f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-17 16:32:46 +0100

    openvpn: add 2.3.2 bottle.

--- Library/Formula/openvpn.rb
+++ Library/Formula/openvpn.rb
@@ -3,6 +3,10 @@ require 'formula'
 class Openvpn < Formula
   homepage 'http://openvpn.net/index.php/download/community-downloads.html'
   url 'http://build.openvpn.net/downloads/releases/openvpn-2.3.2.tar.gz'
+  bottle do
+    sha1 "db24c59b01d92e430bda024281c0104eaf9b5ad9" => :mavericks
+  end
+
   mirror 'http://swupdate.openvpn.org/community/releases/openvpn-2.3.2.tar.gz'
   sha256 '20bda3f9debb9a52db262aecddfa4e814050a9404a9106136b7e3b6f7ef36ffc'
 
