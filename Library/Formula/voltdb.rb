a940b3d44106f35997c2ceeeb8448776c502ee65
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-17 16:32:46 +0100

    voltdb: add 3.7 bottle.

--- Library/Formula/voltdb.rb
+++ Library/Formula/voltdb.rb
@@ -6,6 +6,11 @@ class Voltdb < Formula
   url 'https://github.com/VoltDB/voltdb/archive/voltdb-3.7.tar.gz'
   sha1 'f6a8cf8652b7247ea39bfa2f6fa410a15ffc6ab5'
 
+  bottle do
+    cellar :any
+    sha1 "291adcd10912f469c9f7f0cd79eb9742e267db44" => :mavericks
+  end
+
   depends_on :ant
 
   def install
