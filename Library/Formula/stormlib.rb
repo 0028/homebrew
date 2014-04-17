75043a38a9239728340fc016df42385a2d095dbf
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-18 00:54:15 +0100

    stormlib: add 9.00 bottle.

--- Library/Formula/stormlib.rb
+++ Library/Formula/stormlib.rb
@@ -6,6 +6,13 @@ class Stormlib < Formula
   sha1 "d1c4f458c0f7a87843c983cf2823cfcea45f3cec"
   head "https://github.com/stormlib/StormLib.git"
 
+  bottle do
+    cellar :any
+    sha1 "3798f0a6bf82270e06f94e280db2c77260c4f126" => :mavericks
+    sha1 "8c512b34b84099ffab2d0ad7ec6b7a0a74648dc7" => :mountain_lion
+    sha1 "d204a6dbaee8ac10b529cc93a512d763713c99f8" => :lion
+  end
+
   depends_on "cmake" => :build
 
   def install
