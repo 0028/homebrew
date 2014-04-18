dac0d7974248f00df77981e65c45e30ee0913c09
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 00:38:55 +0100

    snapraid: add 6.1 bottle.

--- Library/Formula/snapraid.rb
+++ Library/Formula/snapraid.rb
@@ -6,6 +6,13 @@ class Snapraid < Formula
   url 'https://downloads.sourceforge.net/project/snapraid/snapraid-6.1.tar.gz'
   sha1 '64469900098b297a90d4ccc3e41cd9f447ba76ce'
 
+  bottle do
+    cellar :any
+    sha1 "1a7b33b1007aa45160ff7fb5ffef46c9f5d6aa7a" => :mavericks
+    sha1 "c348214ab153628b2aba55dc84c112e4e6cfb1e4" => :mountain_lion
+    sha1 "8587892b30e0804f15329f84a5071fe6d5bfd873" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
