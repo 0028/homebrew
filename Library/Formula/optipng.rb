b31cb3d22b47fa8aab29e52a582660e727a0668d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 00:38:55 +0100

    optipng: add 0.7.5 bottle.

--- Library/Formula/optipng.rb
+++ Library/Formula/optipng.rb
@@ -6,6 +6,13 @@ class Optipng < Formula
   url 'https://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.5/optipng-0.7.5.tar.gz'
   sha1 '30b6c333d74fc0f5dc83004aace252fa3321368b'
 
+  bottle do
+    cellar :any
+    sha1 "b4c8d39ae8d09a341ae25b2d14af8e47b8f2d4b2" => :mavericks
+    sha1 "e7a1875acf9a0747e1e11873506b8ffa4bd13405" => :mountain_lion
+    sha1 "541b030c052c02a05c13128b34dabc571f546435" => :lion
+  end
+
   def install
     system "./configure", "--with-system-zlib",
                           "--prefix=#{prefix}",
