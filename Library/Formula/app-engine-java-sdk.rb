38577307c017d2b62a95243919ba73a44556c93c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 00:06:21 +0100

    app-engine-java-sdk: add 1.9.3 bottle.

--- Library/Formula/app-engine-java-sdk.rb
+++ Library/Formula/app-engine-java-sdk.rb
@@ -5,6 +5,13 @@ class AppEngineJavaSdk < Formula
   url 'https://commondatastorage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.3.zip'
   sha1 '4f62f99c7be21637903b312ba4431c6222bfca16'
 
+  bottle do
+    cellar :any
+    sha1 "72be87275b0ef9894c5c75b767c17dee1c58da88" => :mavericks
+    sha1 "965303ac9e28f7b2e07a07f2c90bf2a8bca8587e" => :mountain_lion
+    sha1 "3fe7e7daae87ea400ed6df6489d42d472d57ff85" => :lion
+  end
+
   def install
     rm Dir['bin/*.cmd']
     libexec.install Dir['*']
