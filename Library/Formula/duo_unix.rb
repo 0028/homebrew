495a58b17f5995f95d9ca634a2061559a90f284a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-18 20:26:14 +0100

    duo_unix: add 1.9.10 bottle.

--- Library/Formula/duo_unix.rb
+++ Library/Formula/duo_unix.rb
@@ -5,6 +5,12 @@ class DuoUnix < Formula
   url "https://dl.duosecurity.com/duo_unix-1.9.10.tar.gz"
   sha1 "13977071bdcb24b5d9de13e4fe1a973d60885b37"
 
+  bottle do
+    sha1 "566b8e39c00f04851b88424ff8da34cd06d23680" => :mavericks
+    sha1 "ed3bba9d238ce41b5ed22a9e2cc7ebf0b0d65e9c" => :mountain_lion
+    sha1 "a6b430bb6f7e1dcd94bd47633db842f4f34f25d7" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--sysconfdir=#{etc}"
