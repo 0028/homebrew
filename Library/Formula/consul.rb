0dfe26affe01ed69f76bea50d9506c38d0b2abc3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-18 11:04:06 +0100

    consul: add 0.1.0 bottle.

--- Library/Formula/consul.rb
+++ Library/Formula/consul.rb
@@ -9,6 +9,13 @@ class Consul < Formula
   sha1 "08dfd233011e6fe77928b6dd5d0221b5379250ce"
   version "0.1.0"
 
+  bottle do
+    cellar :any
+    sha1 "a11eb6aeed0157e62e8c31d209c639a66834d316" => :mavericks
+    sha1 "ec973893adb9a767268dc2846e9c00744d6dcdad" => :mountain_lion
+    sha1 "3735216e97dad97edbd616665d66e8b8aad1dd07" => :lion
+  end
+
   def install
     bin.install "consul"
   end
