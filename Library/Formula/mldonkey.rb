d80ad4797f2fbd97b1f1281ee5352e8cdf3f7197
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 00:15:18 +0100

    mldonkey: add 3.1.5 bottle.

--- Library/Formula/mldonkey.rb
+++ Library/Formula/mldonkey.rb
@@ -5,6 +5,12 @@ class Mldonkey < Formula
   url 'https://downloads.sourceforge.net/project/mldonkey/mldonkey/3.1.5/mldonkey-3.1.5.tar.bz2'
   sha1 '7bc4f9272ecfe6403eef7062766b26bf321e3015'
 
+  bottle do
+    sha1 "482e15b2e530fe70434d25e33a48c89a5644914d" => :mavericks
+    sha1 "ffaf19f464ce4bda5453206bdd4985fab148d20a" => :mountain_lion
+    sha1 "b64fd4a75408de8a80fd59fe500cdd5f2a023b0d" => :lion
+  end
+
   option "with-x", "Build mldonkey with X11 support"
 
   depends_on 'pkg-config' => :build
