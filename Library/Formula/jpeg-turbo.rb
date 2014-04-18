1fbd562cfd06eecfd1a956bbb4c62dc5d51e1509
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 00:38:56 +0100

    jpeg-turbo: add 1.3.1 bottle.

--- Library/Formula/jpeg-turbo.rb
+++ Library/Formula/jpeg-turbo.rb
@@ -5,6 +5,13 @@ class JpegTurbo < Formula
   url 'https://downloads.sourceforge.net/project/libjpeg-turbo/1.3.1/libjpeg-turbo-1.3.1.tar.gz'
   sha1 '5fa19252e5ca992cfa40446a0210ceff55fbe468'
 
+  bottle do
+    cellar :any
+    sha1 "5ed930db925daab1f18c018a8e851c1f96969c63" => :mavericks
+    sha1 "ced4deab5503990984d848d150270bfd4bdc92fd" => :mountain_lion
+    sha1 "c2dd02b29014e8d50770f51fb724b468cdd1d55a" => :lion
+  end
+
   depends_on 'nasm' => :build if MacOS.prefer_64_bit?
 
   keg_only "libjpeg-turbo is not linked to prevent conflicts with the standard libjpeg."
