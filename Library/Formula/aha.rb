0460c33e14972f9c77f6746c24f44c5b3b97aac2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 17:20:40 +0100

    aha: add 0.4.7.1 bottle.

--- Library/Formula/aha.rb
+++ Library/Formula/aha.rb
@@ -5,6 +5,13 @@ class Aha < Formula
   url "https://github.com/theZiz/aha/archive/0.4.7.1.tar.gz"
   sha1 "d428499b5e27bd514ca0ae6826a348b233534f59"
 
+  bottle do
+    cellar :any
+    sha1 "74f0183f2bdb15f12bef2219134a63139899658f" => :mavericks
+    sha1 "a96c0d48f12c15172bc1d2502de3961a8afdc5af" => :mountain_lion
+    sha1 "a57b040bdf446d213cd7d58444fab628bb69ec93" => :lion
+  end
+
   def install
     system "make"
     bin.install "aha"
