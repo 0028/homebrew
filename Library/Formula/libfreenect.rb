801b21e5c979a609578f0a8f4fa63bef6dcb7be8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 17:20:39 +0100

    libfreenect: add 0.4.1 bottle.

--- Library/Formula/libfreenect.rb
+++ Library/Formula/libfreenect.rb
@@ -7,6 +7,13 @@ class Libfreenect < Formula
 
   head 'https://github.com/OpenKinect/libfreenect.git'
 
+  bottle do
+    cellar :any
+    sha1 "5bef9967d0c05e85b5b62e10f2d808ea3c49c966" => :mavericks
+    sha1 "0c96ac09902613ac7da746f98c31c66be2be60d6" => :mountain_lion
+    sha1 "7fce9eb5b78ce544aafc5c03cad23ce035b058b6" => :lion
+  end
+
   option :universal
 
   depends_on 'cmake' => :build
