4095e044fd1aae2acf3fc936875d87ebaad90b03
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 04:29:17 +0100

    memcached: update 1.4.18 bottle.

--- Library/Formula/memcached.rb
+++ Library/Formula/memcached.rb
@@ -4,9 +4,9 @@ class Memcached < Formula
   homepage 'http://memcached.org/'
 
   bottle do
-    sha1 "9659921b7f83252f74fd71c9d89b0f087a987c39" => :mavericks
-    sha1 "56150a0077821d1758073238e8bac39f0028b6df" => :mountain_lion
-    sha1 "f0b6c864165782512b716f71853f6e24d57fdbda" => :lion
+    sha1 "da701fad18d7333060628ffbe365490c2a5eca94" => :mavericks
+    sha1 "9d750dc270c55142646d4c7e31c63a4a231634cd" => :mountain_lion
+    sha1 "891dec9f0e1169e7eb7ebd2babf5342057fe8961" => :lion
   end
   url 'http://www.memcached.org/files/memcached-1.4.18.tar.gz'
   sha1 'e550ac63f1accb2c4b8384fd200a79a7e574b364'
