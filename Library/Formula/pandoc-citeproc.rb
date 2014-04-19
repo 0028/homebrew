0444fcad0517adb452c65c142b88fe9c6319b266
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 06:45:13 +0100

    pandoc-citeproc: add 0.3.0.1 bottle.

--- Library/Formula/pandoc-citeproc.rb
+++ Library/Formula/pandoc-citeproc.rb
@@ -8,6 +8,12 @@ class PandocCiteproc < Formula
   url "http://hackage.haskell.org/package/pandoc-citeproc-0.3.0.1/pandoc-citeproc-0.3.0.1.tar.gz"
   sha1 "958309f9996d563ffba0bd4870bb7f201aac08e8"
 
+  bottle do
+    sha1 "fc0e5575030a8f01d56354359200d0126e0f6e99" => :mavericks
+    sha1 "d72bc35593648a2893f9ada11cfbd1665044e09d" => :mountain_lion
+    sha1 "6e7e9af8847458f347c0a51b87901816c09ae203" => :lion
+  end
+
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
 
