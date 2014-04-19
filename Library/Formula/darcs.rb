0c70ecd8bbc27bec934c43d4aa809fc5da5e139d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 06:45:13 +0100

    darcs: add 2.8.4 bottle.

--- Library/Formula/darcs.rb
+++ Library/Formula/darcs.rb
@@ -8,6 +8,13 @@ class Darcs < Formula
   url "http://darcs.net/releases/darcs-2.8.4.tar.gz"
   sha1 "36dde7548a9d73f4688296cac72bcda672542e53"
 
+  bottle do
+    cellar :any
+    sha1 "557d5fc7b5cb8c70a9ab205c0379dfb1e4cf3902" => :mavericks
+    sha1 "9387cbf5abf286b3ba3f2c4ebf4c7b60d1b98c66" => :mountain_lion
+    sha1 "b52a2eda94a5c59c68f1f0248c3e90c7c9df909e" => :lion
+  end
+
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
 
