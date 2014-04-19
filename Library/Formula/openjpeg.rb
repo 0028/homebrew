98ac6452f395b935b83673e5ecc23012ea9a45ff
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 15:04:15 +0100

    openjpeg: add 2.0.0 bottle.

--- Library/Formula/openjpeg.rb
+++ Library/Formula/openjpeg.rb
@@ -4,6 +4,13 @@ class Openjpeg < Formula
   homepage 'http://www.openjpeg.org/'
   url 'https://openjpeg.googlecode.com/files/openjpeg-2.0.0.tar.gz'
   sha1 '0af78ab2283b43421458f80373422d8029a9f7a7'
+  bottle do
+    cellar :any
+    sha1 "1711537db1d39776eee65f4ce2975fa8d9a581fd" => :mavericks
+    sha1 "2e434a426e704dea8f791ced801adec3e1751e33" => :mountain_lion
+    sha1 "510545749f1ef410f596f912887bca9a7037c894" => :lion
+  end
+
   revision 1
 
   head 'http://openjpeg.googlecode.com/svn/trunk/'
