d2dca753679b8806330d681f4ad49c47566de77f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 06:45:13 +0100

    pandoc: add 1.12.3 bottle.

--- Library/Formula/pandoc.rb
+++ Library/Formula/pandoc.rb
@@ -8,6 +8,12 @@ class Pandoc < Formula
   url "https://pandoc.googlecode.com/files/pandoc-1.12.3.tar.gz"
   sha1 "f519b5fb8c88ff4374432477dc12f68bbe238510"
 
+  bottle do
+    sha1 "7ad7517fb5ac7ed3e950af7b3a4f9af7b1c417a4" => :mavericks
+    sha1 "9e2eb4fa8344c1619771940d0cd12e42338f5aa7" => :mountain_lion
+    sha1 "2cbf5718ac852755d05d0c6611f0a2c737a634b0" => :lion
+  end
+
   resource "completion" do
     url "https://github.com/dsanson/pandoc-completion.git", :branch => "master"
   end
