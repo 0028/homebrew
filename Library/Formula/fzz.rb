9b93abf3bc1efd7e3e8a31267772e260c1216f52
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 13:33:06 +0100

    fzz: add 0.0.1 bottle.

--- Library/Formula/fzz.rb
+++ Library/Formula/fzz.rb
@@ -5,6 +5,12 @@ class Fzz < Formula
   url "https://github.com/mrnugget/fzz/archive/v0.0.1.tar.gz"
   sha1 "70b0326d3c7cbffafb327fa092eeaf647f65ea08"
 
+  bottle do
+    sha1 "0a9e5db42c23d22ff98add381621fd9506542f6c" => :mavericks
+    sha1 "c0cc0e7131e433fd5c76f19afc6d528e8b17c776" => :mountain_lion
+    sha1 "798e7838608e62408439ec6b1418ec7950fb5163" => :lion
+  end
+
   depends_on 'go' => :build
 
   def install
