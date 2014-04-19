d69f4f77bbfe8e22522b950808283fe4325387ca
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 17:20:39 +0100

    watchman: add 2.9.6 bottle.

--- Library/Formula/watchman.rb
+++ Library/Formula/watchman.rb
@@ -6,6 +6,13 @@ class Watchman < Formula
   url 'https://github.com/facebook/watchman/archive/v2.9.6.tar.gz'
   sha1 '415e0f2547205c7507e520bc40dc8cca3e06a40a'
 
+  bottle do
+    cellar :any
+    sha1 "8cc66d5cb46721821930b286183a85cda37cdb5e" => :mavericks
+    sha1 "6e36adff071b627efea29ffb26bf8456293debf7" => :mountain_lion
+    sha1 "370646b6e2dfa8cdc8044553d141592d68935bad" => :lion
+  end
+
   depends_on :autoconf
   depends_on :automake
   depends_on 'pkg-config' => :build
