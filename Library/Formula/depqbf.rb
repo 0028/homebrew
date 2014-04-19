8e8b6ff6b848a81a3b9cba8f138398dbd1aba774
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-19 17:20:39 +0100

    depqbf: add 3.01 bottle.

--- Library/Formula/depqbf.rb
+++ Library/Formula/depqbf.rb
@@ -6,6 +6,13 @@ class Depqbf < Formula
   sha1 '7f1dc19f07fc0fa607724d1b7b124d5f2620acee'
   head 'https://github.com/lonsig/depqbf.git'
 
+  bottle do
+    cellar :any
+    sha1 "d1609b988a3401ef04a165ad270eeb8a1cf5766e" => :mavericks
+    sha1 "be850c4867256c7ce8b57ea64f871479d3fe50db" => :mountain_lion
+    sha1 "0abacf64e19bc3c19e1b761781b5a09a6baf47f3" => :lion
+  end
+
   def install
     system "make"
     bin.install "depqbf"
